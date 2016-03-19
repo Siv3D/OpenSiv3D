b6aa3badfc27dad7feee06bcb6de0ee29f24ce19
Reputeless <reputeless@gmail.com>
2016-03-19 22:40:37 +0900

    Array<bool>

--- Siv3D-Test/Siv3D-Test/Test/TestArray.cpp
+++ Siv3D-Test/Siv3D-Test/Test/TestArray.cpp
@@ -10,25 +10,45 @@
 //-----------------------------------------------
 
 # include <Siv3D.hpp>
-# include <vector>
 # include "Siv3DTest.hpp"
 
 void TestArray()
 {
-	static_assert(sizeof(std::vector<int>) == sizeof(s3d::Array<int>), "");
-
-	s3d::Array<s3d::int32> v1 = { 10,20,30,40,50 };
-
-	for (int i = 0; i < 3; ++i)
 	{
-		{
-			RDTSC r;
-			std::vector<int> v(100000, 123);
-		}
+		static_assert(sizeof(s3d::Array<s3d::int32>) == sizeof(std::vector<s3d::int32>), "");
 
+		s3d::Array<s3d::int32> vi = { 10, 20, 30, 40, 50 };
+
+		for (int i = 0; i < 3; ++i)
 		{
-			RDTSC r;
-			s3d::Array<int> v(100000, 123);
+			{
+				RDTSC r;
+				std::vector<int> v(100000, 123);
+			}
+
+			{
+				RDTSC r;
+				s3d::Array<int> v(100000, 123);
+			}
+		}
+	}
+
+	{
+		static_assert(sizeof(s3d::Array<bool>) == sizeof(std::basic_string<bool>), "");
+
+		s3d::Array<bool> vb = { true, true, false, false, true };
+
+		for (int i = 0; i < 3; ++i)
+		{
+			{
+				RDTSC r;
+				std::vector<bool> v(100000, true);
+			}
+
+			{
+				RDTSC r;
+				s3d::Array<bool> v(100000, true);
+			}
 		}
 	}
 }
--- Siv3D/Include/Siv3D/Array.hpp
+++ Siv3D/Include/Siv3D/Array.hpp
@@ -11,6 +11,7 @@
 
 # pragma once
 # include <vector>
+# include <string>
 # include "Allocator.hpp"
 
 namespace s3d
@@ -111,4 +112,91 @@ namespace s3d
 	{
 		a.swap(b);
 	}
+
+	template <>
+	class Array<bool> : protected std::basic_string<bool>
+	{
+	private:
+
+		using base_type = std::basic_string<bool>;
+
+	public:
+
+		using base_type::value_type;
+		using base_type::pointer;
+		using base_type::const_pointer;
+		using base_type::reference;
+		using base_type::const_reference;
+		using base_type::iterator;
+		using base_type::const_iterator;
+		using base_type::reverse_iterator;
+		using base_type::const_reverse_iterator;
+		using base_type::size_type;
+		using base_type::difference_type;
+		using base_type::allocator_type;
+
+		using base_type::operator=;
+		using base_type::assign;
+		using base_type::get_allocator;
+		using base_type::at;
+		using base_type::operator[];
+		using base_type::front;
+		using base_type::back;
+		using base_type::data;
+		using base_type::begin;
+		using base_type::cbegin;
+		using base_type::end;
+		using base_type::cend;
+		using base_type::rbegin;
+		using base_type::crbegin;
+		using base_type::rend;
+		using base_type::empty;
+		using base_type::size;
+		using base_type::max_size;
+		using base_type::reserve;
+		using base_type::capacity;
+		using base_type::shrink_to_fit;
+		using base_type::clear;
+		using base_type::insert;
+		using base_type::erase;
+		using base_type::push_back;
+		using base_type::pop_back;
+		using base_type::resize;
+		using base_type::swap;
+
+		Array() = default;
+
+		Array(size_type count, const bool& value)
+			: base_type(count, value) {}
+
+		explicit Array(size_type count)
+			: base_type(count, false) {}
+
+		template <class InputIt>
+		Array(InputIt first, InputIt last)
+			: base_type(first, last) {}
+
+		Array(const Array& other)
+			: base_type(other) {}
+
+		Array(Array&& other) noexcept
+			: base_type(std::move(other)) {}
+
+		Array(std::initializer_list<bool> init)
+			: base_type(init.begin(), init.end()) {}
+
+		template<class... Args>
+		iterator emplace(const_iterator pos, Args&&... args)
+		{
+			bool tmp(std::forward<Args>(args)...);
+			return (insert(pos, tmp));
+		}
+
+		template<class... Args>
+		void emplace_back(Args&&... args)
+		{
+			bool tmp(std::forward<Args>(args)...);
+			push_back(tmp);
+		}
+	};
 }
