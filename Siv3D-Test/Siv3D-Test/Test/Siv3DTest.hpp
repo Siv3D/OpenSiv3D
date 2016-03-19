a6dfde143f0b6fa817afa3f72573464ac8d71452
Reputeless <reputeless@gmail.com>
2016-03-19 16:02:40 +0900

    テスト追加

---
+++ Siv3D-Test/Siv3D-Test/Test/Siv3DTest.hpp
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (C) 2008-2016 Ryo Suzuki
+//	Copyright (C) 2016 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+
+void TestTypes();
+//void TestArray();
+//void TestString();
---
+++ Siv3D-Test/Siv3D-Test/Test/TestTypes.cpp
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (C) 2008-2016 Ryo Suzuki
+//	Copyright (C) 2016 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D.hpp>
+
+void TestTypes()
+{
+	static_assert(sizeof(s3d::int8) == 1, "");
+	static_assert(sizeof(s3d::int16) == 2, "");
+	static_assert(sizeof(s3d::int32) == 4, "");
+	static_assert(sizeof(s3d::int64) == 8, "");
+
+	static_assert(sizeof(s3d::uint8) == 1, "");
+	static_assert(sizeof(s3d::uint16) == 2, "");
+	static_assert(sizeof(s3d::uint32) == 4, "");
+	static_assert(sizeof(s3d::uint64) == 8, "");
+}
