b10a1f709842917cefb2812d7604e77e98f05690
Reputeless <reputeless@gmail.com>
2016-03-19 16:03:13 +0900

    整形

--- Siv3D-Test/Siv3D-Test/Test/TestTypes.cpp
+++ Siv3D-Test/Siv3D-Test/Test/TestTypes.cpp
@@ -13,12 +13,12 @@
 
 void TestTypes()
 {
-	static_assert(sizeof(s3d::int8) == 1, "");
+	static_assert(sizeof(s3d::int8 ) == 1, "");
 	static_assert(sizeof(s3d::int16) == 2, "");
 	static_assert(sizeof(s3d::int32) == 4, "");
 	static_assert(sizeof(s3d::int64) == 8, "");
 
-	static_assert(sizeof(s3d::uint8) == 1, "");
+	static_assert(sizeof(s3d::uint8 ) == 1, "");
 	static_assert(sizeof(s3d::uint16) == 2, "");
 	static_assert(sizeof(s3d::uint32) == 4, "");
 	static_assert(sizeof(s3d::uint64) == 8, "");
