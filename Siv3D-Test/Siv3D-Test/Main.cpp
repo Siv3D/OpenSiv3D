c50172ff858cc2621c7236f50e87d22d2cddb66a
Reputeless <reputeless@gmail.com>
2016-03-19 15:00:27 +0900

    最初のファイル

---
+++ Siv3D-Test/Siv3D-Test/Main.cpp
@@ -0,0 +1,13 @@
+﻿
+# define  NOMINMAX
+# define  STRICT
+# define  WIN32_LEAN_AND_MEAN
+# define  _WIN32_WINNT _WIN32_WINNT_WIN7
+# define  NTDDI_VERSION NTDDI_WIN7
+# include <Windows.h>
+# include <Siv3D.hpp>
+
+void Main()
+{
+	::OutputDebugStringW(L"Siv3D!\n");
+}
---
+++ Siv3D/Source/Siv3DMain.cpp
@@ -0,0 +1,14 @@
+﻿
+# define  NOMINMAX
+# define  STRICT
+# define  WIN32_LEAN_AND_MEAN
+# define  _WIN32_WINNT _WIN32_WINNT_WIN7
+# define  NTDDI_VERSION NTDDI_WIN7
+# include <Windows.h>
+
+void Main();
+
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, wchar_t*, int)
+{
+	Main();
+}
