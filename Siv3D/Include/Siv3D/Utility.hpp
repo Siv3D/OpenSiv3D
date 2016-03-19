2d8b93e1d57a8cd0046eda8e83fb1cbcd6a89629
Reputeless <reputeless@gmail.com>
2016-03-19 23:34:38 +0900

    Utility

---
+++ Siv3D/Include/Siv3D/Utility.hpp
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (C) 2008-2016 Ryo Suzuki
+//	Copyright (C) 2016 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include <random>
+
+namespace s3d
+{
+	inline auto& GetDefaultRNG()
+	{
+		static thread_local std::mt19937_64 rng;
+		return rng;
+	}
+
+	static auto AsBool = [](const auto& v) { return !!v; };
+}
