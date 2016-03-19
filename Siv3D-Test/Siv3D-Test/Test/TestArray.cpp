7503dc095e654c471f4706d07a6e7c381e67df63
Reputeless <reputeless@gmail.com>
2016-03-20 00:06:06 +0900

    Array test

--- Siv3D-Test/Siv3D-Test/Test/TestArray.cpp
+++ Siv3D-Test/Siv3D-Test/Test/TestArray.cpp
@@ -11,61 +11,76 @@
 
 # include "Siv3DTest.hpp"
 
+using namespace s3d;
+
+void TestArrayInt32()
+{
+	Println(L"###### Array<int32>");
+
+	static_assert(sizeof(Array<int32>) == sizeof(std::vector<s3d::int32>), "");
+
+	for (int i = 0; i < 3; ++i)
+	{
+		{
+			RDTSC r;
+			std::vector<int32> v(100000, 123);
+		}
+
+		{
+			RDTSC r;
+			Array<int32> v(100000, 123);
+		}
+	}
+
+	const Array<int32> v = { 1, 2, 3, 4, 5 };
+
+	Println(L"######### Array<int32>::operator <<");
+	Println((Array<int32>{ 100, 200, 300 } << 1 << 2 << 3).join(L", "));
+
+	Println(L"######### Array<int32>::all");
+	Println(Array<int32>{}.all());
+	Println(Array<int32>{}.all([](auto n) { return n < 10; }));
+	Println(v.all());
+	Println(v.all([](auto n) { return n < 10; }));
+}
+
+void TestArrayString()
+{
+	Println(L"###### Array<String>");
+}
+
+void TestArrayBool()
+{
+	Println(L"###### Array<bool>");
+
+	static_assert(sizeof(Array<bool>) == sizeof(std::basic_string<bool>), "");
+
+	for (int i = 0; i < 3; ++i)
+	{
+		{
+			RDTSC r;
+			std::vector<bool> v(100000, true);
+		}
+
+		{
+			RDTSC r;
+			Array<bool> v(100000, true);
+		}
+	}
+
+	const Array<bool> v = { true, true, false, false, true };
+}
+
+void TestArray__m128()
+{
+	Println(L"###### Array<__m128>");
+}
+
 void TestArray()
 {
-	using namespace s3d;
-
-	{
-		static_assert(sizeof(Array<int32>) == sizeof(std::vector<s3d::int32>), "");
-
-		Array<int32> vi = { 10, 20, 30, 40, 50 };
-
-		for (int i = 0; i < 3; ++i)
-		{
-			{
-				RDTSC r;
-				std::vector<int> v(100000, 123);
-			}
-
-			{
-				RDTSC r;
-				Array<int> v(100000, 123);
-			}
-		}
-	}
-
-	{
-		const Array<int32> v = { 1,2,3,4,5 };
-
-		Println(L"[operator <<]---");
-		Println((Array<int32>{100, 200, 300} << 1 << 2 << 3).join(L", "));
-
-		Println(L"[all]---");
-		Println(Array<int32>{}.all());
-		Println(v.all());
-		Println(Array<bool>{false, false, false}.all());
-		Println(Array<double>{-0.4, 0.5, 1.2}.all([](auto v) { return v > 0.0; }));
-	}
-
-
-
-
-	{
-		static_assert(sizeof(Array<bool>) == sizeof(std::basic_string<bool>), "");
-
-		Array<bool> vb = { true, true, false, false, true };
-
-		for (int i = 0; i < 3; ++i)
-		{
-			{
-				RDTSC r;
-				std::vector<bool> v(100000, true);
-			}
-
-			{
-				RDTSC r;
-				Array<bool> v(100000, true);
-			}
-		}
-	}
+	Println(L"### Array");
+	TestArrayInt32();
+	TestArrayString();
+	TestArrayBool();
+	TestArray__m128();
 }
