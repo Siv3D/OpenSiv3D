fbbb84e1a140680a14978c6ef2a2fdb398509396
Reputeless <reputeless@gmail.com>
2016-03-21 21:00:10 +0900

    ToString

--- Siv3D/Include/Siv3D/FormatFloat.hpp
+++ Siv3D/Include/Siv3D/FormatFloat.hpp
@@ -16,4 +16,55 @@
 namespace s3d
 {
 	String FormatFloat(double value, int32 decimalPlace, bool fixed = false);
+
+	/// <summary>
+	/// 数値を文字列で表します。
+	/// </summary>
+	/// <param name="value">
+	///	数値
+	///	</param>
+	/// <param name="decimalPlace">
+	/// 小数点以下の最大桁数
+	/// </param>
+	/// <returns>
+	///	変換した文字列
+	///	</returns>
+	inline String ToString(float value, int32 decimalPlace = 3)
+	{
+		return FormatFloat(value, decimalPlace);
+	}
+
+	/// <summary>
+	/// 数値を文字列で表します。
+	/// </summary>
+	/// <param name="value">
+	///	数値
+	///	</param>
+	/// <param name="decimalPlace">
+	/// 小数点以下の最大桁数
+	/// </param>
+	/// <returns>
+	///	変換した文字列
+	///	</returns>
+	inline String ToString(double value, int32 decimalPlace = 3)
+	{
+		return FormatFloat(value, decimalPlace);
+	}
+
+	/// <summary>
+	/// 数値を文字列で表します。
+	/// </summary>
+	/// <param name="value">
+	///	数値
+	///	</param>
+	/// <param name="decimalPlace">
+	/// 小数点以下の最大桁数
+	/// </param>
+	/// <returns>
+	///	変換した文字列
+	///	</returns>
+	inline String ToString(long double value, int32 decimalPlace = 3)
+	{
+		return FormatFloat(static_cast<double>(value), decimalPlace);
+	}
 }
