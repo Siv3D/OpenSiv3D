996ebd0a42379f1038d34391afe0888ccfee0cdb
Reputeless <reputeless@gmail.com>
2016-04-02 15:02:26 +0900

    update

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -5,9 +5,28 @@
 using namespace s3d;
 using namespace s3d::literals;
 
+Array<int> Gen()
+{
+	Array<int> v(10);
+
+	for (size_t i = 0; i < v.size(); ++i)
+	{
+		v[i] = i;
+	}
+
+	return v;
+}
+
 void Main()
 {
-	RunTest();
+	//RunTest();
+
+	for (int i = 0; i < 10; ++i)
+	{
+		RDTSC r;
+
+		Log(Gen().shuffled());
+	}
 
 	Log(L"Siv3D!");
 }
