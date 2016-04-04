44c0dcf768acdddd7de1e1970091a6cf5d137da5
Reputeless <reputeless@gmail.com>
2016-04-04 22:50:53 +0900

    update

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -6,9 +6,11 @@ using namespace s3d;
 
 void Main()
 {
-	const auto v = Range(5000000, 100000000)
+	const auto v = Range(5'000'000, 1'000'000'000)
 					.filter([](auto n) { return n % 3333 == 0; })
 					.take(500)
 					.map([](auto n) { return n / 123.4; });
 	Log(v);
 }
+
+
