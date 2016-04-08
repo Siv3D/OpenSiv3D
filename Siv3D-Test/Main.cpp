28d28576e57afd164649448499c2e976a5a5a25b
Reputeless <reputeless@gmail.com>
2016-04-08 22:17:51 +0900

    update

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -3,24 +3,28 @@
 # include "Test/Siv3DTest.hpp"
 
 using namespace s3d;
-using namespace s3d::literals;
 
 void Main()
 {
-	Log(Range(1, 100)
-		.filter(LessThanEqual(10))
-		.map([](auto n) {return n*0.1; })
-		.reduce1(std::plus<>()));
+	// Fibbonaci (0, 1, 1, 2, 3, 5, .., 573147844013817084101)
+	BigInt a = 0, b = 1, t;
+	Log(L"0\n1\n");
+	Range(1, 100).map([&](auto) { t = a + b; a = b; return b = t; }).each(Log<BigInt>);
 
-	/*
-	reduce
-
-	DIGITS
-	Number
-	String & &&
-
-	Pad
-	*/
+	// FizzBuzz (1, 2, Fizz, 4, Buzz, .., Buzz)
+	Range(1, 100).map([](auto n){ return n % 15 == 0 ? L"FizzBuzz"
+									   : n %  3 == 0 ? L"Fizz"
+									   : n %  5 == 0 ? L"Buzz"
+									   : Format(n); }).each(Log<String>);
 }
 
 
+/*
+reduce
+
+DIGITS
+Number
+String & &&
+
+Pad
+*/
