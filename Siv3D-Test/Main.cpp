6640028f6df2023ae874f5934538f32fd06c2255
Reputeless <reputeless@gmail.com>
2016-04-08 17:31:59 +0900

    fix

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -5,28 +5,28 @@
 using namespace s3d;
 using namespace s3d::literals;
 
-
-
 void Main()
 {
 	size_t x = 0;
 
-	constexpr bool k = InRange(0, 100)(20);
-
 	{
-		RDTSC t;
+		//RDTSC t;
+        const auto b = Time::GetNanosec();
 		x = Range(0, 1000000)
 			.filter([](auto n) { return n%2!=0; })
 			.count();
+        Log(Time::GetNanosec()-b);
 	}
 
 	Log(x);
 
 	{
-		RDTSC t;
+		//RDTSC t;
+        const auto b = Time::GetNanosec();
 		x = Range(0, 1000000)
 			.filter(Odd())
 			.count();
+        Log(Time::GetNanosec()-b);
 	}
 
 	Log(x);
