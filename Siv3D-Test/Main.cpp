f4c9f2f2e22c8ab6e25835c56d5e5a77bd59f177
Reputeless <reputeless@gmail.com>
2016-05-02 16:28:13 +0900

    characterset

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -4,8 +4,20 @@
 
 using namespace s3d;
 
+static constexpr char hexTable[2][16] =
+{
+	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' },
+	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' }
+};
+
 void Main()
 {
+	//constexpr char k = hexTable[false][9];
+
+	//const char* table = hexTable[0];
+
+	Log(CharacterSet::PercentEncode(L"ウィキペディア", false));
+	
 	RunTest();
 }
 
