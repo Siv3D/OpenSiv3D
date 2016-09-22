ac178498df04ad3fc6f2e490ea5364070f22cb44
Reputeless <reputeless@gmail.com>
2016-09-22 22:13:42 +0900

    update

--- Siv3D/Include/Siv3D/Allocator.hpp
+++ Siv3D/Include/Siv3D/Allocator.hpp
@@ -18,6 +18,113 @@
 namespace s3d
 {
 	template <class Type, size_t Alignment = alignof(Type)>
+	inline Type* AlignedMalloc(size_t n = 1)
+	{
+		# if defined (SIV3D_TARGET_WINDOWS)
+			return static_cast<Type*>(::_aligned_malloc(sizeof(Type) * n, Alignment));
+		# else
+			Type* p;
+			::posix_memalign(&p, Alignment, sizeof(Type) * n);
+			return p;
+		# endif	
+	}
+
+	inline void AlignedFree(void* p)
+	{
+		# if defined (SIV3D_TARGET_WINDOWS)
+			::_aligned_free(p);
+		# else
+			::free(p);
+		# endif
+	}
+
+	template <class Type>
+	struct HasAlignment
+		: std::integral_constant<bool, (alignof(Type) > SIV3D_ALLOCATOR_MIN_ALIGNMENT)> {};
+
+	template <class Type, class ...Args>
+	inline auto AlignedNew(Args&&... args)
+	{
+		Type* p = AlignedMalloc<Type>();
+
+		if (p == nullptr)
+		{
+			throw std::bad_alloc();
+		}
+
+		try
+		{
+			::new (p) Type(std::forward<Args>(args)...);
+		}
+		catch (...)
+		{
+			AlignedFree(p);
+
+			throw;
+		}
+
+		return p;
+	}
+
+	template <class Type>
+	inline void AlignedDelete(Type* p)
+	{
+		if (p == nullptr)
+		{
+			return;
+		}
+
+		p->~Type();
+
+		AlignedFree(p);
+	}
+
+	template <class Type>
+	struct AlignedDeleter
+	{
+		void operator ()(Type* p)
+		{
+			AlignedDelete(p);
+		}
+	};
+
+	template <class Type, class ...Args>
+	std::unique_ptr<Type, AlignedDeleter<Type>> AlignedUnique(Args&&... args)
+	{
+		return std::unique_ptr<Type, AlignedDeleter<Type>>(AlignedNew<Type>(std::forward<Args>(args)...));
+	}
+
+	template <class Type, class ...Args>
+	std::shared_ptr<Type> AlignedShared(Args&&... args)
+	{
+		return std::shared_ptr<Type>(AlignedNew<Type>(std::forward<Args>(args)...), AlignedDeleter<Type>());
+	}
+
+	template <class Type, class ...Args, std::enable_if_t<!HasAlignment<Type>::value>* = nullptr>
+	std::unique_ptr<Type> MakeUnique(Args&&... args)
+	{
+		return std::make_unique<Type>(std::forward<Args>(args)...);
+	}
+
+	template <class Type, class ...Args, std::enable_if_t<HasAlignment<Type>::value>* = nullptr>
+	std::unique_ptr<Type, AlignedDeleter<Type>> MakeUnique(Args&&... args)
+	{
+		return AlignedUnique<Type>(std::forward<Args>(args)...);
+	}
+
+	template <class Type, class ...Args, std::enable_if_t<!HasAlignment<Type>::value>* = nullptr>
+	std::shared_ptr<Type> MakeShared(Args&&... args)
+	{
+		return std::make_shared<Type>(std::forward<Args>(args)...);
+	}
+
+	template <class Type, class ...Args, std::enable_if_t<HasAlignment<Type>::value>* = nullptr>
+	std::shared_ptr<Type> MakeShared(Args&&... args)
+	{
+		return std::shared_ptr<Type>(AlignedNew<Type>(std::forward<Args>(args)...), AlignedDeleter<Type>());
+	}
+
+	template <class Type, size_t Alignment = alignof(Type)>
 	class AlignedAllocator
 	{
 	public:
@@ -40,22 +147,12 @@ namespace s3d
 
 		Type* allocate(size_t n, const void* = nullptr)
 		{
-			# if defined (SIV3D_TARGET_WINDOWS)
-				return static_cast<Type*>(::_aligned_malloc(n * sizeof(Type), Alignment));
-			# else
-				Type* p;
-				::posix_memalign(&p, Alignment, n * sizeof(Type));
-				return p;
-			# endif
+			return AlignedMalloc<Type, Alignment>(n);
 		}
 
 		void deallocate(Type* p, size_t)
 		{
-			# if defined (SIV3D_TARGET_WINDOWS)
-				::_aligned_free(p);
-			# else
-				::free(p);
-			# endif
+			AlignedFree(p);
 		}
 	};
 
@@ -74,6 +171,5 @@ namespace s3d
 	template <class Type>
 	struct DefaultAllocator
 	{
-		using type = std::conditional_t<(alignof(Type)> SIV3D_ALLOCATOR_MIN_ALIGNMENT), AlignedAllocator<Type>, std::allocator<Type>>;
 	};
 }
