a1350429a5a668239330bddd57fa35a981f6de6c
Reputeless <reputeless@gmail.com>
2017-01-03 16:25:37 +0900

    Base64 準備

--- Siv3D/Include/Siv3D/Base64.hpp
+++ Siv3D/Include/Siv3D/Base64.hpp
@@ -10,8 +10,57 @@
 //-----------------------------------------------
 
 # pragma once
+# include "Fwd.hpp"
+# include "String.hpp"
+# include "ByteArrayView.hpp"
 
 namespace s3d
 {
+	/// <summary>
+	/// Base64
+	/// </summary>
+	/// <remarks>
+	/// Base64 エンコード/デコードの機能を提供します。
+	/// </remarks>
+	namespace Base64
+	{
+		/// <summary>
+		/// データを Base64 エンコードします。
+		/// </summary>
+		/// <param name="data">
+		/// エンコードするデータの先頭ポインタ
+		/// </param>
+		/// <param name="size">
+		/// エンコードするデータのサイズ（バイト）
+		/// </param>
+		/// <returns>
+		/// エンコードされたテキストデータ、エンコードに失敗した場合空の文字列
+		/// </returns>
+		String Encode(const void* data, size_t size);
 
+		/// <summary>
+		/// データを Base64 エンコードします。
+		/// </summary>
+		/// <param name="data">
+		/// エンコードするデータ
+		/// </param>
+		/// <returns>
+		/// エンコードされたテキストデータ、エンコードに失敗した場合空の文字列
+		/// </returns>
+		inline String Encode(const ByteArrayView view)
+		{
+			return Encode(view.data(), view.size());
+		}
+
+		/// <summary>
+		/// テキストを Base64 でデコードします。
+		/// </summary>
+		/// <param name="src">
+		/// デコードするテキスト
+		/// </param>
+		/// <returns>
+		/// デコードされたバイナリデータ、デコードに失敗した場合空のバイナリデータ
+		/// </returns>
+		ByteArray Decode(const String& base64);
+	};
 }
