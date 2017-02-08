1e407ea60f7babda433abf679baaf2579ac6a3b4
Reputeless <reputeless@gmail.com>
2017-02-08 09:03:59 +0900

    fix AlignedMemory

---
+++ Siv3D-Test/Test/TestBase64.cpp
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2017 Ryo Suzuki
+//	Copyright (c) 2016-2017 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include "Siv3DTest.hpp"
+
+# if defined(SIV3D_DO_TEST)
+
+using namespace s3d;
+
+TEST(Base64, Encode)
+{
+	
+}
+
+# endif
