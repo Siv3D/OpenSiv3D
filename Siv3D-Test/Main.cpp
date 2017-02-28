ca3969b0a514d634f28d2a9806805d54f88c3971
Reputeless <reputeless@gmail.com>
2017-02-28 22:26:56 +0900

    test

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -8,9 +8,40 @@ void Main()
 
 	while (System::Update())
 	{
-		if (KeyEnter.down())
-		{
-			Log << L"Hi";
-		}
+		if (KeyColon.down())
+			Log << L"a";
+
+		if (KeySemicolon.down())
+			Log << L"b";
+
+		if (KeyComma.down())
+			Log << L"c";
+
+		if (KeyMinus.down())
+			Log << L"d";
+
+		if (KeyPeriod.down())
+			Log << L"e";
+
+		if (KeySlash.down())
+			Log << L"f";
+
+		if (KeyGraveAccent.down())
+			Log << L"g";
+
+		if (KeyLBracket.down())
+			Log << L"h";
+
+		if (KeyYen.down())
+			Log << L"i";
+
+		if (KeyRBracket.down())
+			Log << L"j";
+
+		if (KeyCaret.down())
+			Log << L"k";
+
+		if (KeyBackslash.down())
+			Log << L"l";
 	}
 }
