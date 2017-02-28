94b5e3fe6491ff26140f2c3984d7c8a9833d366a
Reputeless <reputeless@gmail.com>
2017-03-01 00:09:57 +0900

    Key name

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -6,18 +6,8 @@ void Main()
 {
 	RunTest();
 
-	Array<int> v = { 10,20,30 };
-
-	v.push_front(5);
-
-	v.push_front(6);
-	
-	v.pop_front();
-
-	Log << v;
-
 	while (System::Update())
 	{
-		
+
 	}
 }
