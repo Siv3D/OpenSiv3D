9588259f524f892bfa078a43b2d0e0e4e03d02c6
Reputeless <reputeless@gmail.com>
2017-03-01 01:31:12 +0900

    KeyGroup 準備

--- Siv3D/Include/Siv3D/KeyGroup.hpp
+++ Siv3D/Include/Siv3D/KeyGroup.hpp
@@ -22,7 +22,54 @@ namespace s3d
 
 		Array<KeyConjunction> m_keyConjunctions;
 
+		template <class Type, class ...Args>
+		void append(const Type& key, const Args&... args)
+		{
+			append(key);
+			append(args...);
+		}
+
+		void append();
+
+		void append(const Key& key);
+
+		void append(const KeyConjunction& conjunction);
+
+		void append(const KeyGroup& group);
+
 	public:
 
+		template <class ...Args>
+		explicit KeyGroup(const Args&... args)
+		{
+			append(args...);
+		}
+
+		template <class Type>
+		KeyGroup& operator |=(const Type& key)
+		{
+			append(key);
+			return *this;
+		}
+
+		bool down() const;
+
+		bool pressed() const;
+
+		bool up() const;
+
+		int32 num_pressed() const;
+
+		MillisecondsF pressedDuration() const;
+
+		const Array<Key>& keys() const
+		{
+			return m_keys;
+		}
+
+		const Array<KeyConjunction>& keyConjunctions() const
+		{
+			return m_keyConjunctions;
+		}
 	};
 }
