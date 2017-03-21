8b9d9696665df763e674110b187dc20309084ee6
Reputeless <reputeless@gmail.com>
2017-03-21 22:35:04 +0900

    update

--- Siv3D/Include/Siv3D/Triangle.hpp
+++ Siv3D/Include/Siv3D/Triangle.hpp
@@ -17,6 +17,17 @@ namespace s3d
 {
 	struct Triangle
 	{
+		using position_type = Vec2;
 
+		using value_type = position_type::value_type;
+
+		Vec2 p0, p1, p2;
+
+		Triangle() = default;
+
+		constexpr Triangle(double x0, double y0, double x1, double y1, double x2, double y2) noexcept
+			: p0(x0, y0)
+			, p1(x1, y1)
+			, p2(x2, y2) {}
 	};
 }
