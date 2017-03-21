1d9f6cb467ba70a0578346440a3d0b7af21b5990
Reputeless <reputeless@gmail.com>
2017-03-21 17:02:02 +0900

    update

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -3,6 +3,12 @@
 
 void Main()
 {
+	constexpr int s = sizeof(Circle);
+	constexpr auto t1 = Circle(10, 50, 50).center;
+	Log << t1;
+	Log << t1.x;
+	Log << t1.y;
+
 	while (System::Update())
 	{
 		Graphics::SetBackground(HSV(Cursor::Pos().x * 0.5, 0.5, 1.0));
--- Siv3D/Include/Siv3D/Circle.hpp
+++ Siv3D/Include/Siv3D/Circle.hpp
@@ -19,10 +19,102 @@ namespace s3d
 	{
 		using position_type = Vec2;
 
-		using size_type = double;
+		using size_type = position_type::value_type;
 
 		using value_type = position_type::value_type;
 
+		S3D_DISABLE_MSVC_WARNINGS_PUSH(4201)
 
+			union
+		{
+			/// <summary>
+			/// 円の中心座標
+			/// </summary>
+			position_type center;
+
+			struct
+			{
+				/// <summary>
+				/// 円の中心の X 座標
+				/// </summary>
+				value_type x;
+
+				/// <summary>
+				/// 円の中心の Y 座標
+				/// </summary>
+				value_type y;
+			};
+		};
+
+		/// <summary>
+		/// 円の半径
+		/// </summary>
+		size_type r;
+
+		S3D_DISABLE_MSVC_WARNINGS_POP()
+
+			Circle() = default;
+
+		/// <summary>
+		/// 円を作成します。
+		/// </summary>
+		/// <param name="_r">
+		/// 円の半径
+		/// </param>
+		explicit constexpr Circle(size_type _r) noexcept
+			: center(0.0, 0.0)
+			, r(_r) {}
+
+		/// <summary>
+		/// 円を作成します。
+		/// </summary>
+		/// <param name="_x">
+		/// 円の中心の X 座標
+		/// </param>
+		/// <param name="_y">
+		/// 円の中心の Y 座標
+		/// </param>
+		constexpr Circle(value_type _x, value_type _y) noexcept
+			: center(_x, _y)
+			, r(0.0) {}
+
+		/// <summary>
+		/// 円を作成します。
+		/// </summary>
+		/// <param name="_x">
+		/// 円の中心の X 座標
+		/// </param>
+		/// <param name="_y">
+		/// 円の中心の Y 座標
+		/// </param>
+		/// <param name="_r">
+		/// 円の半径
+		/// </param>
+		constexpr Circle(value_type _x, value_type _y, size_type _r) noexcept
+			: center(_x, _y)
+			, r(_r) {}
+
+		/// <summary>
+		/// 円を作成します。
+		/// </summary>
+		/// <param name="_center">
+		/// 円の中心の座標
+		/// </param>
+		explicit constexpr Circle(const position_type& _center) noexcept
+			: center(_center)
+			, r(0.0) {}
+
+		/// <summary>
+		/// 円を作成します。
+		/// </summary>
+		/// <param name="_center">
+		/// 円の中心の座標
+		/// </param>
+		/// <param name="_r">
+		/// 円の半径
+		/// </param>
+		constexpr Circle(const position_type& _center, size_type _r) noexcept
+			: center(_center)
+			, r(_r) {}
 	};
 }
