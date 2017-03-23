ee5211a720eef3741bbfea6ea479613320b06db7
Reputeless <reputeless@gmail.com>
2017-03-24 01:27:10 +0900

    update

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -3,24 +3,8 @@
 
 void Main()
 {
-	const Image image(L"example/windmill.png");
-
-	auto task = CreateConcurrentTask([&]()
-	{
-		Range(4, 20).map(Multiplies(0.1)).asArray().parallel_each([&](double t)
-		{
-			image.savePerceptualJPEG(L"b{}.jpg"_fmt(t), t);
-		});
-	});
-
 	while (System::Update())
 	{
-		Window::SetTitle(L"変換中 ", String(9, L'□').insert(Time::GetMillisec() / 200 % 10, 1, L'■'));
 
-		if (task.is_done())
-		{
-			break;
-		}
 	}
 }
-
