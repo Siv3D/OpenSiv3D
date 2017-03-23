eae898e628ef243243f9974191c6137c9c17e5a2
Reputeless <reputeless@gmail.com>
2017-03-24 01:38:50 +0900

    fix copyright

--- Siv3D/Source/Siv3D/System/SystemFactory.cpp
+++ Siv3D/Source/Siv3D/System/SystemFactory.cpp
@@ -2,8 +2,8 @@
 //
 //	This file is part of the Siv3D Engine.
 //
-//	Copyright (C) 2008-2017 Ryo Suzuki
-//	Copyright (C) 2016-2017 OpenSiv3D Project
+//	Copyright (c) 2008-2017 Ryo Suzuki
+//	Copyright (c) 2016-2017 OpenSiv3D Project
 //
 //	Licensed under the MIT License.
 //
