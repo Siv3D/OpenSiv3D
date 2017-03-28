d1e44537f00b4bc0ef4ab09d8131e7805e89c560
Reputeless <reputeless@gmail.com>
2017-03-28 15:01:41 +0900

    FileSystem 分割準備

---
+++ Siv3D/Source/Siv3D/FileSystem/SivFileSystem_macOS.cpp
@@ -0,0 +1,15 @@
+//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2017 Ryo Suzuki
+//	Copyright (c) 2016-2017 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/Platform.hpp>
+# if defined(SIV3D_TARGET_MACOS)
+
+# endif
