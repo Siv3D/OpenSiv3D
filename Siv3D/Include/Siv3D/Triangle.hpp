f0883c0f9060ecbe3a8f58c8f905ef0e89aec18d
Reputeless <reputeless@gmail.com>
2017-03-28 18:37:38 +0900

    Triangle 準備

--- Siv3D/Include/Siv3D/Triangle.hpp
+++ Siv3D/Include/Siv3D/Triangle.hpp
@@ -15,6 +15,11 @@
 
 namespace s3d
 {
+	namespace detail
+	{
+		constexpr double Sqrt3 = 1.73205080756887729353; // std::sqrt(3.0)
+	}
+
 	struct Triangle
 	{
 		using position_type = Vec2;
@@ -25,9 +30,52 @@ namespace s3d
 
 		Triangle() = default;
 
+		explicit Triangle(double sides) noexcept
+			: p0(0.0, -1.0 / detail::Sqrt3 * sides)
+			, p1(sides * 0.5, sides / (2.0 * detail::Sqrt3))
+			, p2(-p1.x, p1.y) {}
+
+		Triangle(double sides, double angle) noexcept;
+
+		Triangle(double x, double y, double sides) noexcept;
+
+		Triangle(const Vec2& pos, double sides) noexcept
+			: Triangle(pos.x, pos.y, sides) {}
+
+		Triangle(double x, double y, double sides, double angle) noexcept;
+			
+		Triangle(const Vec2& pos, double sides, double angle) noexcept
+			: Triangle(pos.x, pos.y, sides, angle) {}
+
 		constexpr Triangle(double x0, double y0, double x1, double y1, double x2, double y2) noexcept
 			: p0(x0, y0)
 			, p1(x1, y1)
 			, p2(x2, y2) {}
+
+		constexpr Triangle(const Vec2& _p0, const Vec2& _p1, const Vec2& _p2) noexcept
+			: p0(_p0)
+			, p1(_p1)
+			, p2(_p2) {}
+
+		constexpr Triangle& set(double x0, double y0, double x1, double y1, double x2, double y2) noexcept
+		{
+			p0.set(x0, y0);
+			p1.set(x1, y1);
+			p2.set(x2, y2);
+			return *this;
+		}
+
+		constexpr Triangle& set(const Vec2& _p0, const Vec2& _p1, const Vec2& _p2) noexcept
+		{
+			p0.set(_p0);
+			p1.set(_p1);
+			p2.set(_p2);
+			return *this;
+		}
+
+		constexpr Triangle& set(const Triangle& triangle) noexcept
+		{
+			return *this = triangle;
+		}
 	};
 }
---
+++ Siv3D/Source/Siv3D/Triangle/SivTriangle.cpp
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2017 Ryo Suzuki
+//	Copyright (c) 2016-2017 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/Triangle.hpp>
+# include <Siv3D/Circular.hpp>
+
+namespace s3d
+{
+	Triangle::Triangle(const double sides, const double angle) noexcept
+		: p0(Circular(1.0 / detail::Sqrt3 * sides, angle))
+		, p1(Circular(1.0 / detail::Sqrt3 * sides, 2.0 * Math::OneThirdPi + angle))
+		, p2(Circular(1.0 / detail::Sqrt3 * sides, 4.0 * Math::OneThirdPi + angle))
+	{
+
+	}
+}
