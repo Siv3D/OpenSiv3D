ba1d4f28ceaccc554df9f4cb6ce8ecb6c45a6e8f
Reputeless <reputeless@gmail.com>
2017-04-07 22:31:58 +0900

    Profiler 準備

---
+++ Siv3D/Source/Siv3D/Profiler/CProfiler.cpp
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2017 Ryo Suzuki
+//	Copyright (c) 2016-2017 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include "CProfiler.hpp"
+
+namespace s3d
+{
+	CProfiler::CProfiler()
+	{
+
+	}
+
+	CProfiler::~CProfiler()
+	{
+
+	}
+
+	bool CProfiler::init()
+	{
+
+		return true;
+	}
+}
---
+++ Siv3D/Source/Siv3D/Profiler/CProfiler.hpp
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2017 Ryo Suzuki
+//	Copyright (c) 2016-2017 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include "IProfiler.hpp"
+
+namespace s3d
+{
+	class CProfiler : public ISiv3DProfiler
+	{
+	private:
+
+	public:
+
+		CProfiler();
+
+		~CProfiler() override;
+
+		bool init() override;
+	};
+}
