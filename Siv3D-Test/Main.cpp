548d71c81f2970f8eea16c027c9b23adb5fb0863
Reputeless <reputeless@gmail.com>
2017-05-15 13:53:31 +0900

    update

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -3,51 +3,14 @@
 
 void Main()
 {
-	// 線を作成
-	const LineString line
-	{
-		//{600,40},
-		{ 50, 100 },{ 600, 100 },
-		{ 50, 200 },{ 600, 200 },
-		{ 50, 300 },{ 600, 300 },
-		{ 50, 400 }//,{600,400}
-	};
-
-	int32 index = 0;
-
-	constexpr LineStyle::Parameters styles[5] =
-	{
-		LineStyle::SquareCap,
-
-		LineStyle::RoundCap,
-
-		LineStyle::NoCap,
-
-		LineStyle::SquareDot,
-
-		LineStyle::RoundDot,
-	};
-
-	double dotOffset = 0.0;
-
 	while (System::Update())
 	{
-		Window::SetTitle(index, L" @ ", Profiler::FPS(), L"FPS");
+		const double offset = Time::GetMillisec() * 0.005;
 
-		if (MouseL.down())
-		{
-			++index %= 5;
-		}
+		LineString{ {100, 100}, {500, 100}, {500, 400}, {100, 400} }
+			.draw(LineStyle::SquareDot(offset * 2), 8, Palette::White, true);
 
-
-		dotOffset = Cursor::Pos().x / 10.0;
-
-		Line(50, 0, 50, 480).draw();
-		Line(600, 0, 600, 480).draw();
-
-		// 黄色い線を太さ 3 で描く
-		line.draw(styles[index](dotOffset), 8, Palette::Yellow, MouseR.pressed());
-
-		Line(500, 400, 200, 440).draw(styles[index](dotOffset), 12, Palette::White);
+		Line(150, 350, 450, 150)
+			.draw(LineStyle::RoundDot(offset), 10, { HSV(20), HSV(60) });
 	}
 }
