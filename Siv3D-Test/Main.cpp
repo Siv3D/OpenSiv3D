d0833d67ce1995e9834019778f4e543e1bde22ec
Reputeless <reputeless@gmail.com>
2017-06-26 21:13:37 +0900

    fix TextBox

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -1,21 +1,46 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.1.5
+# include <HamFramework.hpp>
 
 void Main()
 {
 	Graphics::SetBackground(ColorF(0.8, 0.9, 1.0));
 
-	const Font font(50);
+	const Font font(50), fontSmall(20);
+	const TextBox textBox(fontSmall, Vec2(40, 40), 200);
 
-	const Texture textureCat(Emoji(L"🐈"), TextureDesc::Mipped);
+	const Array<Vec2> outer = Shape2D::Hexagon(100, Vec2(500, 200)).vertices().map([](Float2 v) { return Vec2(v); });
+	const Array<Vec2> hole = Shape2D::Hexagon(40, Vec2(500, 180)).vertices().reversed().map([](Float2 v) { return Vec2(v); });
+	const Polygon polygon(outer, { hole });
+	const Circle circle(220, 240, 120);
+
+	Camera2D camera;
 
 	while (System::Update())
 	{
-		font(L"Hello, Siv3D!🐣").drawAt(Window::Center(), Palette::Black);
+		camera.update();
+		auto t = camera.createTransformer();
 
-		font(Cursor::Pos()).draw(20, 400, ColorF(0.6));
+		textBox.update();
 
-		textureCat.resize(80).draw(540, 380);
+		font(L"Siv3D v0.1.5!🐣").draw(100, 400, ColorF(0.4));
 
-		Circle(Cursor::Pos(), 60).draw(ColorF(1, 0, 0, 0.5));
+		const Rect rect(Cursor::Pos(), 200, 100);
+
+		circle.draw(Palette::Skyblue);
+
+		polygon.draw(Palette::Seagreen);
+
+		rect.draw(ColorF(Palette::Orange, 0.5));
+
+		if (const auto intersections = rect.intersectsAt(circle))
+		{
+			for (const auto& point : intersections.value())
+			{
+				Circle(point, 10).draw(Palette::Red);
+			}
+		}
+
+		textBox.draw();
+		camera.draw(Palette::Pink);
 	}
 }
