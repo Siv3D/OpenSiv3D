0bbf9281f524bf973ee9117a6c39c1883f4fe14b
Reputeless <reputeless@gmail.com>
2017-07-20 01:45:34 +0900

    bug fix

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -2,50 +2,14 @@
 
 void Main()
 {
-	const Audio audio(L"example/test.wav");
+	Print << FileSystem::DirectoryContents(L"e");
 
-	audio.play();
+	Print << FileSystem::DirectoryContents(L"example");
 
-	uint64 max = 0, previous = 0;
+	Print << FileSystem::DirectoryContents(L"example/");
 
 	while (System::Update())
 	{	
-		const uint64 c = audio.posSample();
 
-		if (c < previous)
-		{
-			max = previous;
-		}
-
-		previous = c;
-
-		ClearPrint();
-		Print << c;
-		Print << max << L"/"  << audio.samples();
-
-		if (Key0.down())
-		{
-			audio.play();
-		}
-
-		if (Key1.down())
-		{
-			audio.play(0.1s);
-		}
-
-		if (Key2.down())
-		{
-			audio.pause(1s);
-		}
-
-		if (Key3.down())
-		{
-			audio.stop(1s);
-		}
-
-		if (Key4.down())
-		{
-			audio.setVolume_dB(-12);
-		}
 	}
 }
