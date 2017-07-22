5c10444e2280a7c8019908f0ee3ede7f32d87eaa
Reputeless <reputeless@gmail.com>
2017-07-22 19:43:23 +0900

    fix

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -1,17 +1,35 @@
-﻿# include <Siv3D.hpp> // OpenSiv3D v0.1.6
+﻿# include <Siv3D.hpp>
+# include <HamFramework.hpp>
 
 void Main()
 {
-	Script script(L"example/script.txt");
-	
-	auto GetNumber = script.getFunction<int32(void)>(L"GetNumber");
-	
-	auto DrawCircle = script.getFunction<void(const Circle&, const ColorF&)>(L"DrawCircle");
-	
-	Print << GetNumber();
-	
+	Window::Resize(1280, 720);
+
+	Graphics::SetBackground(Palette::Darkgreen);
+
+	const PlayingCard::Pack cardsPack(70, Palette::Red);
+
+	Array<PlayingCard::Card> cards = PlayingCard::CreateDeck(13);
+
 	while (System::Update())
-	{	
-		DrawCircle(Circle(Cursor::Pos(), 50), ColorF(1.0, 0.0, 0.0, 0.5));
+	{
+		size_t index = 0;
+
+		for (auto y : step(5))
+		{
+			for (auto x : step(13))
+			{
+				const Vec2 pos(50 + 85 * x, 25 + 125 * y);
+
+				if (cardsPack.region(pos).leftClicked())
+				{
+					cards[index].flip();
+				}
+
+				cardsPack(cards[index]).draw(pos);
+
+				++index;
+			}
+		}
 	}
 }
