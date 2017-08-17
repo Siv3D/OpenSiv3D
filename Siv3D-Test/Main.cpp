46ab749e18d3a8da019dae3e48de738002b8475f
Reputeless <Reputeless@users.noreply.github.com>
2017-08-17 17:54:41 +0900

    v0.1.6 準備

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -1,46 +1,21 @@
-﻿# include <Siv3D.hpp>
-# include <HamFramework.hpp>
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.1.6
 
 void Main()
 {
-	ScalableWindow::SetBaseSize(640, 480);
-	Window::Resize(1280, 720);
-	Graphics::SetBackground(Palette::Darkgreen);
+	Graphics::SetBackground(ColorF(0.8, 0.9, 1.0));
 
-	const PlayingCard::Pack pack(75, Palette::Red);
-	Array<PlayingCard::Card> cards = PlayingCard::CreateDeck(2);
-	Camera2D camera;
+	const Font font(50);
 
-	const Texture texture(L"example/windmill.png");
-
-	const Audio audio(L"example/test.mp3");
-	audio.play();
-	
-	TextBox textbox(Font(40), 240, 40, 200);
+	const Texture textureCat(Emoji(L"🐈"), TextureDesc::Mipped);
 
 	while (System::Update())
 	{
-		Window::SetTitle(audio.isPlaying(), L" ", audio.isPaused());
-		if(Key1.down()) audio.play();
-		if(Key2.down()) audio.pause();
-		if(Key3.down()) audio.stop();
-		
-		auto tc = camera.createTransformer();
-		auto ts = ScalableWindow::CreateTransformer();
-		camera.update();
-		textbox.update();
-		texture.draw();
+		font(L"Hello, Siv3D!🐣").drawAt(Window::Center(), Palette::Black);
 
-		for(auto i : step(5))
-		{
-			const Vec2 pos(320 + i * 20, 240);
-			Transformer2D t2(Mat3x2::Rotate(i*5_deg, pos));
-			pack(PlayingCard::Card(PlayingCard::Heart, i+1)).drawAt(pos);
-		}
+		font(Cursor::Pos()).draw(20, 400, ColorF(0.6));
 
-		textbox.draw();
+		textureCat.resize(80).draw(540, 380);
 
-		camera.draw();
-		ScalableWindow::DrawBlackBars();
+		Circle(Cursor::Pos(), 60).draw(ColorF(1, 0, 0, 0.5));
 	}
 }
