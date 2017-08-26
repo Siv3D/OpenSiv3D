8d3c9c13afcf8afb9d254b457e844e695d212fff
Reputeless <Reputeless@users.noreply.github.com>
2017-08-26 12:55:50 +0300

    update

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -2,24 +2,8 @@
 
 void Main()
 {
-	const Vec2 p1(0, 1);
-	const Vec2 p2(1, 1);
-
-	LineString lineString;
-
-	for (double x = 0; x <= 1.0; x += 0.01)
-		lineString.emplace_back(p1.lerp(p2, x).movedBy(0, 1.0*(pow(0.5 - x, 2.0) - 0.25)));
-
 	while (System::Update())
 	{
-		const auto t = Transformer2D(Mat3x2::Scale(300));
 
-		//描画されない
-		lineString.draw(0.1);
-
-		//描画される
-		if(MouseL.pressed())
-		for (int i = 0; i < int(lineString.size() - 1); i++)
-			Line(lineString[i], lineString[i + 1]).draw(0.1);
 	}
 }
