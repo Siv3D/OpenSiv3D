7f3e69a09bf1d4fb52ec967acf1dc71dfaadf191
Reputeless <Reputeless@users.noreply.github.com>
2017-08-30 22:20:11 +0300

    RandomVec2 Triangle

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -2,10 +2,22 @@
 
 void Main()
 {
-	Range(0, 10) >> Square >> Multiplies(0.1) >> Print;
+	const Triangle triangle({ 40, 100 }, { 400, 20 }, { 200, 250 });
+
+	Array<Vec2> points;
+
+	for (auto i : step(2000))
+	{
+		points << RandomVec2(triangle);
+	}
 
 	while (System::Update())
 	{
+		triangle.draw(Color(100));
 
+		for (const auto& point : points)
+		{
+			Circle(point, 1).draw();
+		}
 	}
 }
