e505b19c24b9a17e2f6d425ea54627629e71bb71
Reputeless <Reputeless@users.noreply.github.com>
2017-09-02 21:18:05 +0300

    fix

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -1,10 +1,27 @@
-﻿# include <Siv3D.hpp> // OpenSiv3D v0.1.7
+﻿# include <Siv3D.hpp>
 # include <HamFramework.hpp>
 
 void Main()
 {
+	const Size baseSize(720, 720);
+
+	ScalableWindow::SetBaseSize(baseSize);
+
+	const auto[displayIndex, displayMode]
+		= OptimalScreen::Get(OptimalScreen::Preference::AspectMin, baseSize);
+
+	Print << displayIndex << L":" << displayMode.size << L"@" << displayMode.refreshRateHz;
+
+	Graphics::SetFullScreen(true, displayMode.size, displayIndex, displayMode.refreshRateHz);
+
+	Graphics::SetBackground(ColorF(0.7, 0.9, 0.8));
+
 	while (System::Update())
 	{
+		const auto t = ScalableWindow::CreateTransformer();
 
+		Circle(Cursor::Pos(), 100).draw();
+
+		ScalableWindow::DrawBlackBars(ColorF(0.0));
 	}
 }
