e6a0fb47d4cba90957b306da9c99985ead1866e7
wynd2608 <wynd2608@outlook.jp>
2017-10-07 10:17:25 +0900

    GLSamplerState.cppでコンパイルが通らない問題修正

--- Siv3D/Source/Siv3D/Graphics/GL/SamplerState/GLSamplerState.cpp
+++ Siv3D/Source/Siv3D/Graphics/GL/SamplerState/GLSamplerState.cpp
@@ -65,7 +65,7 @@ namespace s3d
 		std::unique_ptr<SamplerState_GL> samplerState = std::make_unique<SamplerState_GL>();
 
 		const GLuint sampler = samplerState->m_sampler;
-		static const GLfloat border[] = { state.borderColor.x, state.borderColor.y, state.borderColor.z, state.borderColor.w };
+		static const GLfloat border[] = { state.borderColor[0], state.borderColor[1], state.borderColor[2], state.borderColor[3] };
 		static const GLuint wraps[] = { GL_REPEAT, GL_MIRRORED_REPEAT, GL_CLAMP_TO_EDGE, GL_CLAMP_TO_BORDER };
 		
 		::glSamplerParameteri(sampler, GL_TEXTURE_MIN_FILTER,
