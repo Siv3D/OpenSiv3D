c7dc4566ef897f86d3f8ebfb0ffbf55b7b1869c4
azaika <takaaza@googlemail.com>
2017-10-08 14:17:14 +0900

    Change interface of Optional::then()

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -3,56 +3,19 @@
 
 void Main()
 {
-	Window::Resize(640, 640);
+	Optional<int> x = 1, y = none;
+	
+	Print << x
+		.then([](int& a) { a += 5; })
+		.map([](int a) { return L"value is " + ToString(a); })
+		.map([](const String& s) { return s.length(); }, []() { return -1; });
 
-	const TOMLReader reader(L"test.toml");
+	// size_t と -1 の common_type を取るため、-1 が size_t にキャストされてしまう
+	Print << y
+		.then([](int& a) { a += 5; })
+		.map([](int a) { return ToString(a); })
+		.map([](const String& s) { return s.length(); }, []() { return -1; });
 
-	while (System::Update())
-	{
-		if (!reader)
-		{
-			Print << L"Failed to load.";
-			continue;
-		}
-
-		Print << L"\n";
-		// key access
-		Print << L"table.key = " << reader[L"table"][L"key"].getString();
-		// chained key access
-		Print << L"table.subtable.key = " << reader[L"table.subtable.key"].getString();
-
-		// each table access and get integer
-		for (auto&& p : reader[L"integer"].tableView())
-		{
-			if (p.value.isNumber())
-			{
-				Print << L"integer." + p.name + L" = " << p.value.get<int64>();
-			}
-		}
-
-		// get floating number
-		Print << L"float.fractional.key2 = " << reader[L"float.fractional.key2"].get<double>();
-
-		// get boolean
-		Print << L"boolean.True = " << reader[L"boolean.True"].get<bool>();
-
-		// get datetime
-		Print << L"datetime.key1 = " << reader[L"datetime.key1"].getDateTime();
-
-		// get array (and formatting)
-		Print << L"array.key1 = " << reader[L"array.key1"];
-
-		// get table array
-		Print << L"products = {";
-		for (auto&& t : reader[L"products"].tableArrayView())
-		{
-			Print << L"\t{";
-			for (auto&& e : t.tableView())
-			{
-				Print << L"\t\t" << e.value << L",";
-			}
-			Print << L"\t},";
-		}
-		Print << L"}";
+	while (System::Update()) {
 	}
 }
