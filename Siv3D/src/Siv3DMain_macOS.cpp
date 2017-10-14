bafba0c2dbf8c2b14cc9626ab8d8b64731743c7b
Reputeless <reputeless@gmail.com>
2017-10-14 21:46:34 +0300

    プロジェクト設定更新

---
+++ Siv3D/src/Siv3DMain_macOS.cpp
@@ -0,0 +1,17 @@
+//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2017 Ryo Suzuki
+//	Copyright (c) 2016-2017 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+void Main();
+
+int main()
+{
+	Main();
+}
