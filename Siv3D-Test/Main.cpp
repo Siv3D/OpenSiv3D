d922379a36b0f22bde45e1371bddc03c25d4c5ef
Reputeless <Reputeless@users.noreply.github.com>
2017-11-14 22:02:41 +0200

    fix

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -6,23 +6,5 @@ void Main()
 {
 	//Siv3D_RunTest();
 
-	INIData ini;
 
-	ini[U"Month.January"] = 1;
-	ini[U"Month.March"] = 3;
-	ini[U"Month.May"] = 5;
-	ini[U"Month.July"] = 7;
-
-	ini[U"Monday"] = 11;
-	ini[U"Tuesday"] = 12;
-	ini[U"Wednesday"] = 13;
-	ini[U"Thursdat"] = 14;
-	ini[U"Friday"] = 15;
-	ini[U"Saturday"] = 16;
-	ini[U"A.bbb.ccc"] = 100;
-
-	ini.save(U"test.ini");
-
-	INIData(U"test.ini").save(U"test2.ini");
-	//*/
 }
