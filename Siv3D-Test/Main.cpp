9fecf5f561d9dbf7dfe4adf89f5db5d60788333b
Reputeless <Reputeless@users.noreply.github.com>
2018-02-10 08:39:14 +0900

    Point::paint / overwrite

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -1,71 +1,20 @@
 ﻿# include <Siv3D.hpp>
-# include <HamFramework.hpp>
 
 void Main()
 {
-	Graphics::SetBackground(ColorF(0.9));
-	Window::Resize(1280, 720);
-	Effect effect;
+	Image image(256, 256, Palette::Black);
 
-	Vec2 left(640 - 100, 100), right(640 + 100, 100);
-	double angle = 0_deg;
-	double scale = 400.0;
-	bool covered = true;
+	DynamicTexture texture(image);
 
 	while (System::Update())
 	{
-		scale += Mouse::Wheel() * -5;
-		angle += 1_deg * MouseR.pressed() -1_deg * MouseL.pressed();
-		if (KeyC.down())
-			covered = !covered;
-
-		Circle(Vec2(640 - 300, 450), scale / 2).drawFrame(scale * 0.1);
-		Circle(Vec2(640 + 300, 450), scale / 2).drawFrame(scale * 0.1);
-
-		for (size_t i = 0; i < Gamepad.MaxUserCount; ++i)
+		if (MouseL.pressed())
 		{
-			if (JoyCon::IsJoyConL(Gamepad(i)))
-			{
-				const auto joy = JoyCon(Gamepad(i));
+			Cursor::Pos().paint(image, ColorF(1.0, 0.5));
 
-				JoyConSymbol::DrawLAt(joy, Vec2(640-300, 450), scale, -90_deg - angle, covered);
-
-				if (auto d = joy.povD8())
-				{
-					left += Circular(4, *d * 45_deg);
-				}
-
-				if (joy.button2.down())
-				{
-					effect.add([center = left](double t) {
-						Circle(center, 20 + t * 200).drawFrame(10, 0, AlphaF(1.0 - t));
-						return t < 1.0;
-					});
-				}
-			}
-			else if (JoyCon::IsJoyConR(Gamepad(i)))
-			{
-				const auto joy = JoyCon(Gamepad(i));
-
-				JoyConSymbol::DrawRAt(joy, Vec2(640 + 300, 450), scale, 90_deg + angle, covered);
-
-				if (auto d = joy.povD8())
-				{
-					right += Circular(4, *d * 45_deg);
-				}
-
-				if (joy.button2.down())
-				{
-					effect.add([center = right](double t) {
-						Circle(center, 20 + t * 200).drawFrame(10, 0, AlphaF(1.0 - t));
-						return t < 1.0;
-					});
-				}
-			}
+			texture.fill(image);
 		}
 
-		Circle(left, 30).draw(ColorF(0.0, 0.75, 0.9));
-		Circle(right, 30).draw(ColorF(1.0, 0.4, 0.3));
-		effect.update();
+		texture.draw();
 	}
 }
---
+++ Siv3D/src/Siv3D/Painting/SivPainting.cpp
@@ -0,0 +1,47 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2018 Ryo Suzuki
+//	Copyright (c) 2016-2018 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/Image.hpp>
+# include <Siv3D/PointVector.hpp>
+
+namespace s3d
+{
+	const Point& Point::paint(Image& dst, const Color& color) const
+	{
+		if (x < 0 || dst.width() <= x || y < 0 || dst.height() <= y)
+		{
+			return *this;
+		}
+
+		const uint32 srcBlend = color.a;
+		const uint32 dstBlend = 255 - srcBlend;
+
+		Color* const pDst = dst.data() + y * dst.width() + x;
+		pDst->r = ((pDst->r * dstBlend) + (color.r * srcBlend)) / 255;
+		pDst->g = ((pDst->g * dstBlend) + (color.g * srcBlend)) / 255;
+		pDst->b = ((pDst->b * dstBlend) + (color.b * srcBlend)) / 255;
+
+		return *this;
+	}
+
+	const Point& Point::overwrite(Image& dst, const Color& color) const
+	{
+		if (x < 0 || dst.width() <= x || y < 0 || dst.height() <= y)
+		{
+			return *this;
+		}
+
+		Color* const pDst = dst.data() + y * dst.width() + x;
+		*pDst = color;
+
+		return *this;
+	}
+}
