2ef85a1b773aa75371d5a89f73a1dccd9983fcef
Reputeless <Reputeless@users.noreply.github.com>
2018-02-11 12:31:44 +0900

    Periodic

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -2,8 +2,19 @@
 
 void Main()
 {
+	Grid<double> grid(16, 12);
+
+	for (auto& e : grid)
+	{
+		e = Random(0.4, 1.8);
+	}
+
 	while (System::Update())
 	{
-
+		for (auto p : step(grid.size()))
+		{
+			Circle(p * 40, 15).movedBy(20, 20)
+				.draw(ColorF(0.0, 1.0, 0.0, Periodic::Sine0_1(1s * grid[p])));
+		}
 	}
 }
