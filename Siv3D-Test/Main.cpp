561a779ec87189494655cad87ba096581167d15b
Reputeless <Reputeless@users.noreply.github.com>
2018-02-24 23:08:27 +0900

    update

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -1,17 +1,8 @@
-# include <Siv3D.hpp>
+﻿# include <Siv3D.hpp>
 # include <HamFramework.hpp>
 
 void Main()
 {
-	Duration d = 35ms;
-
-	Print << 10s;
-
-	Print << d;
-
-	for(auto i : step(10))
-		Print << Random(300.0ms, 500.0ms);
-
 	while (System::Update())
 	{
 
