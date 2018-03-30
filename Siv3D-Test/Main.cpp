e9aa657307cc7816a32ba5522152c84838ce2b69
Reputeless <Reputeless@users.noreply.github.com>
2018-03-30 20:22:33 +0900

    P2Body::drawFrame()

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -1,24 +1,51 @@
-﻿
-# include <Siv3D.hpp> // OpenSiv3D v0.2.3
+﻿# include <Siv3D.hpp>
+# include <HamFramework.hpp>
 
 void Main()
 {
-	const String def(U"abc");
+	P2World world;
+	const P2Body line = world.createLine(Vec2(0, 0), Line(-12, 0, 12, 0), P2Material(1, 0.1, 1.0), none, P2BodyType::Static);
+	Array<P2Body> bodies;
 
-	Optional<String> s;
+	Array<Polygon> polys;
+	Array<Texture> textures;
+	HashTable<P2BodyID, size_t> table;
 
-	Print << s.value_or(def);
+	for (const auto& animal : { U"🐘", U"🐧", U"🐐", U"🐤" })
+	{
+		polys << ImageProcessing::FindExternalContour(Emoji::LoadImage(animal), true)
+			.moveBy(-Emoji::ImageSize / 2).simplified(0.8).scale(0.04);
 
-	INIData ini(U"example/test.ini");
+		textures << Texture(Emoji(animal), TextureDesc::Mipped);
+	}
 
-	Print << ini.getOr<String>(U"Window.Title", def);
-
-	CSVData csv(U"example/test.csv");
-
-	Print << csv.getOr<String>(0, 0, def);
+	Camera2D camera(Vec2(0, -8), 20);
+	size_t index = 0;
 
 	while (System::Update())
 	{
-		
+		world.update();
+		camera.update();
+		auto t = camera.createTransformer();
+
+		if (MouseL.down())
+		{
+			bodies << world.createPolygon(Cursor::PosF(), polys[index], P2Material(0.1, 0.0, 1.0));
+			table.emplace(bodies.back().id(), std::exchange(index, Random(polys.size() - 1)));
+		}
+
+		for (const auto& body : bodies)
+		{
+			//body.draw();
+
+			body.drawWireframe(1);
+
+			//textures[table[body.id()]].scaled(0.04).rotated(body.getAngle()).drawAt(body.getPos());
+		}
+
+		line.draw(Palette::Green);
+		textures[index].scaled(0.04).drawAt(Cursor::PosF());
+
+		camera.draw(Palette::Orange);
 	}
 }
