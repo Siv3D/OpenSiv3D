25819ca9e815d0e29e512e97c8439e929514935a
Reputeless <Reputeless@users.noreply.github.com>
2018-04-08 16:49:21 +0900

    xml 準備

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -3,29 +3,8 @@
 
 void Main()
 {
-	Polygon polygon(Shape2D::NStar(10, 100, 80, Window::Center()));
-
-	Serializer<MemoryWriter> w;
-
-	w(polygon);
-
-	Deserializer<ByteArray> b(w.getWriter().data(), w.getWriter().size());
-
-	Polygon polygon2;
-
-	b(polygon2);
-
-	Print << (polygon.boundingRect() == polygon2.boundingRect());
-
 	while (System::Update())
 	{
-		if (MouseL.pressed())
-		{
-			polygon.draw();
-		}
-		else
-		{
-			polygon2.draw();
-		}
+
 	}
 }
