01be3b62faeabca0807703f327175bd8d999f9a4
Reputeless <Reputeless@users.noreply.github.com>
2018-04-21 18:53:59 +0900

    update

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -1,14 +1,18 @@
 ﻿
 # include <Siv3D.hpp>
+# include <HamFramework.hpp>
 
 void Main()
 {
-	TCPServer server;
-
-	server.start(3001);
-
+	int32 n = 0;
+	
 	while (System::Update())
 	{
-
+		if (MouseL.down())
+		{
+			n = Random(-99999, 99999);
+		}
+		
+		SevenSegment::DrawText(Format(n).lpad(6), Vec2(20, 20), 70, 8, 4, 10, ColorF(0, 1, 0), ColorF(0.25));
 	}
 }
