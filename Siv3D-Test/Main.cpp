32f3f4f7a02db38abef361e57fa242c833c3a813
Reputeless <Reputeless@users.noreply.github.com>
2018-05-28 12:48:05 +0900

    QR 準備

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -3,42 +3,8 @@
 
 void Main()
 {
-	for (const auto& info : System::EnumerateWebcams())
-	{
-		Print << U"--";
-		Print << info.index;
-		Print << info.name;
-		Print << info.uniqueName;
-	}
-
-	Webcam webcam(0);
-
-	if (!webcam)
-	{
-		return;
-	}
-
-	Print << webcam.getResolution();
-
-	//if (webcam.setResolution(Size(1280, 720)))
-	//{
-	//	Print << webcam.getResolution();
-	//}
-
-	webcam.start();
-
-	DynamicTexture texture;
-
 	while (System::Update())
 	{
-		if (webcam.hasNewFrame())
-		{
-			webcam.getFrame(texture);
-		}
 
-		if (texture)
-		{
-			texture.draw();
-		}
 	}
 }
