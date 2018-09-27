753c35980942d28773ea8990596e5c80e5ba4916
Reputeless <Reputeless@users.noreply.github.com>
2018-09-27 22:16:14 +0900

    v0.3.0 リリース準備

--- Siv3D-Test/Main.cpp
+++ Siv3D-Test/Main.cpp
@@ -1,10 +1,22 @@
 ﻿
-# include <Siv3D.hpp>
+# include <Siv3D.hpp> // OpenSiv3D v0.3.0
 
 void Main()
 {
+	Graphics::SetBackground(ColorF(0.8, 0.9, 1.0));
+
+	const Font font(60);
+
+	const Texture textureCat(Emoji(U"🐈"), TextureDesc::Mipped);
+
 	while (System::Update())
 	{
+		font(U"Hello, Siv3D!🐣").drawAt(Window::Center(), Palette::Black);
 
+		font(Cursor::Pos()).draw(20, 500, ColorF(0.6));
+
+		textureCat.resized(80).draw(700, 500);
+
+		Circle(Cursor::Pos(), 60).draw(ColorF(1, 0, 0, 0.5));
 	}
 }
