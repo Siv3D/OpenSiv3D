d32beabd25034373d89be51e0d6e2594e7290c23
Ryo Suzuki <reputeless+github@gmail.com>
2019-05-24 20:41:13 +0900

    update Main.cpp

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -1,34 +1,43 @@
 
-# include <Siv3D.hpp>
+# include <Siv3D.hpp> // OpenSiv3D v0.4.0 beta
 
 void Main()
 {
+	// Set background color to sky blue
 	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
 	
+	// Create a new font
 	const Font font(60);
 	
-	const Texture emoji(Emoji(U"🐈"));
+	// Create a new texture that contains a cat emoji
+	const Texture cat(Emoji(U"🐈"));
 	
-	Vec2 emojiPos(650, 450);
+	// Coordinates of the cat
+	Vec2 catPos(640, 450);
 	
 	while (System::Update())
 	{
+		// Put a message in the middle of the screen
 		font(U"Hello, Siv3D!🐣").drawAt(Scene::Center(), Palette::Black);
 		
-		font(Cursor::Pos()).draw(20, 500, ColorF(0.6));
+		// Display the texture with animated size
+		cat.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(catPos);
 		
-		emoji.resized(80).drawAt(emojiPos);
-		
+		// Draw a translucent red circle that follows the mouse cursor
 		Circle(Cursor::Pos(), 40).draw(ColorF(1, 0, 0, 0.5));
 		
+		// When [A] key is down
 		if (KeyA.down())
 		{
+			// Print `Hello!`
 			Print << U"Hello!";
 		}
 		
+		// When [Move the cat] button is pushed
 		if (SimpleGUI::Button(U"Move the cat", Vec2(600, 20)))
 		{
-			emojiPos = RandomVec2(Scene::Rect());
+			// Move the cat's coordinates to a random position in the screen
+			catPos = RandomVec2(Scene::Rect());
 		}
 	}
 }
