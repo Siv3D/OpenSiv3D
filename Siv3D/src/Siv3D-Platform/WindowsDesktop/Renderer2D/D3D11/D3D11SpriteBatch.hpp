c72affbead61915e0923348b8d635421627938e3
Ryo Suzuki <reputeless+github@gmail.com>
2019-07-20 00:42:04 +0900

    fix VertexBufferSize

--- Siv3D/src/Siv3D-Platform/WindowsDesktop/Renderer2D/D3D11/D3D11SpriteBatch.hpp
+++ Siv3D/src/Siv3D-Platform/WindowsDesktop/Renderer2D/D3D11/D3D11SpriteBatch.hpp
@@ -63,7 +63,7 @@ namespace s3d
 		static constexpr uint32 MaxVertexArraySize		= 65536 * 64; // 4,194,304
 		static constexpr uint32 MaxIndexArraySize		= 65536 * 64; // 4,194,304
 
-		static constexpr uint32 VertexBufferSize		= 65536;// 65536;
+		static constexpr uint32 VertexBufferSize		= 65535;// 65535;
 		static constexpr uint32 IndexBufferSize			= VertexBufferSize * 4; // 524,288
 
 	public:
