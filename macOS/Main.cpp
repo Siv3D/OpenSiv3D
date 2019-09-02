15569430264d48aee21348d585aa708d0c43e879
Ryo Suzuki <reputeless+github@gmail.com>
2019-09-03 00:05:57 +0900

    #330 対応確認
    mac, Linux は対応済み

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -1,10 +1,20 @@
 
-# include <Siv3D.hpp> // OpenSiv3D v0.4.1
+# include <Siv3D.hpp> // OpenSiv3D v0.4.1 pre
 
 void Main()
 {
+	Scene::Resize(256, 256);
+	
+	//Window::Resize(256, 256);
+	Scene::SetTextureFilter(TextureFilter::Nearest);
+	
 	while (System::Update())
 	{
-
+		Circle(128, 128, 60).draw();
+		
+		if (KeyS.down())
+		{
+			ScreenCapture::SaveCurrentFrame();
+		}
 	}
 }
