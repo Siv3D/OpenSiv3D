9102d04e8eab061a8ee27448d8b87a1920e50f3c
Ryo Suzuki <reputeless+github@gmail.com>
2019-09-13 20:33:17 +0900

    fix #357

--- WindowsDesktop/Main.cpp
+++ WindowsDesktop/Main.cpp
@@ -2,21 +2,42 @@
 
 void Main()
 {
-	MSRenderTexture rt(100, 100, ColorF(0.0, 1.0));
-	Image image;
+	const Array<Polygon> polygons =
+	{
+		Shape2D::Star(280, Vec2(400, 300)),
+		Rect(Arg::center(400,300), 200).asPolygon(),
+		Circle(400, 300, 280).asPolygon()
+	};
+
+	size_t polygonIndex = 0;
 
 	while (System::Update())
 	{
 		if (MouseL.down())
 		{
-			{
-				ScopedRenderTarget2D target(rt);
-				Circle(50, 50, 40).draw();
-			}
-			Graphics2D::Flush();
-			rt.resolve();
-			rt.readAsImage(image);
-			image.save(U"s.png");
+			++polygonIndex %= polygons.size();
 		}
+
+		const Rect rect(Arg::center = Cursor::Pos(), 120, 60);
+
+		ScopedRenderStates2D rast(RasterizerState::SolidCullBack);
+		{
+			const Array<Polygon> results = Geometry2D::Subtract(polygons[polygonIndex], rect.asPolygon());
+
+			for (auto[i, result] : Indexed(results))
+			{
+				result.draw(HSV(i * 60, 0.5));
+
+				for (const auto& hole : result.inners())
+				{
+					for (size_t i : step(hole.size() - 1))
+					{
+						Line(hole[i], hole[i + 1]).drawArrow();
+					}
+				}
+			}
+		}
+
+		//polygon.draw();
 	}
 }
