c44e8dfeff17a93ce371ebf2e99a3da3367c1ced
Ryo Suzuki <reputeless+github@gmail.com>
2019-09-20 20:32:23 +0900

    リファクタリング

--- WindowsDesktop/Main.cpp
+++ WindowsDesktop/Main.cpp
@@ -2,24 +2,8 @@
 
 void Main()
 {
-	Grid<float> grid(40, 40, 1.0f);
-
-	RenderTexture rt(grid);
-
 	while (System::Update())
 	{
-		if (MouseL.down())
-		{
-			ScopedRenderTarget2D target(rt);
-			ScopedRenderStates2D blend(BlendState::Additive);
 
-			Circle(Cursor::Pos(), 20).draw(ColorF(0.5));
-		}
-
-		if (MouseR.down())
-		{
-			rt.read(grid);
-			Logger << grid;
-		}
 	}
 }
