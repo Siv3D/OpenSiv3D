58cccd18ea78511feedf1291bd45803cb423138b
Ryo Suzuki <reputeless+github@gmail.com>
2019-09-23 15:22:43 +0900

    remove u8 literals
    #366

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -3,13 +3,30 @@
 
 void Main()
 {
-
 	EmojiList emojis(Resource(U"engine/emoji/noto7_dictionary.dat"));
 	
-	Print << emojis.getList().size();
+	Print << emojis.size();
+	
+	for (const auto& emoji : emojis)
+	{
+		Logger << emoji;
+	}
+	
+	const size_t emojiCount = emojis.size();
+	
+	size_t index = 0;
+	
+	DynamicTexture dtex(Emoji::CreateImage(emojis[index]));
 	
 	while (System::Update())
 	{
-
+		if (MouseL.down())
+		{
+			++index %= emojiCount;
+			
+			dtex.fill(Emoji::CreateImage(emojis[index]));
+		}
+		
+		dtex.draw();
 	}
 }
