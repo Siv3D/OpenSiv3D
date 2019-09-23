9b957f0ee0157274701fc595a78582b96feae831
Ryo Suzuki <reputeless+github@gmail.com>
2019-09-23 21:25:41 +0900

    サンプルシェーダ追加・整理 (Windows)

--- WindowsDesktop/Main.cpp
+++ WindowsDesktop/Main.cpp
@@ -3,30 +3,8 @@
 
 void Main()
 {
-	EmojiList emojis(Resource(U"engine/emoji/noto7_dictionary.dat"));
-
-	Print << emojis.size();
-
-	for (const auto& emoji : emojis)
-	{
-		Logger << emoji;
-	}
-
-	const size_t emojiCount = emojis.size();
-
-	size_t index = 0;
-
-	DynamicTexture dtex(Emoji::CreateImage(emojis[index]));
-
 	while (System::Update())
 	{
-		if (MouseL.down())
-		{
-			++index %= emojiCount;
 
-			dtex.fill(Emoji::CreateImage(emojis[index]));
-		}
-
-		dtex.draw();
 	}
 }
