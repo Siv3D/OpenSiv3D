783fef81981575d80e6079858db46daa73b61612
Ryo Suzuki <reputeless+github@gmail.com>
2019-09-24 23:22:59 +0900

    mac 用の Main コメント

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -41,3 +41,25 @@ void Main()
 		}
 	}
 }
+
+//
+// = お役立ちリンク =
+//
+// OpenSiv3D リファレンス
+// https://siv3d.github.io/ja-jp/
+//
+// チュートリアル
+// https://siv3d.github.io/ja-jp/tutorial/basic/
+//
+// よくある間違い
+// https://siv3d.github.io/ja-jp/articles/mistakes/
+//
+// サポートについて
+// https://siv3d.github.io/ja-jp/support/support/
+//
+// Siv3D Slack (ユーザコミュニティ) への参加
+// https://siv3d.github.io/ja-jp/community/community/
+//
+// 新機能の提案やバグの報告
+// https://github.com/Siv3D/OpenSiv3D/issues
+//
