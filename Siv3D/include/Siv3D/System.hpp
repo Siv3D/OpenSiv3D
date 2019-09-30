2e3ca87d15e2fc2c827cdba9bd986b093b678c36
Ryo Suzuki <reputeless+github@gmail.com>
2019-09-30 14:45:21 +0900

    ドキュメント追加

--- Siv3D/include/Siv3D/System.hpp
+++ Siv3D/include/Siv3D/System.hpp
@@ -18,37 +18,32 @@ namespace s3d
 	namespace System
 	{
 		/// <summary>
-		/// 
-		/// 
+		/// 描画や入力情報など、フレームを更新します。
 		/// </summary>
 		[[nodiscard]] bool Update();
 
 		/// <summary>
-		/// 
-		/// 
+		/// プログラムを終了するために、この直後の `System::Update()` が `false` を返すように設定します。
 		/// </summary>
 		void Exit();
 
 		/// <summary>
-		/// 
-		/// 
+		/// アプリケーション終了トリガーに設定するユーザアクションを設定します。
 		/// </summary>
 		void SetTerminationTriggers(uint32 userActionFlags);
 
 		/// <summary>
-		/// 
-		/// 
+		/// アプリケーション終了トリガーに設定したユーザアクションのフラグの現在の設定を返します。
 		/// </summary>
 		[[nodiscard]] uint32 GetTerminationTriggers();
 
 		/// <summary>
-		/// 
-		/// 
+		/// 前回のフレームで発生したユーザアクションのフラグを返します。
 		/// </summary>
 		[[nodiscard]] uint32 GetUserActions();
 
 		/// <summary>
-		/// 指定したミリ秒だけ処理を停止します。
+		/// 現在のスレッドの実行を指定した時間だけ停止します。
 		/// </summary>
 		/// <param name="milliseconds">
 		/// 処理を停止する時間（ミリ秒）
@@ -59,7 +54,7 @@ namespace s3d
 		void Sleep(int32 milliseconds);
 
 		/// <summary>
-		/// 指定したミリ秒だけ処理を停止します。
+		/// 現在のスレッドの実行を指定した時間だけ停止します。
 		/// </summary>
 		/// <param name="duration">
 		/// 処理を停止する時間
@@ -69,6 +64,9 @@ namespace s3d
 		/// </returns>
 		void Sleep(const Duration& duration);
 
+		/// <summary>
+		/// 指定した URL をデフォルトの Web ブラウザでオープンします。
+		/// </summary>
 		bool LaunchBrowser(const FilePath& url);
 	}
 }
