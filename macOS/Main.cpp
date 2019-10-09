006d07313ee09fb67ff4c6a8ef4c52097b4dddd7
Ryo Suzuki <reputeless+github@gmail.com>
2019-10-10 00:25:25 +0900

    FIleSystem 更新作業 (macOS)
    #390

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -2,24 +2,32 @@
 
 void Main()
 {
-	Print << U"{:#x}"_fmt(uint16(0x1122));
-	Print << U"{:#x}"_fmt(uint32(0x11223344));
-	Print << U"{:#x}"_fmt(uint64(0x1122334455667788));
+	Logger << U"Module: " << FileSystem::ModulePath();
+	Logger << U"Initial: " << FileSystem::InitialDirectory();
+	Logger << U"Current: " << FileSystem::CurrentDirectory();
+	
+	Logger << U"FullPath1: " << FileSystem::FullPath(U"../App/example/");
+	Logger << U"FullPath2: " << FileSystem::FullPath(U"../App/example/windmill.png");
+	Logger << U"Parent1: " << FileSystem::ParentPath(U"./");
+	Logger << U"Parent2: " << FileSystem::ParentPath(U"example/windmill.png");
+	Logger << U"Relative1: " << FileSystem::RelativePath(FileSystem::FullPath(U"../App/example/"));
+	Logger << U"Relative2: " << FileSystem::RelativePath(FileSystem::FullPath(U"../App/example/windmill.png"));
 
-	Print << U"{:#x}"_fmt(SwapEndian(uint16(0x1122)));
-	Print << U"{:#x}"_fmt(SwapEndian(uint32(0x11223344)));
-	Print << U"{:#x}"_fmt(SwapEndian(uint64(0x1122334455667788)));
-
-	Print << U"{:#x}"_fmt(uint16(0xFFEE));
-	Print << U"{:#x}"_fmt(uint32(0xFFEEDDCC));
-	Print << U"{:#x}"_fmt(uint64(0xFFEEDDCCBBAA9988));
-
-	Print << U"{:#x}"_fmt(SwapEndian(uint16(0xFFEE)));
-	Print << U"{:#x}"_fmt(SwapEndian(uint32(0xFFEEDDCC)));
-	Print << U"{:#x}"_fmt(SwapEndian(uint64(0xFFEEDDCCBBAA9988)));
-
+	Print << U"Module: " << FileSystem::ModulePath();
+	Print << U"Initial: " << FileSystem::InitialDirectory();
+	Print << U"Current: " << FileSystem::CurrentDirectory();
+	
+	Print << U"FullPath1: " << FileSystem::FullPath(U"../App/example/");
+	Print << U"FullPath2: " << FileSystem::FullPath(U"../App/example/windmill.png");
+	Print << U"Parent1: " << FileSystem::ParentPath(U"./");
+	Print << U"Parent2: " << FileSystem::ParentPath(U"example/windmill.png");
+	Print << U"Relative1: " << FileSystem::RelativePath(FileSystem::FullPath(U"../App/example/"));
+	Print << U"Relative2: " << FileSystem::RelativePath(FileSystem::FullPath(U"../App/example/windmill.png"));
+	
+	//const Texture texture(U"example/windmill.png");
+	
     while (System::Update())
     {
-		
+		//texture.draw();
     }
 }
