f04720059d998802cfbc584c76449a9deaa7559b
Ryo Suzuki <reputeless+github@gmail.com>
2019-10-22 00:52:55 +0900

    Point::manhattanDistanceFrom()

--- WindowsDesktop/Main.cpp
+++ WindowsDesktop/Main.cpp
@@ -3,22 +3,6 @@
 
 void Main()
 {
-	Print << U"{:#x}"_fmt(uint16(0x1122));
-	Print << U"{:#x}"_fmt(uint32(0x11223344));
-	Print << U"{:#x}"_fmt(uint64(0x1122334455667788));
-
-	Print << U"{:#x}"_fmt(SwapEndian(uint16(0x1122)));
-	Print << U"{:#x}"_fmt(SwapEndian(uint32(0x11223344)));
-	Print << U"{:#x}"_fmt(SwapEndian(uint64(0x1122334455667788)));
-
-	Print << U"{:#x}"_fmt(uint16(0xFFEE));
-	Print << U"{:#x}"_fmt(uint32(0xFFEEDDCC));
-	Print << U"{:#x}"_fmt(uint64(0xFFEEDDCCBBAA9988));
-
-	Print << U"{:#x}"_fmt(SwapEndian(uint16(0xFFEE)));
-	Print << U"{:#x}"_fmt(SwapEndian(uint32(0xFFEEDDCC)));
-	Print << U"{:#x}"_fmt(SwapEndian(uint64(0xFFEEDDCCBBAA9988)));
-
 	while (System::Update())
 	{
 
