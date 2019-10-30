1cbd9c1ab42761ad29f68aef7c0c9b76b110c28d
Ryo Suzuki <reputeless+github@gmail.com>
2019-10-30 15:46:48 +0900

    par_shape 追加
    #412

--- Siv3D/src/Siv3D/Sphere/SivSphere.cpp
+++ Siv3D/src/Siv3D/Sphere/SivSphere.cpp
@@ -9,12 +9,28 @@
 //
 //-----------------------------------------------
 
+# include <Siv3D/Platform.hpp>
+
+SIV3D_DISABLE_MSVC_WARNINGS_PUSH(4100)
+SIV3D_DISABLE_MSVC_WARNINGS_PUSH(4244)
+SIV3D_DISABLE_MSVC_WARNINGS_PUSH(4305)
+SIV3D_DISABLE_MSVC_WARNINGS_PUSH(4456)
+# define PAR_SHAPES_IMPLEMENTATION
+# define _CRT_SECURE_NO_WARNINGS
+# include <par_shapes/par_shapes.h>
+# undef _CRT_SECURE_NO_WARNINGS
+SIV3D_DISABLE_MSVC_WARNINGS_POP()
+SIV3D_DISABLE_MSVC_WARNINGS_POP()
+SIV3D_DISABLE_MSVC_WARNINGS_POP()
+SIV3D_DISABLE_MSVC_WARNINGS_POP()
+
 # include <Siv3D/Sphere.hpp>
 # include <Siv3D/Mat4x4.hpp>
 # include <Siv3D/Graphics2D.hpp>
 # include <Siv3D/Vertex2D.hpp>
 # include <Siv3DEngine.hpp>
 # include <Renderer2D/IRenderer2D.hpp>
+
 
 namespace s3d
 {
