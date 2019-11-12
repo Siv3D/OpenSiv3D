26bea4dbd05276dd73e29a6b61749a25b7747edc
Ryo Suzuki <reputeless+github@gmail.com>
2019-11-12 20:40:41 +0900

    update RtAudio
    #417

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -1,11 +1,36 @@
-# include <Siv3D.hpp> // OpenSiv3D v0.4.2
+# include <Siv3D.hpp>
 
 void Main()
 {
-	while (System::Update())
-	{
-		//Circle(Scene::Center(), 400).draw(Palette::Yellow, Palette::Black);
+    // マイクをセットアップ
+    Microphone mic(0);
 
-		Ellipse(Scene::Center(), 800, 600).draw(ColorF(1.0), ColorF(0.0));
-	}
+    if (!mic)
+    {
+        // マイクを利用できない場合、終了
+        throw Error(U"Microphone not available");
+    }
+
+    // 録音をスタート
+    mic.start();
+
+    FFTResult fft;
+
+    while (System::Update())
+    {
+        // FFT の結果を取得
+        mic.fft(fft);
+
+        // 結果を可視化
+        for (auto i : step(800))
+        {
+            const double size = Pow(fft.buffer[i], 0.6f) * 1200;
+            RectF(Arg::bottomLeft(i, 600), 1, size).draw(HSV(240 - i));
+        }
+
+        // 周波数表示
+        Rect(Cursor::Pos().x, 0, 1, Scene::Height()).draw();
+        ClearPrint();
+        Print << U"{} Hz"_fmt(Cursor::Pos().x * fft.resolution);
+    }
 }
