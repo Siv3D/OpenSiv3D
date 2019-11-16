e5f4ebcd87d438715063d1a967de47e0c6c0b4ef
Ryo Suzuki <reputeless+github@gmail.com>
2019-11-16 16:05:40 +0900

    v0.4.2 beta 準備

--- Siv3D/include/Siv3D/Version.hpp
+++ Siv3D/include/Siv3D/Version.hpp
@@ -16,7 +16,7 @@
 /// ライブラリのバージョン表示
 /// Version text
 /// </summary>
-# define SIV3D_VERSION	U"0.4.2dev"
+# define SIV3D_VERSION	U"0.4.2beta"
 
 namespace s3d
 {
@@ -24,5 +24,5 @@ namespace s3d
 	/// バージョン ID
 	/// Version value
 	/// </summary>
-	inline constexpr uint32 Siv3DVersion = 200'004'020u;
+	inline constexpr uint32 Siv3DVersion = 200'004'021u;
 }
--- WindowsDesktop/Main.cpp
+++ WindowsDesktop/Main.cpp
@@ -1,36 +1,73 @@
 ﻿
-# include <Siv3D.hpp>
+# include <Siv3D.hpp> // OpenSiv3D v0.4.2
 
 void Main()
 {
-	const FilePath tomlPath = FileSystem::FullPath(U"example/gui/gui.toml");
-	const DirectoryWatcher watcher(FileSystem::ParentPath(tomlPath));
-	SimpleGUIManager gui(tomlPath);
+	// 背景を水色にする
+	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
+
+	// 大きさ 60 のフォントを用意
+	const Font font(60);
+
+	// 猫のテクスチャを用意
+	const Texture cat(Emoji(U"🐈"));
+
+	// 猫の座標
+	Vec2 catPos(640, 450);
 
 	while (System::Update())
 	{
-		for (const auto& change : watcher.retrieveChanges())
+		// テキストを画面の中心に描く
+		font(U"Hello, Siv3D!🐣").drawAt(Scene::Center(), Palette::Black);
+
+		// 大きさをアニメーションさせて猫を表示する
+		cat.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(catPos);
+
+		// マウスカーソルに追従する半透明の赤い円を描く
+		Circle(Cursor::Pos(), 40).draw(ColorF(1, 0, 0, 0.5));
+
+		// [A] キーが押されたら
+		if (KeyA.down())
 		{
-			if (change.first == tomlPath && change.second == FileAction::Modified)
-			{
-				ClearPrint();
-				gui.load(tomlPath);
-			}
+			// Hello とデバッグ表示する
+			Print << U"Hello!";
 		}
 
-		gui.draw();
-
-		if (gui.button(U"bt-OK"))
+		// ボタンが押されたら
+		if (SimpleGUI::Button(U"Move the cat", Vec2(600, 20)))
 		{
-			Print << U"OK";
+			// 猫の座標を画面内のランダムな位置に移動する
+			catPos = RandomVec2(Scene::Rect());
 		}
-		else if (gui.button(U"bt-Cancel"))
-		{
-			Print << U"Cancel";
-		}
-
-		Scene::SetBackground(gui.colorPicker(U"cp-Color"));
 	}
-
-	//gui.save(U"save.toml");
 }
+
+//
+// = アドバイス =
+// Debug ビルドではプログラムの最適化がオフになります。
+// 実行速度が遅いと感じた場合は Release ビルドを試しましょう。
+// アプリをリリースするときにも、Release ビルドにするのを忘れないように！
+//
+// 思ったように動作しない場合は「デバッグの開始」でプログラムを実行すると、
+// 出力ウィンドウに詳細なログが表示されるので、エラーの原因を見つけやすくなります。
+//
+// = お役立ちリンク =
+//
+// OpenSiv3D リファレンス
+// https://siv3d.github.io/ja-jp/
+//
+// チュートリアル
+// https://siv3d.github.io/ja-jp/tutorial/basic/
+//
+// よくある間違い
+// https://siv3d.github.io/ja-jp/articles/mistakes/
+//
+// サポートについて
+// https://siv3d.github.io/ja-jp/support/support/
+//
+// Siv3D Slack (ユーザコミュニティ) への参加
+// https://siv3d.github.io/ja-jp/community/community/
+//
+// 新機能の提案やバグの報告
+// https://github.com/Siv3D/OpenSiv3D/issues
+//
