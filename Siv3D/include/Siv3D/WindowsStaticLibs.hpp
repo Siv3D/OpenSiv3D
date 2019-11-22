524d58f4d40e296604eddef03de2668b232892d4
Ryo Suzuki <reputeless+github@gmail.com>
2019-11-23 00:14:08 +0900

    Serial 準備

--- Siv3D/include/Siv3D/WindowsStaticLibs.hpp
+++ Siv3D/include/Siv3D/WindowsStaticLibs.hpp
@@ -50,6 +50,7 @@
 #		pragma comment (lib, "jumanpp/jpp_core_d")
 #		pragma comment (lib, "jumanpp/jumanpp_v2_d")
 #		pragma comment (lib, "boost/libboost_filesystem-vc142-mt-sgd-x64-1_70")
+#		pragma comment (lib, "serial/serial_d")
 
 # else
 
@@ -78,6 +79,7 @@
 #		pragma comment (lib, "jumanpp/jpp_core")
 #		pragma comment (lib, "jumanpp/jumanpp_v2")
 #		pragma comment (lib, "boost/libboost_filesystem-vc142-mt-s-x64-1_70")
+#		pragma comment (lib, "serial/serial")
 
 #	endif
 
