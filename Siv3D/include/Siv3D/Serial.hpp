84847c9f2d4d1bebbbf24886d1565db25e891add
Ryo Suzuki <reputeless+github@gmail.com>
2019-11-23 23:52:45 +0900

    Serial 途中

---
+++ Siv3D/include/Siv3D/Serial.hpp
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2019 Ryo Suzuki
+//	Copyright (c) 2016-2019 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include "Fwd.hpp"
+# include "Array.hpp"
+
+namespace s3d
+{
+	//class Serial
+	//{
+	//private:
+
+	//	class SerialDetail;
+
+	//	std::shared_ptr<SerialDetail> pImpl;
+
+	//public:
+
+	//	Serial();
+
+	//	explicit Serial(size_t index);
+
+	//	~Serial();
+
+	//	[[nodiscard]] bool isAvailable() const;
+
+	//	[[nodiscard]] explicit operator bool() const;
+	//};
+
+	struct SerialPortInfo
+	{
+		String port;
+
+		String description;
+
+		String hardwareID;
+	};
+
+	namespace System
+	{
+		[[nodiscard]] Array<SerialPortInfo> EnumerateSerialPorts();
+	}
+}
---
+++ Siv3D/src/Siv3D/Serial/SivSerial.cpp
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2019 Ryo Suzuki
+//	Copyright (c) 2016-2019 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <serial/serial.h>
+# include <Siv3D/Serial.hpp>
+
+namespace s3d
+{
+	namespace System
+	{
+		Array<SerialPortInfo> EnumerateSerialPorts()
+		{
+			Array<SerialPortInfo> infos;
+
+			for (const auto& p : serial::list_ports())
+			{
+				SerialPortInfo info;
+				info.port = Unicode::Widen(p.port);
+				info.description = Unicode::Widen(p.description);
+				info.hardwareID = Unicode::Widen(p.hardware_id);
+				infos << info;
+			}
+
+			return infos;
+		}
+	}
+}
