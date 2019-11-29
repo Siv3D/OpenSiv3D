49bbf01d2120590fa9679f967d4c12dbdf49aa57
Ryo Suzuki <reputeless+github@gmail.com>
2019-11-29 16:21:38 +0900

    SaturatedLinework

--- Siv3D/include/HamFramework/SaturatedLinework.hpp
+++ Siv3D/include/HamFramework/SaturatedLinework.hpp
@@ -483,3 +483,51 @@ namespace s3d
 		}
 	};
 }
+
+/* example
+
+# include <Siv3D.hpp>
+# include <HamFramework.hpp>
+
+void Main()
+{
+	Scene::SetBackground(ColorF(0.98, 0.96, 0.94));
+	const Texture texture(Emoji(U"🦀"));
+
+	Ellipse target(400, 300, 180, 120);
+	Rect outer = Scene::Rect();
+	double minThickness = 3.0, maxThickness = 10.0;
+	double lineCount = 150;
+	double offsetRange = 60.0;
+
+	SaturatedLinework<Ellipse> linework(target, outer);
+	linework
+		.setThickness(minThickness, maxThickness)
+		.setLineCount(static_cast<size_t>(lineCount))
+		.setOffsetRange(offsetRange);
+
+	while (System::Update())
+	{
+		if (MouseR.down())
+		{
+			target.setCenter(Cursor::Pos());
+			linework.setTargetShape(target);
+		}
+
+		texture.scaled(1.6).drawAt(target.center);
+
+		linework.draw(ColorF(0.1));
+
+		if (SimpleGUI::Slider(U"lineCount", lineCount, 0.0, 400.0, Vec2(20, 20), 150))
+		{
+			linework.setLineCount(static_cast<size_t>(lineCount));
+		}
+
+		if (SimpleGUI::Slider(U"offsetRange", offsetRange, 0.0, 100.0, Vec2(20, 60), 150))
+		{
+			linework.setOffsetRange(offsetRange);
+		}
+	}
+}
+
+*/
