a8353823d35aff6e9f3d0f2e2842643eaab1d5bc
Ryo Suzuki <reputeless+github@gmail.com>
2020-02-12 22:53:43 +0900

    fix #459

--- WindowsDesktop/Main.cpp
+++ WindowsDesktop/Main.cpp
@@ -3,20 +3,12 @@
 
 void Main()
 {
-	RenderTexture r1(200, 200);
-	RenderTexture r2(Size(200, 200));
-	RenderTexture r3(200, 200, Palette::Orange);
-	RenderTexture r4(Size(200, 200), Palette::Orange);
-	RenderTexture r5(200, 200, TextureFormat::R8G8B8A8_Unorm);
-	RenderTexture r6(Size(200, 200), TextureFormat::R8G8B8A8_Unorm);
+	const Polygon poly = RoundRect(0, 0, 8, 4, 1).asPolygon();
 
 	while (System::Update())
 	{
-		r1.draw(0, 0);
-		r2.draw(200, 0);
-		r3.draw(400, 0);
-		r4.draw(600, 0);
-		r5.draw(0, 200);
-		r6.draw(200, 200);
+		Transformer2D tr(Mat3x2::Scale(40));
+
+		poly.draw();
 	}
 }
