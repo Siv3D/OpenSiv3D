16d0e6c117dcafbca7a9bc924e5c407c5f536573
Ryo Suzuki <reputeless+github@gmail.com>
2020-02-14 18:30:38 +0900

    Zlib (macOS)
    #474

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -1,20 +1,59 @@
 
 # include <Siv3D.hpp> // OpenSiv3D v0.4.3
 
+bool Test(int32 min, int32 max)
+{
+	const size_t size = Random(min, max);
+	Array<uint8> input(size);
+	const int32 target = Random(0, 255);
+	for (auto& in : input)
+	{
+		in = static_cast<uint8>(Random(0, target));
+	}
+
+	const auto compressed = Zlib::Compress(input);
+
+	const auto decompressed = Zlib::Decompress(compressed);
+
+	Array<uint8> output(decompressed.size());
+
+	std::memcpy(output.data(), decompressed.data(), output.size_bytes());
+
+	const bool result = (input == output);
+	Logger << U"{} -> {} ({})"_fmt(input.size(), compressed.size(), result);
+
+	return result;
+}
+
+bool TestSmall()
+{
+	return Test(0, 200);
+}
+
+bool TestLarge()
+{
+	return Test(4 * 1024, 256 * 1024);
+}
+
 void Main()
 {
-	Print << FileSystem::FullPath(U"example/");
-	
-	for (const auto& path : FileSystem::DirectoryContents(U"example/"))
-	{
-		Console << path;
-	}
-	
+	int32 n = 0;
+
 	while (System::Update())
 	{
-		if (KeyA.down())
+		if (!TestSmall())
 		{
-			Print << Dialog::SelectFolder();
+			Print << U"Fail S";
+		}
+
+		if (!TestLarge())
+		{
+			Print << U"Fail L";
+		}
+
+		if (++n % 100 == 0)
+		{
+			Print << n;
 		}
 	}
 }
