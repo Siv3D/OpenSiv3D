c0e3392d8dc53f10b4e1c8fd1a275e18d2e95494
Ryo Suzuki <reputeless+github@gmail.com>
2020-02-23 16:23:03 +0900

    zip test file
    #480

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -3,39 +3,29 @@
 
 void Main()
 {
-//	{
-//		const ZIPReader zip(U"zipTestW.zip");
-//		Print << zip.isOpen();
-//		zip.enumPaths().each(Print);
-//		Print << zip.extract(U"siv3d-kun.png", U"arcout/");
-//		Print << zip.extractAll(U"arcout2/");
-//	}
+	const ZIPReader zip(U"example/zip/zip_test.zip");
 
-	//Console << U"---";
-
-	//{
-	//	const ZIPReader zip(Resource(U"arc/example.zip"));
-	//	zip.enumPaths().each(Console);
-	//	Print << zip.extract(U"example/bay.jpg", U"arcout/");
-	//	Print << zip.extractAll(U"arcout2/");
-	//}
-
-	//Console << U"---";
-
+	if (!zip)
 	{
-		//const ZIPReader zip(U"ziptest.zip");
-		//zip.enumPaths().each(Console);
-		//Print << zip.extract(U"ziptest/siv3d-kun.png", U"arcout/");
-		//Print << zip.extractAll(U"arcout2/");
+		return;
 	}
 
-	//const ZIPReader zip(U"ziptest.zip");
-	//const Texture texture1(zip.extractToMemory(U"example/bay.jpg"));
-	//const Texture texture2(zip.extractToMemory(U"ziptest/siv3d-kun.png"));
+	for (const auto& path : zip.enumPaths())
+	{
+		Print << path;
+	}
+
+	const Texture texture1(zip.extractToMemory(U"zip_test/image/windmill.png"));
+	const Texture texture2(zip.extractToMemory(U"zip_test/image/siv3d-kun.png"));
+
+	Print << U"-----";
+	Print << zip.extract(U"zip_test/loremipsum.txt", U"unzipped1/");
+	Print << zip.extract(U"zip_test/image/*", U"unzipped2/");
+	Print << zip.extractAll(U"unzipped3/");
 
 	while (System::Update())
 	{
-		//texture1.draw();
-		//texture2.draw();
+		texture1.draw();
+		texture2.draw();
 	}
 }
