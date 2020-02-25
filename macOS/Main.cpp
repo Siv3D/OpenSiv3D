c9b0365fe78c02ee11459c8b03cfeccdf5780e45
Ryo Suzuki <reputeless+github@gmail.com>
2020-02-25 13:14:36 +0900

    AnimatedGIFReader (mac)
    #481

--- macOS/Main.cpp
+++ macOS/Main.cpp
@@ -1,17 +1,95 @@
 
 # include <Siv3D.hpp> // OpenSiv3D v0.4.3
 
+struct AnimationTexture
+{
+	Array<Texture> textures;
+	
+	Array<int32> delays;
+	
+	int32 duration = 0;
+
+	explicit operator bool() const noexcept
+	{
+		return !textures.isEmpty();
+	}
+
+	Size size() const noexcept
+	{
+		if (!textures)
+		{
+			return Size(0, 0);
+		}
+
+		return textures.front().size();
+	}
+
+	size_t frames() const noexcept
+	{
+		return textures.size();
+	}
+
+	size_t getFrameIndex(int32 timeMillisec) const noexcept
+	{
+		return AnimatedGIFReader::MillisecToIndex(timeMillisec, delays, duration);
+	}
+
+	const Texture& getTexture(int32 timeMillisec) const noexcept
+	{
+		return textures[getFrameIndex(timeMillisec)];
+	}
+};
+
 void Main()
 {
+	AnimationTexture animation;
+	{
+		const AnimatedGIFReader gif(U"example/test.gif");
+
+		if (!gif)
+		{
+			throw Error(U"Failed to open a gif file");
+		}
+
+		Array<Image> images;
+			
+		if (gif.read(images, animation.delays, animation.duration))
+		{
+			animation.textures = images.map([](const Image& i) { return Texture(i); });
+		}
+		else
+		{
+			throw Error(U"Failed to load a gif animation");
+		}
+	}
+
+	Print << U"{}, {} frames ({} ms)"_fmt(animation.size(), animation.frames(), animation.duration);
+
+	const Point pos(10, 90);
+	bool showTiles = false;
+
 	while (System::Update())
 	{
-		Rect(50, 50, 300)
-			.drawFrame(20, Palette::Orange, Palette::White);
+		const int32 timeMillisec = static_cast<int32>(Scene::Time() * 1000);
+		const auto& texture = animation.getTexture(timeMillisec);
 
-		Circle(200, 200, 100)
-			.drawPie(0_deg, 120_deg, Palette::Orange, Palette::White);
+		SimpleGUI::CheckBox(showTiles, U"Show tiles", Vec2(10, 40));
 
-		Circle(400, 200, 100)
-			.drawArc(0_deg, 120_deg, 10, 10, Palette::Orange, Palette::White);
+		if (showTiles)
+		{
+			Rect(pos, texture.size()).draw();
+			{
+				ScopedViewport2D vp(pos, texture.size());
+				for (auto p : step(texture.size() / 10 + Size(1, 1)))
+				{
+					if (IsEven(p.x + p.y))
+					{
+						Rect(p * 10, 10).draw(ColorF(0.8));
+					}
+				}
+			}
+		}
+
+		texture.draw(pos);
 	}
 }
