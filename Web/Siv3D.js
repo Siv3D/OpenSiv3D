af0c95ff94223ce2f61b04dc6408acd0b7c8ebea
nokotan <kamenokonokotan@gmail.com>
2020-11-25 14:45:14 +0900

    [Web] Add Web specific javascript API

---
+++ Web/Siv3D.js
@@ -0,0 +1,35 @@
+mergeInto(LibraryManager.library, {
+    glfwGetKeysSiv3D: function (windowid) {
+        const window = GLFW.WindowFromId(windowid);
+        if (!window) return 0;
+        if (!window.keysBuffer) {
+            window.keysBuffer = Module._malloc(349 /* GLFW_KEY_LAST + 1 */)
+        }
+        Module.HEAPU8.set(window.keys, window.keysBuffer);
+        return window.keysBuffer;
+    },
+    glfwGetKeysSiv3D__sig: "ii",
+
+    glfwGetMonitorInfo_Siv3D: function(handle, displayID, xpos, ypos, w, h) {
+        setValue(displayID, 1, 'i32');
+        setValue(xpos, 0, 'i32');
+        setValue(ypos, 0, 'i32');
+        setValue(w, 0, 'i32');
+        setValue(h, 0, 'i32');
+    },
+    glfwGetMonitorInfo_Siv3D__sig: "viiiiiiiiiii",
+
+    glfwGetMonitorWorkarea: function(handle, wx, wy, ww, wh) {
+        setValue(wx, 0, 'i32');
+        setValue(wy, 0, 'i32');
+        setValue(ww, 1280, 'i32');
+        setValue(wh, 720, 'i32');
+    },
+    glfwGetMonitorWorkarea__sig: "viiiii",
+
+    glfwGetMonitorContentScale: function(handle, xscale, yscale) {
+        setValue(xscale, 1, 'float');
+        setValue(yscale, 1, 'float'); 
+    },
+    glfwGetMonitorContentScale__sig: "viii"
+})
