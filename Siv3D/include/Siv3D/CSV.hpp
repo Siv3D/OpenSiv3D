0dc61dd70fc7d007a7d9bac22c65c69a0fad08d0
Ryo Suzuki <reputeless+github@gmail.com>
2021-01-29 11:25:34 +0900

    [共通] CSV

--- Siv3D/include/Siv3D/CSV.hpp
+++ Siv3D/include/Siv3D/CSV.hpp
@@ -10,3 +10,110 @@
 //-----------------------------------------------
 
 # pragma once
+# include "Common.hpp"
+# include "String.hpp"
+# include "Optional.hpp"
+# include "IReader.hpp"
+# include "TextReader.hpp"
+
+namespace s3d
+{
+	class CSV
+	{
+	public:
+
+		CSV() = default;
+
+		explicit CSV(FilePathView path, StringView separators = U",", StringView quotes = U"\"", StringView escapes = U"\\");
+
+		template <class Reader, std::enable_if_t<std::is_base_of_v<IReader, Reader> && !std::is_lvalue_reference_v<Reader>>* = nullptr>
+		explicit CSV(Reader&& reader, StringView separators = U",", StringView quotes = U"\"", StringView escapes = U"\\");
+
+		explicit CSV(std::unique_ptr<IReader>&& reader, StringView separators = U",", StringView quotes = U"\"", StringView escapes = U"\\");
+
+		bool load(FilePathView path, StringView separators = U",", StringView quotes = U"\"", StringView escapes = U"\\");
+
+		template <class Reader, std::enable_if_t<std::is_base_of_v<IReader, Reader> && !std::is_lvalue_reference_v<Reader>>* = nullptr>
+		bool load(Reader&& reader, StringView separators = U",", StringView quotes = U"\"", StringView escapes = U"\\");
+
+		bool load(std::unique_ptr<IReader>&& reader, StringView separators = U",", StringView quotes = U"\"", StringView escapes = U"\\");
+
+		void clear() noexcept;
+
+		[[nodiscard]]
+		bool isEmpty() const noexcept;
+
+		[[nodiscard]]
+		explicit operator bool() const noexcept;
+
+		[[nodiscard]]
+		size_t rows() const noexcept;
+
+		[[nodiscard]]
+		size_t columns(size_t row) const noexcept;
+
+		template <class Type = String>
+		[[nodiscard]]
+		Type get(size_t row, size_t column) const;
+
+		template <class Type, class U>
+		[[nodiscard]]
+		Type getOr(size_t row, size_t column, U&& defaultValue) const;
+
+		template <class Type>
+		[[nodiscard]]
+		Optional<Type> getOpt(size_t row, size_t column) const;
+
+		[[nodiscard]]
+		const Array<Array<String>>& getData() const;
+
+		[[nodiscard]]
+		const Array<String>& getRow(size_t row) const;
+
+		[[nodiscard]]
+		const Array<String>& operator [](size_t row) const;
+
+		[[nodiscard]]
+		Array<Array<String>>& getData();
+
+		[[nodiscard]]
+		Array<String>& getRow(size_t row);
+
+		[[nodiscard]]
+		Array<String>& operator [](size_t row);
+
+		void write(const String& record);
+
+		template <class Type>
+		void write(const Type& value);
+
+		template <class ... Args>
+		void write(const Args& ... records);
+
+		template <class ... Args>
+		void writeRow(const Args& ... records);
+
+		void newLine();
+
+		bool save(FilePathView path, char32 separator = U',', char32 quote = U'\"', char32 escape = U'\\') const;
+
+	private:
+
+		Array<Array<String>> m_data;
+
+		bool m_onHead = true;
+
+		Optional<String> getItem(size_t row, size_t column) const;
+
+		bool inBounds(size_t row, size_t column) const;
+
+		bool loadFromTextReader(TextReader& reader, StringView separators, StringView quotes, StringView escapes);
+
+		void _write();
+
+		template <class Type, class ... Args>
+		void _write(const Type& record, const Args& ... records);
+	};
+}
+
+# include "detail/CSV.ipp"
--- Siv3D/include/Siv3D/detail/CSV.ipp
+++ Siv3D/include/Siv3D/detail/CSV.ipp
@@ -13,5 +13,81 @@
 
 namespace s3d
 {
+	template <class Reader, std::enable_if_t<std::is_base_of_v<IReader, Reader> && !std::is_lvalue_reference_v<Reader>>*>
+	inline CSV::CSV(Reader&& reader, const StringView separators, const StringView quotes, const StringView escapes)
+	{
+		load(std::move(reader), separators, quotes, escapes);
+	}
 
+	template <class Reader, std::enable_if_t<std::is_base_of_v<IReader, Reader> && !std::is_lvalue_reference_v<Reader>>*>
+	inline bool CSV::load(Reader&& reader, const StringView separators, const StringView quotes, const StringView escapes)
+	{
+		return load(std::make_unique<Reader>(std::move(reader), separators, quotes, escapes));
+	}
+
+	inline CSV::operator bool() const noexcept
+	{
+		return (not isEmpty());
+	}
+
+	template <class Type>
+	inline Type CSV::get(const size_t row, const size_t column) const
+	{
+		if (const auto opt = getOpt<Type>(row, column))
+		{
+			return opt.value();
+		}
+
+		return Type();
+	}
+
+	template <class Type, class U>
+	inline Type CSV::getOr(const size_t row, const size_t column, U&& defaultValue) const
+	{
+		return getOpt<Type>(row, column).value_or(std::forward<U>(defaultValue));
+	}
+
+	template <class Type>
+	inline Optional<Type> CSV::getOpt(const size_t row, const size_t column) const
+	{
+		if (const auto item = getItem(row, column))
+		{
+			return ParseOpt<Type>(item.value());
+		}
+
+		return none;
+	}
+
+	template <class Type>
+	inline void CSV::write(const Type& value)
+	{
+		write(Format(value));
+	}
+
+	template <class ... Args>
+	inline void CSV::write(const Args& ... records)
+	{
+		_write(records...);
+	}
+
+	template <class ... Args>
+	inline void CSV::writeRow(const Args& ... records)
+	{
+		_write(records...);
+
+		newLine();
+	}
+
+	inline void CSV::_write()
+	{
+		return;
+	}
+
+	template <class Type, class ... Args>
+	inline void CSV::_write(const Type& record, const Args& ... records)
+	{
+		write(Format(record));
+
+		return _write(records...);
+	}
 }
