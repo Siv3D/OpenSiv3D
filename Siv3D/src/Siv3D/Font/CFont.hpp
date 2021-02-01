67e8ca01f1ddab7240a0418fc1a0e747c3982744
Ryo Suzuki <reputeless+github@gmail.com>
2021-02-01 22:38:32 +0900

    [macOS] update

--- Siv3D/src/Siv3D/Font/CFont.hpp
+++ Siv3D/src/Siv3D/Font/CFont.hpp
@@ -14,6 +14,14 @@
 # include <Siv3D/Font.hpp>
 # include "IFont.hpp"
 
+extern "C"
+{
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include FT_GLYPH_H
+#include FT_OUTLINE_H
+}
+
 namespace s3d
 {
 	class CFont final : public ISiv3DFont
