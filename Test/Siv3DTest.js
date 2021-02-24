3c6595ff9923fe8a122ce3074842ef1e7be519d2
nokotan <kamenokonokotan@gmail.com>
2021-02-24 21:37:05 +0900

    [Web] Add resources folder into test environment

--- Test/Siv3DTest.js
+++ Test/Siv3DTest.js
@@ -3,5 +3,8 @@ Module.preRun = [
   {
     FS.mkdir('/test');
     FS.mount(NODEFS, { root: '../../Test/test' }, '/test');
+
+    FS.mkdir('/resources');
+    FS.mount(NODEFS, { root: './resources' }, '/resources');
   }
 ]
