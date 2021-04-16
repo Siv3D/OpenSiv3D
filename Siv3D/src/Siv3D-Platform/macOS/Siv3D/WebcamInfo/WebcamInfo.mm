07ea1717f6bcd20443dfc97d206a60e29e7c9c0c
Ryo Suzuki <reputeless+github@gmail.com>
2021-04-16 17:10:36 +0900

    [macOS] Webcam

---
+++ Siv3D/src/Siv3D-Platform/macOS/Siv3D/WebcamInfo/WebcamInfo.mm
@@ -0,0 +1,74 @@
+//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/WebcamInfo.hpp>
+# import <AVFoundation/AVFoundation.h>
+
+namespace s3d
+{
+	struct macOS_WebcamInfo
+	{
+		unsigned cameraIndex;
+		
+		std::string name;
+		
+		std::string uniqueName;
+	};
+
+	std::vector<macOS_WebcamInfo> macOS_EnumerateWebcam()
+	{
+		NSAutoreleasePool* localpool = [[NSAutoreleasePool alloc] init];
+		
+		NSArray* devices = [[AVCaptureDevice devicesWithMediaType: AVMediaTypeVideo]
+							arrayByAddingObjectsFromArray:[AVCaptureDevice devicesWithMediaType:AVMediaTypeMuxed]];
+		
+		std::vector<macOS_WebcamInfo> results;
+		
+		for (unsigned index = 0; index < devices.count; ++index)
+		{
+			AVCaptureDevice* captureDevice = devices[index];
+			
+			if (!captureDevice)
+			{
+				continue;
+			}
+			
+			macOS_WebcamInfo info;
+			info.cameraIndex = index;
+			info.name = [[captureDevice localizedName] UTF8String];
+			info.uniqueName = [[captureDevice uniqueID] UTF8String];
+			
+			results.push_back(info);
+		}
+		
+		[localpool drain];
+		return results;
+	}
+
+	namespace System
+	{
+		Array<WebcamInfo> EnumerateWebcams()
+		{
+			Array<WebcamInfo> results;
+			
+			for (const auto& info : macOS_EnumerateWebcam())
+			{
+				WebcamInfo result;
+				result.cameraIndex = info.cameraIndex;
+				result.name  = Unicode::FromUTF8(info.name);
+				result.uniqueName = Unicode::FromUTF8(info.uniqueName);
+				results << result;
+			}
+			
+			return results;
+		}
+	}
+}
