b6e65a63ebf2ad5b0abd7c65a60117712effbb2a
Reputeless <reputeless@gmail.com>
2021-04-16 17:46:27 +0900

    [Linux] Webcam

---
+++ Siv3D/src/Siv3D-Platform/Linux/Siv3D/WebcamInfo/SivWebcamInfo.cpp
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <Siv3D/WebcamInfo.hpp>
+
+namespace s3d
+{
+	namespace System
+	{
+		Array<WebcamInfo> EnumerateWebcams()
+		{
+			Array<WebcamInfo> results;
+
+			for (uint32 index = 0; index < 8; ++index)
+			{
+				const std::string name = "/dev/video" + std::to_string(index);
+
+				int h = ::open(name.c_str(), O_RDONLY);
+
+				if (h != -1)
+				{
+					::close(h);
+
+					WebcamInfo info;
+					info.cameraIndex = index;
+					info.uniqueName = info.name = Unicode::Widen(name);				
+					results << info;
+				}
+			}
+
+			return results;
+		}
+	}
+}
