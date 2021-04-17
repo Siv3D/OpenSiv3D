b990d4af20178e6dae9c71dea7c0f7208486fa73
Ryo Suzuki <reputeless+github@gmail.com>
2021-04-17 17:55:54 +0900

    [Linux] fix

--- Siv3D/src/Siv3D-Platform/Linux/Siv3D/Window/CWindow.cpp
+++ Siv3D/src/Siv3D-Platform/Linux/Siv3D/Window/CWindow.cpp
@@ -25,7 +25,7 @@ namespace s3d
 	{
 		static void ErrorCallback(const int error, const char* description)
 		{
-			std::cout << "Error: {}. "_fmt(error) << description << '\n';
+			std::cout << U"Error: {}. "_fmt(error) << description << '\n';
 		}
 	}
 
