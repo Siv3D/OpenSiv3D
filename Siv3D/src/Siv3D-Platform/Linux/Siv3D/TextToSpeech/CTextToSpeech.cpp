136f503991236754cc4ce2c7ac514aa48ff37cb3
Ryo Suzuki <reputeless+github@gmail.com>
2021-04-18 20:40:18 +0900

    [Linux] TextToSpeech 空実装

--- Siv3D/src/Siv3D-Platform/Linux/Siv3D/TextToSpeech/CTextToSpeech.cpp
+++ Siv3D/src/Siv3D-Platform/Linux/Siv3D/TextToSpeech/CTextToSpeech.cpp
@@ -24,5 +24,68 @@ namespace s3d
 	void CTextToSpeech::init()
 	{
 		LOG_SCOPED_TRACE(U"CTextToSpeech::init()");
+
+		// [Siv3D ToDo]
+	}
+
+	bool CTextToSpeech::hasLanguage(const LanguageCode languageCode)
+	{
+		// [Siv3D ToDo]
+		return(false);
+	}
+
+	void CTextToSpeech::setDefaultLanguage(const LanguageCode languageCode)
+	{
+		// [Siv3D ToDo]
+	}
+
+	LanguageCode CTextToSpeech::getDefaultLanguage() const
+	{
+		// [Siv3D ToDo]
+		return(LanguageCode::Unspecified);
+	}
+
+	bool CTextToSpeech::speak(const StringView text, const LanguageCode languageCode)
+	{
+		// [Siv3D ToDo]
+		return(false);
+	}
+
+	bool CTextToSpeech::isSpeaking() const
+	{
+		// [Siv3D ToDo]
+		return(false);
+	}
+
+	void CTextToSpeech::pause()
+	{
+		// [Siv3D ToDo]
+	}
+
+	void CTextToSpeech::resume()
+	{
+		// [Siv3D ToDo]
+	}
+
+	void CTextToSpeech::setVolume(const double volume)
+	{
+		// [Siv3D ToDo]
+	}
+
+	double CTextToSpeech::getVolume() const
+	{
+		// [Siv3D ToDo]
+		return(1.0);
+	}
+
+	void CTextToSpeech::setSpeed(const double speed)
+	{
+		// [Siv3D ToDo]
+	}
+
+	double CTextToSpeech::getSpeed() const
+	{
+		// [Siv3D ToDo]
+		return(1.0);
 	}
 }
