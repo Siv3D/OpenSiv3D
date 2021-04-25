dc5a923f1faa05cf5c478839ea463e96531485e4
Ryo Suzuki <reputeless+github@gmail.com>
2021-04-25 19:57:21 +0900

    [共通] Say

--- Siv3D/include/Siv3D/Say.hpp
+++ Siv3D/include/Siv3D/Say.hpp
@@ -10,3 +10,81 @@
 //-----------------------------------------------
 
 # pragma once
+# include <memory>
+# include "Common.hpp"
+# include "Formatter.hpp"
+
+namespace s3d
+{
+	namespace detail
+	{
+		struct SayBuffer
+		{
+			std::unique_ptr<FormatData> formatData;
+
+			SayBuffer();
+
+			SayBuffer(SayBuffer&& other) noexcept;
+
+			~SayBuffer();
+
+			SIV3D_CONCEPT_FORMATTABLE
+			SayBuffer& operator <<(const Formattable& value)
+			{
+				Formatter(*formatData, value);
+
+				return *this;
+			}
+		};
+
+		struct Say_impl
+		{
+			void operator()(const char32_t* s) const;
+
+			void operator()(StringView s) const;
+
+			void operator()(const String& s) const;
+
+		# if __cpp_lib_concepts
+
+			template <Concept::Formattable... Args>
+			void operator()(const Args&... args) const
+			{
+				return write(Format(args..., U'\n'));
+			}
+
+			template <class... Args>
+			void operator()(const Args&... args) const
+			{
+				// Format できない値が Say() に渡されたときに発生するエラーです
+				static_assert(0, "Say(): Unformattable parameter value detected");
+			}
+
+		# else
+
+			template <class... Args>
+			void operator()(const Args&... args) const
+			{
+				return write(Format(args..., U'\n'));
+			}
+
+		# endif
+
+			/// @brief Format 可能な値を Say で出力するバッファに追加します。
+			/// @tparam Formattable 値の型（Format 可能な型でないといけません）
+			/// @param value 出力する値
+			/// @return Say 出力のバッファ
+			SIV3D_CONCEPT_FORMATTABLE
+			SayBuffer operator <<(const Formattable& value) const
+			{
+				SayBuffer buf;
+
+				Formatter(*buf.formatData, value);
+
+				return buf;
+			}
+		};
+	}
+
+	inline constexpr auto Say = detail::Say_impl{};
+}
---
+++ Siv3D/src/Siv3D/Say/SivSay.cpp
@@ -0,0 +1,48 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/Say.hpp>
+# include <Siv3D/TextToSpeech.hpp>
+
+namespace s3d
+{
+	namespace detail
+	{
+		SayBuffer::SayBuffer()
+			: formatData{ std::make_unique<FormatData>() } {}
+
+		SayBuffer::SayBuffer(SayBuffer&& other) noexcept
+			: formatData{ std::move(other.formatData) } {}
+
+		SayBuffer::~SayBuffer()
+		{
+			if (formatData)
+			{
+				TextToSpeech::Speak(formatData->string);
+			}
+		}
+
+		void Say_impl::operator()(const char32_t* s) const
+		{
+			TextToSpeech::Speak(s);
+		}
+
+		void Say_impl::operator()(const StringView s) const
+		{
+			TextToSpeech::Speak(s);
+		}
+
+		void Say_impl::operator()(const String& s) const
+		{
+			TextToSpeech::Speak(s);
+		}
+	}
+}
