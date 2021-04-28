61812aa7cd36582514f2429c9f48705ad57cd292
nokotan <kamenokonokotan@gmail.com>
2021-04-29 00:40:10 +0900

    [Web] Copy from linux version

---
+++ Siv3D/src/Siv3D-Platform/Web/Siv3D/Clipboard/CClipboard.cpp
@@ -0,0 +1,85 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/EngineLog.hpp>
+# include <Siv3D/Window/IWindow.hpp>
+# include <Siv3D/Common/Siv3DEngine.hpp>
+# include "CClipboard.hpp"
+
+namespace s3d
+{
+	CClipboard::CClipboard() {}
+
+	CClipboard::~CClipboard()
+	{
+		LOG_SCOPED_TRACE(U"CClipboard::~CClipboard()");
+	}
+
+	void CClipboard::init()
+	{
+		LOG_SCOPED_TRACE(U"CClipboard::init()");
+
+		m_window = static_cast<GLFWwindow*>(SIV3D_ENGINE(Window)->getHandle());
+	}
+
+	bool CClipboard::hasChanged()
+	{
+		// [Siv3D ToDo]
+		return false;
+	}
+
+	bool CClipboard::getText(String& text)
+	{
+		if (m_window)
+		{
+			text = Unicode::Widen(::glfwGetClipboardString(m_window));
+		}
+		else
+		{
+			text.clear();
+		}
+		
+		return (not text.isEmpty());
+	}
+
+	bool CClipboard::getImage(Image& image)
+	{
+		image.clear();
+
+		// [Siv3D ToDo]
+
+		return (not image.isEmpty());
+	}
+
+	bool CClipboard::getFilePaths(Array<FilePath>& paths)
+	{
+		paths.clear();
+
+		// [Siv3D ToDo]
+
+		return (not paths.isEmpty());
+	}
+
+	void CClipboard::setText(const String& text)
+	{
+		::glfwSetClipboardString(m_window, text.narrow().c_str());
+	}
+
+	void CClipboard::setImage(const Image& image)
+	{
+		// [Siv3D ToDo]
+	}
+
+	void CClipboard::clear()
+	{
+		// [Siv3D ToDo]
+	}
+}
---
+++ Siv3D/src/Siv3D-Platform/Web/Siv3D/TextInput/CTextInput.hpp
@@ -0,0 +1,76 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include <mutex>
+# include <Siv3D/TextInput/ITextInput.hpp>
+# include <Siv3D/Stopwatch.hpp>
+# include <Siv3D/Common/OpenGL.hpp>
+
+namespace s3d
+{
+	class CTextInput final : public ISiv3DTextInput
+	{
+	public:
+
+		CTextInput();
+
+		~CTextInput() override;
+
+		void init() override;
+
+		void update() override;
+
+		void pushChar(uint32 ch) override;
+
+		const String& getChars() const override;
+
+		const String& getEditingText() const override;
+
+		void enableIME(bool enabled) override;
+
+		std::pair<int32, int32> getCursorIndex() const override;
+		
+		const Array<String>& getCandidates() const override;
+		
+		//
+		//	Linux
+		//
+		
+		void onHaveMarkedText(const char* text);
+		
+	private:
+
+		std::mutex m_mutex;
+		
+		String m_internalChars;
+		
+		String m_chars;
+		
+		
+		std::mutex m_mutexMarkedText;
+		
+		String m_internalMarkedText;
+		
+		String m_markedText;
+		
+		bool m_haveMarkedText = false;
+		
+		
+		Stopwatch m_enterPress;
+		
+		Stopwatch m_tabPress;
+		
+		Stopwatch m_backSpacePress;
+		
+		static void OnCharacterInput(GLFWwindow*, uint32 codePoint);
+	};
+}
