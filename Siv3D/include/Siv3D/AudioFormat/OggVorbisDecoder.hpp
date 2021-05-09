6a99089b52d7c8a1a4baed06dc3dab3780947e15
Ryo Suzuki <reputeless+github@gmail.com>
2021-05-09 23:01:59 +0900

    [共通] Ogg Vorbis 準備

---
+++ Siv3D/include/Siv3D/AudioFormat/OggVorbisDecoder.hpp
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include <Siv3D/IAudioDecoder.hpp>
+
+namespace s3d
+{
+	class OggVorbisDecoder : public IAudioDecoder
+	{
+	public:
+
+		[[nodiscard]]
+		StringView name() const override;
+
+		[[nodiscard]]
+		bool isHeader(const uint8(&bytes)[16]) const override;
+
+		[[nodiscard]]
+		const Array<String>& possibleExtensions() const override;
+
+		[[nodiscard]]
+		AudioFormat audioFormat() const noexcept override;
+
+		[[nodiscard]]
+		Wave decode(FilePathView path) const override;
+
+		[[nodiscard]]
+		Wave decode(IReader& reader, FilePathView pathHint = {}) const override;
+	};
+}
---
+++ Siv3D/include/Siv3D/AudioFormat/OggVorbisEncoder.hpp
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+# include <Siv3D/IAudioEncoder.hpp>
+
+namespace s3d
+{
+	class OggVorbisEncoder : public IAudioEncoder
+	{
+	public:
+
+		[[nodiscard]]
+		StringView name() const override;
+
+		[[nodiscard]]
+		const Array<String>& possibleExtensions() const override;
+
+		bool save(const Wave& wave, FilePathView path) const override;
+
+		bool save(const Wave& wave, FilePathView path, WAVEFormat format) const;
+
+		bool encode(const Wave& wave, IWriter& writer) const override;
+
+		bool encode(const Wave& wave, IWriter& writer, int32 quality) const;
+
+		[[nodiscard]]
+		Blob encode(const Wave& wave) const override;
+
+		[[nodiscard]]
+		Blob encode(const Wave& wave, int32 quality) const;
+	};
+}
