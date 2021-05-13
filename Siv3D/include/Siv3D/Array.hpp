e1f860702b17268d419f861f7f4704aa36fbf3be
Ryo Suzuki <reputeless+github@gmail.com>
2021-05-13 11:53:05 +0900

    [共通] update Array

--- Siv3D/include/Siv3D/Array.hpp
+++ Siv3D/include/Siv3D/Array.hpp
@@ -44,16 +44,37 @@ namespace s3d
 		/// @brief 配列の要素の型
 		using value_type				= typename base_type::value_type;
 
+		/// @brief 要素のポインタ型
 		using pointer					= typename base_type::pointer;
+		
+		/// @brief 要素の const ポインタ型
 		using const_pointer				= typename base_type::const_pointer;
+		
+		/// @brief 要素の参照型
 		using reference					= typename base_type::reference;
+		
+		/// @brief 要素の const 参照型
 		using const_reference			= typename base_type::const_reference;
+		
+		/// @brief イテレータ型
 		using iterator					= typename base_type::iterator;
+		
+		/// @brief const イテレータ型
 		using const_iterator			= typename base_type::const_iterator;
+		
+		/// @brief リバース・イテレータ型
 		using reverse_iterator			= typename base_type::reverse_iterator;
+		
+		/// @brief const リバース・イテレータ型
 		using const_reverse_iterator	= typename base_type::const_reverse_iterator;
+		
+		/// @brief 要素の個数を表現する型
 		using size_type					= typename base_type::size_type;
+		
+		/// @brief 2 つの要素間の距離を表現する型
 		using difference_type			= typename base_type::difference_type;
+		
+		/// @brief アロケータの型
 		using allocator_type			= typename base_type::allocator_type;
 
 		using base_type::vector;
@@ -100,6 +121,9 @@ namespace s3d
 		SIV3D_NODISCARD_CXX20
 		Array(Array&&) = default;
 
+		/// @brief `asArray()` を持つ型から配列を作成します。
+		/// @tparam ArrayIsh `asArray()` を持つ型
+		/// @param a `asArray()` を持つ型のオブジェクト
 		template <class ArrayIsh, std::enable_if_t<Meta::HasAsArray<ArrayIsh>::value>* = nullptr>
 		SIV3D_NODISCARD_CXX20
 		explicit Array(ArrayIsh&& a);
@@ -211,6 +235,11 @@ namespace s3d
 		/// @return *this
 		Array& operator <<(value_type&& value);
 
+		/// @brief 各要素に関数を適用します。
+		/// @tparam Fty 適用する関数の型
+		/// @param f 適用する関数
+		/// @remark Fty が戻り値を持たない場合 `.each(f), 戻り値を持つ場合は `.map(f)` と同じです。
+		/// @return 各要素に関数を適用した結果の配列。Fty が戻り値を持たない場合 void
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type>>* = nullptr>
 		auto operator >>(Fty f) const;
 
@@ -222,30 +251,58 @@ namespace s3d
 		[[nodiscard]]
 		bool all(Fty f = Identity) const;
 
+		/// @brief 条件を満たす要素があるかを返します。
+		/// @tparam Fty 条件を記述した関数の型
+		/// @param f 条件を記述した関数
+		/// @return 条件を満たす要素が 1 つでもあれば true, 俺以外の場合は false
 		template <class Fty = decltype(Identity), std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		bool any(Fty f = Identity) const;
 
+		/// @brief 配列の末尾に別の配列を追加します。
+		/// @param other_array 追加する配列
+		/// @return *this
 		Array& append(const Array& other_array);
 
+		/// @brief 配列の要素を 1 つランダムに返します。
+		/// @return 配列からランダムに選ばれた要素への参照
 		[[nodiscard]]
 		value_type& choice();
 
+		/// @brief 配列の要素を 1 つランダムに返します。
+		/// @return 配列からランダムに選ばれた要素への参照
 		[[nodiscard]]
 		const value_type& choice() const;
 
+		/// @brief 指定した乱数エンジンを用いて、配列の要素を 1 つランダムに返します。
+		/// @tparam URBG 使用する乱数エンジンの型
+		/// @param rbg 使用する乱数エンジン
+		/// @return 配列からランダムに選ばれた要素への参照
 		SIV3D_CONCEPT_URBG
 		[[nodiscard]]
 		value_type& choice(URBG&& rbg);
 
+		/// @brief 指定した乱数エンジンを用いて、配列の要素を 1 つランダムに返します。
+		/// @tparam URBG 使用する乱数エンジンの型
+		/// @param rbg 使用する乱数エンジン
+		/// @return 配列からランダムに選ばれた要素への参照
 		SIV3D_CONCEPT_URBG
 		[[nodiscard]]
 		const value_type& choice(URBG&& rbg) const;
 
+		/// @brief 配列の要素から指定した個数だけ重複なくランダムに選んで返します。
+		/// @tparam Int 個数に使う整数型
+		/// @param n 選択する個数
+		/// @return ランダムに選ばれた要素の配列
 		SIV3D_CONCEPT_INTEGRAL
 		[[nodiscard]]
 		Array choice(Int n) const;
 
+		/// @brief 指定した乱数エンジンを用いて、 配列の要素から指定した個数だけ重複なくランダムに選んで返します。
+		/// @tparam Size_t 個数に使う整数型
+		/// @param n 選択する個数
+		/// @param rbg 使用する乱数エンジン
+		/// @return ランダムに選ばれた要素の配列
 	# if __cpp_lib_concepts
 		template <Concept::Integral Size_t, Concept::UniformRandomBitGenerator URBG>
 	# else
@@ -255,300 +312,598 @@ namespace s3d
 		[[nodiscard]]
 		Array choice(Size_t n, URBG&& rbg) const;
 
+		/// @brief 指定した個数の要素を持つ配列のグループに分割します。最後のグループの要素数は n 個未満になることがあります。
+		/// @param n 1 つのグループが持つ要素数
+		/// @remark { 0, 1, 2, 3, 4, 5, 6 } を 3 個の要素を持つ配列のグループに分割すると { { 0, 1, 2 }, { 3, 4, 5 }, { 6 }} になります。
+		/// @return 分割されたグループ
 		[[nodiscard]]
 		Array<Array<value_type>> chunk(size_t n) const;
 
+		/// @brief 指定した値と等しい要素の個数を返します。
+		/// @param value 検索する値
+		/// @return 指定した値と等しい要素の個数
 		[[nodiscard]]
 		size_t count(const value_type& value) const;
 
+		/// @brief 条件を満たす要素の個数を返します。
+		/// @tparam Fty 条件を記述した関数の型
+		/// @param f 条件を記述した関数
+		/// @return 条件を満たす要素の個数
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		size_t count_if(Fty f) const;
 
+		/// @brief 全ての要素を順番に引数にして関数を呼び出します。
+		/// @tparam Fty 呼び出す関数の型
+		/// @param f 呼び出す関数
+		/// @remark `for (auto& x : xs) f(x);` と同じです。
+		/// @return *this
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type&>>* = nullptr>
 		Array& each(Fty f);
 
+		/// @brief 全ての要素を順番に引数にして関数を呼び出します。
+		/// @tparam Fty 呼び出す関数の型
+		/// @param f 呼び出す関数
+		/// @remark `for (const auto& x : xs) f(x);` と同じです。
+		/// @return *this
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type>>* = nullptr>
 		const Array& each(Fty f) const;
 
+		/// @brief 全ての要素とそのインデックスを順番に引数にして関数を呼び出します。
+		/// @tparam Fty 呼び出す関数の型
+		/// @param f 呼び出す関数
+		/// @remark `for (size_t i = 0; auto& x : xs) f(i++, x);` と同じです。
+		/// @return *this
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, size_t, Type&>>* = nullptr>
 		Array& each_index(Fty f);
 
+		/// @brief 全ての要素とそのインデックスを順番に引数にして関数を呼び出します。
+		/// @tparam Fty 呼び出す関数の型
+		/// @param f 呼び出す関数
+		/// @remark `for (size_t i = 0; const auto& x : xs) f(i++, x);` と同じです。
+		/// @return *this
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, size_t, Type>>* = nullptr>
 		const Array& each_index(Fty f) const;
 
+		/// @brief 指定したインデックスにある要素を返します。インデックスが範囲外の場合デフォルト値を返します。
+		/// @param index インデックス
+		/// @param defaultValue インデックスが範囲外の場合に返すデフォルト値
+		/// @return 指定したインデックスにある要素、範囲外の場合 defaultValue
 		[[nodiscard]]
 		const value_type& fetch(size_t index, const value_type& defaultValue) const;
 
+		/// @brief 指定した値を全ての要素に代入します。
+		/// @param value 代入する値
+		/// @return *this
 		Array& fill(const value_type& value);
 
+		/// @brief 指定した条件を満たす要素だけを集めた配列を返します。
+		/// @tparam Fty 条件を記述した関数の型
+		/// @param f 条件を記述した関数
+		/// @remark 結果において、要素の前後関係は維持されます。
+		/// @return 指定した条件を満たす要素を集めた配列
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		Array filter(Fty f) const;
 
+		/// @brief 要素を指定したグループ数に分割します。
+		/// @param group グループ数
+		/// @remark { 0, 1, 2, 3, 4, 5, 6 } を 3 グループに分割すると { { 0, 1, 2 }, { 3, 4 }, { 5, 6 }} になります。
+		/// @return 分割したグループ
 		[[nodiscard]]
 		Array<Array<value_type>> in_groups(size_t group) const;
 
+		/// @brief 指定した値と等しい要素があるかを返します。
+		/// @param value 検索する値
+		/// @return 指定した値と等しい要素がある場合 true, それ以外の場合は false
 		[[nodiscard]]
 		bool includes(const value_type& value) const;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		bool includes_if(Fty f) const;
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasLessThan_v<T>>* = nullptr>
 		[[nodiscard]]
 		bool isSorted() const;
 
+		/// @brief 
+		/// @param sep 
+		/// @param begin 
+		/// @param end 
+		/// @return 
 		[[nodiscard]]
 		String join(StringView sep = U", "_sv, StringView begin = U"{"_sv, StringView end = U"}"_sv) const;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type>>* = nullptr>
 		auto map(Fty f) const;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty = decltype(Identity), std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		bool none(Fty f = Identity) const;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @tparam R 
+		/// @param f 
+		/// @param init 
+		/// @return 
 		template <class Fty, class R = std::decay_t<std::invoke_result_t<Fty, Type, Type>>>
 		auto reduce(Fty f, R init) const;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type, Type>>* = nullptr>
 		auto reduce1(Fty f) const;
 
+		/// @brief 
+		/// @param value 
+		/// @return 
 		Array& remove(const value_type& value);
 
+		/// @brief 
+		/// @param value 
+		/// @return 
 		[[nodiscard]]
 		Array removed(const value_type& value) const&;
 
+		/// @brief 
+		/// @param value 
+		/// @return 
 		[[nodiscard]]
 		Array removed(const value_type& value)&&;
 
+		/// @brief 
+		/// @param index 
+		/// @return 
 		Array& remove_at(size_t index);
 
+		/// @brief 
+		/// @param index 
+		/// @return 
 		[[nodiscard]]
 		Array removed_at(size_t index) const;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		Array& remove_if(Fty f);
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		Array removed_if(Fty f) const&;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		Array removed_if(Fty f)&&;
 
+		/// @brief 
+		/// @param oldValue 
+		/// @param newValue 
+		/// @return 
 		Array& replace(const value_type& oldValue, const value_type& newValue);
 
+		/// @brief 
+		/// @param oldValue 
+		/// @param newValue 
+		/// @return 
 		[[nodiscard]]
 		Array replaced(const value_type& oldValue, const value_type& newValue) const&;
 
+		/// @brief 
+		/// @param oldValue 
+		/// @param newValue 
+		/// @return 
 		[[nodiscard]]
 		Array replaced(const value_type& oldValue, const value_type& newValue)&&;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @param newValue 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		Array& replace_if(Fty f, const value_type& newValue);
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @param newValue 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		Array replaced_if(Fty f, const value_type& newValue) const&;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @param newValue 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		Array replaced_if(Fty f, const value_type& newValue)&&;
 
+		/// @brief 
+		/// @return 
 		Array& reverse();
 
+		/// @brief 
+		/// @return 
 		[[nodiscard]]
 		Array reversed() const&;
 
+		/// @brief 
+		/// @return 
 		[[nodiscard]]
 		Array reversed()&&;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type&>>* = nullptr>
 		Array& reverse_each(Fty f);
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type>>* = nullptr>
 		const Array& reverse_each(Fty f) const;
 
+		/// @brief 
+		/// @param count 
+		/// @return 
 		Array& rotate(std::ptrdiff_t count = 1);
 
+		/// @brief 
+		/// @param count 
+		/// @return 
 		[[nodiscard]]
 		Array rotated(std::ptrdiff_t count = 1) const&;
 
+		/// @brief 
+		/// @param count 
+		/// @return 
 		[[nodiscard]]
 		Array rotated(std::ptrdiff_t count = 1)&&;
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasGreaterThan_v<T>>* = nullptr>
 		Array& rsort();
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasGreaterThan_v<T>>* = nullptr>
 		[[nodiscard]]
 		Array rsorted() const&;
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasGreaterThan_v<T>>* = nullptr>
 		[[nodiscard]]
 		Array rsorted()&&;
 
+		/// @brief 
+		/// @return 
 		Array& shuffle();
 
+		/// @brief 
+		/// @param rbg 
+		/// @return 
 		SIV3D_CONCEPT_URBG
 		Array& shuffle(URBG&& rbg);
 
+		/// @brief 
+		/// @return 
 		[[nodiscard]]
 		Array shuffled() const&;
 
+		/// @brief 
+		/// @return 
 		[[nodiscard]]
 		Array shuffled()&&;
 
+		/// @brief 
+		/// @tparam URBG 
+		/// @param rbg 
+		/// @return 
 		SIV3D_CONCEPT_URBG
 		[[nodiscard]]
 		Array shuffled(URBG&& rbg) const&;
 
+		/// @brief 
+		/// @tparam URBG 
+		/// @param rbg 
+		/// @return 
 		SIV3D_CONCEPT_URBG
 		[[nodiscard]]
 		Array shuffled(URBG&& rbg)&&;
 
+		/// @brief 
+		/// @param index 
+		/// @return 
 		[[nodiscard]]
 		Array slice(size_t index) const;
 
+		/// @brief 
+		/// @param index 
+		/// @param length 
+		/// @return 
 		[[nodiscard]]
 		Array slice(size_t index, size_t length) const;
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasLessThan_v<T>>* = nullptr>
 		Array& sort();
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasLessThan_v<T>>* = nullptr>
 		Array& stable_sort();
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type, Type>>* = nullptr>
 		Array& sort_by(Fty f);
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type, Type>>* = nullptr>
 		Array& stable_sort_by(Fty f);
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasLessThan_v<T>>* = nullptr>
 		[[nodiscard]]
 		Array sorted() const&;
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasLessThan_v<T>>* = nullptr>
 		[[nodiscard]]
 		Array stable_sorted() const&;
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasLessThan_v<T>>* = nullptr>
 		[[nodiscard]]
 		Array sorted()&&;
 
+		/// @brief 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasLessThan_v<T>>* = nullptr>
 		[[nodiscard]]
 		Array stable_sorted()&&;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type, Type>>* = nullptr>
 		[[nodiscard]]
 		Array sorted_by(Fty f) const&;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type, Type>>* = nullptr>
 		[[nodiscard]]
 		Array stable_sorted_by(Fty f) const&;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type, Type>>* = nullptr>
 		[[nodiscard]]
 		Array sorted_by(Fty f)&&;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type, Type>>* = nullptr>
 		[[nodiscard]]
 		Array stable_sorted_by(Fty f)&&;
 
+		/// @brief 
+		/// @tparam T 
+		/// @return 
 		template <class T = Type, std::enable_if_t<Meta::HasPlus_v<T>>* = nullptr>
 		[[nodiscard]]
 		auto sum() const;
 
+		/// @brief 
+		/// @tparam T 
 		template <class T = Type, std::enable_if_t<not Meta::HasPlus_v<T>>* = nullptr>
 		void sum() const = delete;
 
+		/// @brief 
+		/// @tparam T 
+		/// @return 
 		template <class T = Type, std::enable_if_t<std::is_floating_point_v<T>>* = nullptr>
 		[[nodiscard]]
 		auto sumF() const;
 
+		/// @brief 
+		/// @tparam T 
+		/// @return 
 		template <class T = Type, std::enable_if_t<not std::is_floating_point_v<T>>* = nullptr>
 		[[nodiscard]]
 		auto sumF() const = delete;
 
+		/// @brief 
+		/// @param n 
+		/// @return 
 		[[nodiscard]]
 		Array take(size_t n) const;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		Array take_while(Fty f) const;
 
+		/// @brief 
+		/// @return 
 		Array& stable_unique();
 
+		/// @brief 
+		/// @return 
 		[[nodiscard]]
 		Array stable_uniqued() const;
 
+		/// @brief 
+		/// @return 
 		Array& sort_and_unique();
 
+		/// @brief 
+		/// @return 
 		[[nodiscard]]
 		Array sorted_and_uniqued() const&;
 
+		/// @brief 
+		/// @return 
 		[[nodiscard]]
 		Array sorted_and_uniqued()&&;
 
+		/// @brief 
+		/// @return 
 		Array& unique_consecutive();
 
+		/// @brief 
+		/// @return 
 		[[nodiscard]]
 		Array uniqued_consecutive() const&;
 
+		/// @brief 
+		/// @return 
 		[[nodiscard]]
 		Array uniqued_consecutive()&&;
 
+		/// @brief 
+		/// @param indices 
+		/// @return 
 		[[nodiscard]]
 		Array values_at(std::initializer_list<size_t> indices) const;
 
 	# ifndef SIV3D_NO_CONCURRENT_API
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<bool, Fty, Type>>* = nullptr>
 		[[nodiscard]]
 		size_t parallel_count_if(Fty f) const;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type&>>* = nullptr>
 		void parallel_each(Fty f);
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type>>* = nullptr>
 		void parallel_each(Fty f) const;
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param f 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_v<Fty, Type>>* = nullptr>
 		auto parallel_map(Fty f) const;
 
 	# endif
 
+		/// @brief 
+		/// @param output 
+		/// @param value 
+		/// @return 
 		friend std::ostream& operator <<(std::ostream& output, const Array& value)
 		{
 			return (output << Format(value).narrow());
 		}
 
+		/// @brief 
+		/// @param output 
+		/// @param value 
+		/// @return 
 		friend std::wostream& operator <<(std::wostream& output, const Array& value)
 		{
 			return (output << Format(value).toWstr());
 		}
 
+		/// @brief 
+		/// @param output 
+		/// @param value 
+		/// @return 
 		friend std::basic_ostream<char32>& operator <<(std::basic_ostream<char32>& output, const Array& value)
 		{
 			const String s = Format(value);
 			return output.write(s.data(), s.size());
 		}
 
+		/// @brief 
+		/// @param formatData 
+		/// @param value 
 		friend void Formatter(FormatData& formatData, const Array& value)
 		{
 			Formatter(formatData, value.begin(), value.end());
 		}
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param size 
+		/// @param generator 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<Type, Fty>>* = nullptr>
 		[[nodiscard]]
 		static Array Generate(size_type size, Fty generator);
 
+		/// @brief 
+		/// @tparam Fty 
+		/// @param size 
+		/// @param indexedGenerator 
+		/// @return 
 		template <class Fty, std::enable_if_t<std::is_invocable_r_v<Type, Fty, size_t>>* = nullptr>
 		[[nodiscard]]
 		static Array IndexedGenerate(size_type size, Fty indexedGenerator);
@@ -588,6 +943,12 @@ namespace s3d
 	template <class ArrayIsh, std::enable_if_t<Meta::HasAsArray<ArrayIsh>::value>* = nullptr>
 	Array(const ArrayIsh& a)->Array<typename decltype(std::declval<ArrayIsh>().asArray())::value_type>;
 
+	/// @brief 
+	/// @tparam T0 
+	/// @tparam ...Ts 
+	/// @param first 
+	/// @param ...args 
+	/// @return 
 	template <class T0, class... Ts>
 	[[nodiscard]]
 	inline auto MakeArray(T0&& first, Ts&&... args);
