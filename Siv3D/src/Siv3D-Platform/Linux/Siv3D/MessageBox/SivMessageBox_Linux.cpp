8b061c874c7b200802335112f3dd4d9d29319523
Ryo Suzuki <reputeless+github@gmail.com>
2021-05-18 23:14:12 +0900

    [Linux] MessageBox 準備

---
+++ Siv3D/src/Siv3D-Platform/Linux/Siv3D/MessageBox/SivMessageBox_Linux.cpp
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/MessageBox.hpp>
+
+namespace s3d
+{
+	namespace System
+	{
+		//MessageBoxResult MessageBoxOK(const StringView title, const StringView text, const MessageBoxStyle style)
+		//{
+
+		//}
+
+		//MessageBoxResult MessageBoxOKCancel(const StringView title, const StringView text, const MessageBoxStyle style)
+		//{
+
+		//}
+
+		//MessageBoxResult MessageBoxYesNo(const StringView title, const StringView text, const MessageBoxStyle style)
+		//{
+
+		//}
+	}
+}
