0d497d516e3997d7e36fdcfadd3e05cfdd4449ef
Reputeless <reputeless@gmail.com>
2021-05-19 16:40:00 +0900

    [Linux] Dialog

--- Siv3D/src/Siv3D-Platform/Linux/Siv3D/Dialog/SivDialog_Linux.cpp
+++ Siv3D/src/Siv3D-Platform/Linux/Siv3D/Dialog/SivDialog_Linux.cpp
@@ -10,33 +10,144 @@
 //-----------------------------------------------
 
 # include <Siv3D/Dialog.hpp>
+# include <ThirdParty/nativefiledialog-extended/nfd.hpp>
 
 namespace s3d
 {
+	static bool g_dialogInitialized = false;
+
 	namespace Dialog
 	{
-		Optional<FilePath> OpenFile(const Array<FileFilter>& filters, const FilePathView defaultPath, const StringView title)
+		Optional<FilePath> OpenFile(const Array<FileFilter>& filters, const FilePathView defaultPath, const StringView)
 		{
-			// [Siv3D ToDo]
-			return (none);
+			if (not g_dialogInitialized)
+			{
+				NFD_Init();
+				g_dialogInitialized = true;
+			}
+
+			const Array<std::pair<std::string, std::string>> tmp =
+				filters.map([](const FileFilter& f) ->std::pair<std::string, std::string>
+					{ return{ f.name.narrow(), f.patterns.join(U",", U"", U"").narrow() }; });
+
+			const Array<nfdfilteritem_t> filterItems = tmp
+				.map([](const std::pair<std::string, std::string>& p)
+					{ return nfdfilteritem_t{ p.first.c_str(), p.second.c_str() }; });
+
+			const std::string defaultPathUTF8 = defaultPath.narrow();
+			const char* defaultPathC = (defaultPathUTF8.empty() ? nullptr : defaultPathUTF8.data());
+
+			NFD::UniquePath outPath;
+			const nfdresult_t result = NFD::OpenDialog(outPath, filterItems.data(), static_cast<nfdfiltersize_t>(filterItems.size()), defaultPathC);
+	
+			if (result == NFD_OKAY)
+			{
+				return Unicode::Widen(outPath.get());
+			}
+			else
+			{
+				return none;
+			}
 		}
 
-		Array<FilePath> OpenFiles(const Array<FileFilter>& filters, const FilePathView defaultPath, const StringView title)
+		Array<FilePath> OpenFiles(const Array<FileFilter>& filters, const FilePathView defaultPath, const StringView)
 		{
-			// [Siv3D ToDo]
-			return{};
+			if (not g_dialogInitialized)
+			{
+				NFD_Init();
+				g_dialogInitialized = true;
+			}
+
+			const Array<std::pair<std::string, std::string>> tmp =
+				filters.map([](const FileFilter& f) ->std::pair<std::string, std::string>
+					{ return{ f.name.narrow(), f.patterns.join(U",", U"", U"").narrow() }; });
+
+			const Array<nfdfilteritem_t> filterItems = tmp
+				.map([](const std::pair<std::string, std::string>& p)
+					{ return nfdfilteritem_t{ p.first.c_str(), p.second.c_str() }; });
+
+			const std::string defaultPathUTF8 = defaultPath.narrow();
+			const char* defaultPathC = (defaultPathUTF8.empty() ? nullptr : defaultPathUTF8.data());
+
+			NFD::UniquePathSet outPaths;
+			const nfdresult_t result = NFD::OpenDialogMultiple(outPaths, filterItems.data(), static_cast<nfdfiltersize_t>(filterItems.size()), defaultPathC);
+
+			if (result == NFD_OKAY)
+			{
+				nfdpathsetsize_t numPaths;
+				NFD::PathSet::Count(outPaths, numPaths);
+
+				Array<FilePath> results(Arg::reserve = numPaths);
+
+				for (nfdfiltersize_t i = 0; i < numPaths; ++i)
+				{
+					NFD::UniquePathSetPath path;
+					NFD::PathSet::GetPath(outPaths, i, path);
+					results << Unicode::Widen(path.get());
+				}
+
+				return results;
+			}
+			else
+			{
+				return{};
+			}
 		}
 
-		Optional<FilePath> SaveFile(const Array<FileFilter>& filters, const FilePathView defaultPath, const StringView title)
+		Optional<FilePath> SaveFile(const Array<FileFilter>& filters, const FilePathView defaultPath, const StringView)
 		{
-			// [Siv3D ToDo]
-			return (none);
+			if (not g_dialogInitialized)
+			{
+				NFD_Init();
+				g_dialogInitialized = true;
+			}
+
+			const Array<std::pair<std::string, std::string>> tmp =
+				filters.map([](const FileFilter& f) ->std::pair<std::string, std::string>
+					{ return{ f.name.narrow(), f.patterns.join(U",", U"", U"").narrow() }; });
+
+			const Array<nfdfilteritem_t> filterItems = tmp
+				.map([](const std::pair<std::string, std::string>& p)
+					{ return nfdfilteritem_t{ p.first.c_str(), p.second.c_str() }; });
+
+			const std::string defaultPathUTF8 = defaultPath.narrow();
+			const char* defaultPathC = (defaultPathUTF8.empty() ? nullptr : defaultPathUTF8.data());
+
+			NFD::UniquePath outPath;
+			const nfdresult_t result = NFD::SaveDialog(outPath, filterItems.data(), static_cast<nfdfiltersize_t>(filterItems.size()), defaultPathC);
+
+			if (result == NFD_OKAY)
+			{
+				return Unicode::Widen(outPath.get());
+			}
+			else
+			{
+				return none;
+			}
 		}
 
-		Optional<FilePath> SelectFolder(const FilePathView defaultPath, const StringView title)
+		Optional<FilePath> SelectFolder(const FilePathView defaultPath, const StringView)
 		{
-			// [Siv3D ToDo]
-			return (none);
+			if (not g_dialogInitialized)
+			{
+				NFD_Init();
+				g_dialogInitialized = true;
+			}
+
+			const std::string defaultPathUTF8 = defaultPath.narrow();
+			const char* defaultPathC = (defaultPathUTF8.empty() ? nullptr : defaultPathUTF8.data());
+
+			NFD::UniquePath outPath;
+			const nfdresult_t result = NFD::PickFolder(outPath, defaultPathC);
+
+			if (result == NFD_OKAY)
+			{
+				return Unicode::Widen(outPath.get());
+			}
+			else
+			{
+				return none;
+			}
 		}
 	}
 }
