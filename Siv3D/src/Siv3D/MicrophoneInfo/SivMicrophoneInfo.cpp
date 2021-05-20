cc9a5553fac7bf4b34dd297c449bd51776bd29b6
Ryo Suzuki <reputeless+github@gmail.com>
2021-05-21 00:08:26 +0900

    [共通] MicrophoneInfo 統合

--- Siv3D/src/Siv3D/MicrophoneInfo/SivMicrophoneInfo.cpp
+++ Siv3D/src/Siv3D/MicrophoneInfo/SivMicrophoneInfo.cpp
@@ -1,4 +1,4 @@
-//-----------------------------------------------
+﻿//-----------------------------------------------
 //
 //	This file is part of the Siv3D Engine.
 //
@@ -20,27 +20,27 @@ namespace s3d
 		Array<MicrophoneInfo> EnumerateMicrophones()
 		{
 			LOG_SCOPED_TRACE(U"System::EnumerateMicrophones()");
-			
+
 			RtAudio device;
-			
+
 			const uint32 num_devices = device.getDeviceCount();
-			
+
 			Array<MicrophoneInfo> results;
-			
+
 			uint32 deviceIndex = 0;
-			
+
 			for (uint32 i = 0; i < num_devices; ++i)
 			{
 				const auto info = device.getDeviceInfo(i);
-				
+
 				if (info.inputChannels == 0)
 				{
 					continue;
 				}
-				
-				const String name = Unicode::Widen(info.name);
+
+				const String name = Unicode::FromUTF8(info.name);
 				const Array<uint32> sampleRates(info.sampleRates.begin(), info.sampleRates.end());
-				
+
 				results.push_back(MicrophoneInfo{ deviceIndex, name, sampleRates, info.preferredSampleRate });
 				++deviceIndex;
 			}
