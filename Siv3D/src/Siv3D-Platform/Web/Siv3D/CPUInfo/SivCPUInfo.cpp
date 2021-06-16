d67fbec11b748fd9f1dc642b05f310692d6788b0
kamenokonokotan <kamenokonokotan@gmail.com>
2021-06-17 01:53:59 +0900

    [Web] Add fake CPUInfo

---
+++ Siv3D/src/Siv3D-Platform/Web/Siv3D/CPUInfo/SivCPUInfo.cpp
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <cstring>
+# include <Siv3D/CPUInfo.hpp>
+# include <Siv3D/Unicode.hpp>
+
+namespace s3d
+{
+	namespace detail
+	{
+		[[nodiscard]]
+		static CPUInfo InitCPUInfo() noexcept
+		{
+			constexpr bool SSEEnabled
+		# if defined(__SSE4_2__)
+				= true;
+		# else
+				= false;
+		# endif
+
+			CPUInfo result
+			{
+				.features = 
+				{
+					.mmx = SSEEnabled,
+					.sse = SSEEnabled,
+					.sse2 = SSEEnabled,
+					.sse3 = SSEEnabled,
+					.sse4_1 = SSEEnabled,
+					.sse4_2 = SSEEnabled,
+				},
+
+				.family	= 0,
+				.model	= 0,
+				.stepping = 0,
+
+				.vendor	= U"Unknown",
+				.brand	= U"Unknown"
+			};
+
+			return result;
+		}
+	}
+
+	const CPUInfo g_CPUInfo = detail::InitCPUInfo();
+
+	const CPUInfo& GetCPUInfo() noexcept
+	{
+		return g_CPUInfo;
+	}
+}
