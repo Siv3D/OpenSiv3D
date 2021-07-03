72bae02067bd9365816ee796bc068a09749f54c1
kamenokonokotan <kamenokonokotan@gmail.com>
2021-07-04 00:41:27 +0900

    [Web] add error fallback on abort

---
+++ Web/Siv3D.pre.js
@@ -0,0 +1,21 @@
+(function() 
+{
+    Module["onAbort"] = function(e) 
+    {
+        if (e instanceof WebAssembly.CompileError)
+        {
+            const additionalMessage = 
+                `WebAssembly compilation failed. 
+Maybe some webassembly features are required to execute this program.
+Please refer https://webassembly.org/roadmap/ to check which webassembly features are supported in your brouser.
+
+Original Error: ${e}`
+            
+            window.alert(additionalMessage);
+        }
+        else
+        {
+            window.alert(e)
+        }
+    }
+})()
