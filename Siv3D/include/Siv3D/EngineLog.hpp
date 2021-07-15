7c448ed60d1b730878371d3bc934f01659c38a42
Ryo Suzuki <reputeless+github@gmail.com>
2021-07-15 23:27:57 +0900

    [共通] update EngineLog

--- Siv3D/include/Siv3D/EngineLog.hpp
+++ Siv3D/include/Siv3D/EngineLog.hpp
@@ -34,8 +34,8 @@
 #	define LOG_FAIL(S)		s3d::Internal::OutputEngineLog(s3d::LogType::Fail,		S)
 #	define LOG_WARNING(S)	s3d::Internal::OutputEngineLog(s3d::LogType::Warning,	S)
 #	define LOG_INFO(S)		s3d::Internal::OutputEngineLog(s3d::LogType::Info,		S)
-#	define LOG_TRACE(S)		s3d::Internal::OutputEngineLog(s3d::LogType::Trace,		S)
+#	define LOG_TRACE(S)		((void)0)
 #	define LOG_VERBOSE(S)	((void)0)
-#	define LOG_SCOPED_TRACE(S)	const s3d::Internal::ScopedEngineLog s3d_scoped_trace{s3d::LogType::Trace, S}
+#	define LOG_SCOPED_TRACE(S)	((void)0)
 
 # endif
