e68f0229c985d159ecc56028f4babed1419a7e1b
Ryo Suzuki <reputeless+github@gmail.com>
2021-07-19 23:47:38 +0900

    [GL4] fix

--- Siv3D/src/Siv3D-Platform/OpenGL4/Siv3D/Renderer/GL4/BlendState/GL4BlendState.cpp
+++ Siv3D/src/Siv3D-Platform/OpenGL4/Siv3D/Renderer/GL4/BlendState/GL4BlendState.cpp
@@ -86,12 +86,10 @@ namespace s3d
 		{
 			if (state.alphaToCoverageEnable)
 			{
-				::glEnable(GL_MULTISAMPLE);
 				::glEnable(GL_SAMPLE_ALPHA_TO_COVERAGE);
 			}
 			else
 			{
-				::glDisable(GL_MULTISAMPLE);
 				::glDisable(GL_SAMPLE_ALPHA_TO_COVERAGE);
 			}
 		}
