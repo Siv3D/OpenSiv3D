54db22831370c624cd8b48626ed414e54d86997f
kamenokonokotan <kamenokonokotan@gmail.com>
2021-09-14 02:33:06 +0900

    [Web] Update Microphone

---
+++ Siv3D/src/Siv3D-Platform/Web/Siv3D/MicrophoneInfo/SivMicrophoneInfo.cpp
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2021 Ryo Suzuki
+//	Copyright (c) 2016-2021 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/MicrophoneInfo.hpp>
+# include <Siv3D/EngineLog.hpp>
+# include <Siv3D/Unicode.hpp>
+
+namespace s3d
+{
+	namespace System
+	{
+		Array<MicrophoneInfo> EnumerateMicrophones()
+		{
+			LOG_SCOPED_TRACE(U"System::EnumerateMicrophones()");
+
+			Array<MicrophoneInfo> results;
+
+			return results;
+		}
+	}
+}
