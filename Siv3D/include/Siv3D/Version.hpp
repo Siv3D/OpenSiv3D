e8e41e6be2e763237faf62972d30413ec55c7886
Ryo Suzuki <reputeless+github@gmail.com>
2021-10-24 14:51:08 +0900

    [共通] update version

--- Siv3D/include/Siv3D/Version.hpp
+++ Siv3D/include/Siv3D/Version.hpp
@@ -18,7 +18,7 @@
 # define SIV3D_VERSION_MINOR			6
 
 /// @biref ライブラリのリビジョンバージョン | Library revision version
-# define SIV3D_VERSION_REVISION			2
+# define SIV3D_VERSION_REVISION			3
 
 /// @biref プレリリースタグ | Pre-release tag
 # define SIV3D_VERSION_PRERELEASE_TAG	U""
@@ -26,8 +26,11 @@
 /// @biref ライブラリのバージョン | Library version
 # define SIV3D_VERSION			((SIV3D_VERSION_MAJOR * 100 * 100) + (SIV3D_VERSION_MINOR * 100) + (SIV3D_VERSION_REVISION))
 
+/// @biref ライブラリのバージョンの文字列（簡易版） | Short Library version
+# define SIV3D_VERSION_SHORT_STRING	U"0.6.3"
+
 /// @biref ライブラリのバージョンの文字列 | Library version
-# define SIV3D_VERSION_STRING	U"0.6.2" SIV3D_VERSION_PRERELEASE_TAG
+# define SIV3D_VERSION_STRING	U"0.6.3" SIV3D_VERSION_PRERELEASE_TAG
 
 /// @biref ライブラリのバージョン名 | Library version name
-# define SIV3D_VERSION_NAME		U"Siv3D September 2021 r2"
+# define SIV3D_VERSION_NAME		U"Siv3D September 2021 r3"
