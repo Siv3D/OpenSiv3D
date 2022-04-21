f6f3572ccc4310db12f6c796a9caea54595b9a76
Ryo Suzuki <reputeless+github@gmail.com>
2022-04-22 08:21:55 +0900

    [共通] -1.0 ～ 1.0 の範囲を返す Periodic:: を追加 #761

--- Siv3D/include/Siv3D/Periodic.hpp
+++ Siv3D/include/Siv3D/Periodic.hpp
@@ -69,6 +69,59 @@ namespace s3d
 	// 0.0 *       *       *       *
 	//     ----------------------------
 	//     0       1       2       3
+	//
+	// 
+	// 
+	// [Sine1_1]
+	// 
+	// 1.0 | **      **      **      **
+	//     |*  *    *  *    *  *    *
+	// 0.0 *    *  *    *  *    *  *
+	//     |    *  *    *  *    *  *
+	//-1.0 |     **      **      **
+	//     ----------------------------
+	//     0       1       2       3
+	//
+	// [Square1_1]
+	// 
+	// 1.0 ****    ****    ****    ****
+	//     |
+	// 0.5 |
+	//     |  
+	//-1.0 |   ****    ****    ****   
+	//     ----------------------------
+	//     0       1       2       3
+	//
+	// [Triangle1_1]
+	// 
+	// 1.0 |   *       *       *       
+	//     |  * *     * *     * *     *
+	// 0.0 | *   *   *   *   *   *   *
+	//     |*     * *     * *     * *
+	//-1.0 *       *       *       *
+	//     ----------------------------
+	//     0       1       2       3
+	//
+	// [Sawtooth1_1]
+	// 
+	// 1.0 |       -       -       -
+	//     |     *       *       *
+	// 0.0 |   *       *       *       *
+	//     | *       *       *       *
+	//-1.0 *       +       +       +
+	//     ----------------------------
+	//     0       1       2       3
+	//
+	// [Jump1_1]
+	// 
+	// 1.0 |  -^-     -^-     -^-     -
+	//     | *   *   *   *   *   *   *
+	// 0.0 |*     * *     * *     * *
+	//     |*     * *     * *     * *
+	//-1.0 *       *       *       *
+	//     ----------------------------
+	//     0       1       2       3
+	//
 
 	namespace Periodic
 	{
@@ -128,11 +181,90 @@ namespace s3d
 		[[nodiscard]]
 		inline double Sawtooth0_1(const Duration& period, double t = Scene::Time()) noexcept;
 
+		/// @brief ジャンプする運動に従って、周期的に [0.0, 1.0] の値を返します。
+		/// @param periodSec 周期（秒）
+		/// @param t 経過時間（秒）
+		/// @return [0.0, 1.0] の範囲の値
 		[[nodiscard]]
 		inline double Jump0_1(double periodSec, double t = Scene::Time()) noexcept;
 
+		/// @brief ジャンプする運動に従って、周期的に [0.0, 1.0] の値を返します。
+		/// @param period 周期
+		/// @param t 経過時間（秒）
+		/// @return [0.0, 1.0] の範囲の値
 		[[nodiscard]]
 		inline double Jump0_1(const Duration& period, double t = Scene::Time()) noexcept;
+
+
+		/// @brief サインカーブに従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param periodSec 周期（秒）
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Sine1_1(double periodSec, double t = Scene::Time()) noexcept;
+
+		/// @brief サインカーブに従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param period 周期
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Sine1_1(const Duration& period, double t = Scene::Time()) noexcept;
+
+		/// @brief 矩形波に従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param periodSec 周期（秒）
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Square1_1(double periodSec, double t = Scene::Time()) noexcept;
+
+		/// @brief 矩形波に従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param period 周期
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Square1_1(const Duration& period, double t = Scene::Time()) noexcept;
+
+		/// @brief 三角波に従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param periodSec 周期（秒）
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Triangle1_1(double periodSec, double t = Scene::Time()) noexcept;
+
+		/// @brief 三角波に従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param period 周期
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Triangle1_1(const Duration& period, double t = Scene::Time()) noexcept;
+
+		/// @brief のこぎり波に従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param periodSec 周期（秒）
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Sawtooth1_1(double periodSec, double t = Scene::Time()) noexcept;
+
+		/// @brief のこぎり波に従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param period 周期
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Sawtooth1_1(const Duration& period, double t = Scene::Time()) noexcept;
+
+		/// @brief ジャンプする運動に従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param periodSec 周期（秒）
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Jump1_1(double periodSec, double t = Scene::Time()) noexcept;
+
+		/// @brief ジャンプする運動に従って、周期的に [-1.0, 1.0] の値を返します。
+		/// @param period 周期
+		/// @param t 経過時間（秒）
+		/// @return [-1.0, 1.0] の範囲の値
+		[[nodiscard]]
+		inline double Jump1_1(const Duration& period, double t = Scene::Time()) noexcept;
 	}
 }
 
--- Siv3D/include/Siv3D/detail/Periodic.ipp
+++ Siv3D/include/Siv3D/detail/Periodic.ipp
@@ -82,5 +82,74 @@ namespace s3d
 		{
 			return Jump0_1(period.count(), t);
 		}
+
+
+		inline double Sine1_1(const double periodSec, const double t) noexcept
+		{
+			const double x = (std::fmod(t, periodSec) / (periodSec * Math::InvTwoPi));
+
+			return std::sin(x);
+		}
+
+		inline double Sine1_1(const Duration& period, const double t) noexcept
+		{
+			return Sine1_1(period.count(), t);
+		}
+
+		inline double Square1_1(const double periodSec, const double t) noexcept
+		{
+			return (std::fmod(t, periodSec) < (periodSec * 0.5)) ? 1.0 : -1.0;
+		}
+
+		inline double Square1_1(const Duration& period, const double t) noexcept
+		{
+			return Square1_1(period.count(), t);
+		}
+
+		inline double Triangle1_1(const double periodSec, const double t) noexcept
+		{
+			const double x = (std::fmod(t, periodSec) / (periodSec * 0.5));
+
+			if (x <= 1.0)
+			{
+				return (2.0 * x - 1.0);
+			}
+			else
+			{
+				return (3.0 - 2.0 * x);
+			}
+		}
+
+		inline double Triangle1_1(const Duration& period, const double t) noexcept
+		{
+			return Triangle1_1(period.count(), t);
+		}
+
+		inline double Sawtooth1_1(const double periodSec, const double t) noexcept
+		{
+			return ((std::fmod(t, periodSec) / periodSec) * 2.0 - 1.0);
+		}
+
+		inline double Sawtooth1_1(const Duration& period, const double t) noexcept
+		{
+			return Sawtooth1_1(period.count(), t);
+		}
+
+		inline double Jump1_1(const double periodSec, const double t) noexcept
+		{
+			double x = (std::fmod(t, periodSec) / (periodSec * 0.5));
+
+			if (1.0 < x)
+			{
+				x = (2.0 - x);
+			}
+
+			return ((2 * x - (x * x)) * 2.0 - 1.0);
+		}
+
+		inline double Jump1_1(const Duration& period, const double t) noexcept
+		{
+			return Jump1_1(period.count(), t);
+		}
 	}
 }
