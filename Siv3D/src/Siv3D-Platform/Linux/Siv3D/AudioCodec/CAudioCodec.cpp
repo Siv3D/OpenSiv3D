bc15e3b4f5d95d102bdef5038c511e4a0fbb731c
Ryo Suzuki <reputeless+github@gmail.com>
2022-05-01 20:10:21 +0900

    [Linux] fix
    reported by: https://twitter.com/Kyure_A/status/1520711070016409600

--- Siv3D/src/Siv3D-Platform/Linux/Siv3D/AudioCodec/CAudioCodec.cpp
+++ Siv3D/src/Siv3D-Platform/Linux/Siv3D/AudioCodec/CAudioCodec.cpp
@@ -10,6 +10,7 @@
 //-----------------------------------------------
 
 # include <Siv3D/Error.hpp>
+# include <Siv3D/Unicode.hpp>
 # include <Siv3D/DLL.hpp>
 # include <Siv3D/EngineLog.hpp>
 # include "CAudioCodec.hpp"
