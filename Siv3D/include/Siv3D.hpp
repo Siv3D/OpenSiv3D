c2a0a097402645f18106e8570ffa2da8049b5b29
MayFlyOvO <ckmiaojapan@gmail.com>
2022-11-14 13:37:47 +0900

    OrderedTable 型を追加 (#909)

--- Siv3D/include/Siv3D.hpp
+++ Siv3D/include/Siv3D.hpp
@@ -162,6 +162,9 @@
 // 文字列ルックアップヘルパー | Heterogeneous lookup helper
 # include <Siv3D/HeterogeneousLookupHelper.hpp>
 
+// 順序付けテーブル | Ordered table
+# include <Siv3D/OrderedTable.hpp>
+
 // ハッシュテーブル | Hash table
 # include <Siv3D/HashTable.hpp>
 
