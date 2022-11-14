734c665226a5abc97192dfa524f94710c03dc14a
Ryo Suzuki <reputeless+github@gmail.com>
2022-11-14 23:04:37 +0900

    [共通] parallel_hashmap v1.3.4 → v1.3.8

--- Siv3D/include/ThirdParty/parallel_hashmap/phmap_dump.h
+++ Siv3D/include/ThirdParty/parallel_hashmap/phmap_dump.h
@@ -54,9 +54,9 @@ bool raw_hash_set<Policy, Hash, Eq, Alloc>::phmap_dump(OutputArchive& ar) const 
                     "value_type should be trivially copyable");
 
     ar.saveBinary(&size_, sizeof(size_t));
+    ar.saveBinary(&capacity_, sizeof(size_t));
     if (size_ == 0)
         return true;
-    ar.saveBinary(&capacity_, sizeof(size_t));
     ar.saveBinary(ctrl_,  sizeof(ctrl_t) * (capacity_ + Group::kWidth + 1));
     ar.saveBinary(slots_, sizeof(slot_type) * capacity_);
     return true;
@@ -69,12 +69,14 @@ bool raw_hash_set<Policy, Hash, Eq, Alloc>::phmap_load(InputArchive& ar) {
                     "value_type should be trivially copyable");
     raw_hash_set<Policy, Hash, Eq, Alloc>().swap(*this); // clear any existing content
     ar.loadBinary(&size_, sizeof(size_t));
-    if (size_ == 0)
-        return true;
     ar.loadBinary(&capacity_, sizeof(size_t));
 
-    // allocate memory for ctrl_ and slots_
-    initialize_slots(capacity_);
+    if (capacity_) {
+        // allocate memory for ctrl_ and slots_
+        initialize_slots(capacity_);
+    }
+    if (size_ == 0)
+        return true;
     ar.loadBinary(ctrl_,  sizeof(ctrl_t) * (capacity_ + Group::kWidth + 1));
     ar.loadBinary(slots_, sizeof(slot_type) * capacity_);
     return true;
@@ -156,6 +158,19 @@ public:
         return true;
     }
 
+    template<typename V>
+    typename std::enable_if<type_traits_internal::IsTriviallyCopyable<V>::value, bool>::type
+    saveBinary(const V& v) {
+        ofs_.write(reinterpret_cast<const char *>(&v), sizeof(V));
+        return true;
+    }
+
+    template<typename Map>
+    auto saveBinary(const Map& v) -> decltype(v.phmap_dump(*this), bool())
+    {
+        return v.phmap_dump(*this);
+    }
+
 private:
     std::ofstream ofs_;
 };
@@ -172,6 +187,19 @@ public:
         return true;
     }
 
+    template<typename V>
+    typename std::enable_if<type_traits_internal::IsTriviallyCopyable<V>::value, bool>::type
+    loadBinary(V* v) {
+        ifs_.read(reinterpret_cast<char *>(v), sizeof(V));
+        return true;
+    }
+
+    template<typename Map>
+    auto loadBinary(Map* v) -> decltype(v->phmap_load(*this), bool())
+    {
+        return v->phmap_load(*this);
+    }
+    
 private:
     std::ifstream ifs_;
 };
