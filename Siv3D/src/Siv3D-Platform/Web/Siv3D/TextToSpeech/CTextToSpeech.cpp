8d83c219303e0f52180e0af285944ab45dbb4296
nokotan <kamenokonokotan@gmail.com>
2023-01-19 21:00:33 +0900

    Revert "[Web] Delete TextToSpeech Support for its unavailability and annoying warnings"
    This reverts commit 2bfc8c4dc552ab90da021ad636a4384efc0b1937.

--- Siv3D/src/Siv3D-Platform/Web/Siv3D/TextToSpeech/CTextToSpeech.cpp
+++ Siv3D/src/Siv3D-Platform/Web/Siv3D/TextToSpeech/CTextToSpeech.cpp
@@ -14,6 +14,38 @@
 
 namespace s3d
 {
+	namespace detail
+	{
+		struct AvailableLanguageList
+		{
+			struct AvailableLanguage
+			{
+				LanguageCode languageCode;
+
+				bool isDefault;
+			};
+
+			size_t length;
+
+			AvailableLanguage* data;
+		};
+
+		__attribute__((import_name("siv3dEnumerateAvailableTextToSpeechLanguages")))
+		extern AvailableLanguageList siv3dEnumerateAvailableTextToSpeechLanguages();
+
+		__attribute__((import_name("siv3dStartTextToSpeechLanguages")))
+		extern void siv3dStartTextToSpeechLanguages(const char* text, double rate, double volume, LanguageCode code);
+
+		__attribute__((import_name("siv3dIsSpeakingTextToSpeechLanguages")))
+		extern bool siv3dIsSpeakingTextToSpeechLanguages();
+
+		__attribute__((import_name("siv3dPauseTextToSpeechLanguages")))
+		extern void siv3dPauseTextToSpeechLanguages();
+
+		__attribute__((import_name("siv3dResumeTextToSpeechLanguages")))
+		extern void siv3dResumeTextToSpeechLanguages();
+	}
+
 	CTextToSpeech::CTextToSpeech() {}
 
 	CTextToSpeech::~CTextToSpeech()
@@ -25,62 +57,94 @@ namespace s3d
 	{
 		LOG_SCOPED_TRACE(U"CTextToSpeech::init()");
 
-		
+		auto list = detail::siv3dEnumerateAvailableTextToSpeechLanguages();
+
+		if (list.length > 0) 
+		{
+			LOG_INFO(U"ℹ️ TextToSpeech available");
+		}
+		else
+		{
+			LOG_WARNING(U"⚠️ TextToSpeech unavailable");
+		}
+
+		for (uint32 i = 0; i < list.length; ++i)
+		{
+			auto& item = list.data[i];
+
+			m_AvailableLanguages << item.languageCode;
+
+			if (item.isDefault)
+			{
+				m_DefaultLanguage = item.languageCode;
+				
+				LOG_TRACE(U"LanguageCode: {} - Default"_fmt(item.languageCode));
+			}
+			else
+			{
+				LOG_TRACE(U"LanguageCode: {}"_fmt(item.languageCode));
+			}
+		}
+
+		::free(list.data);
 	}
 
 	bool CTextToSpeech::hasLanguage(const LanguageCode languageCode)
 	{
-		return false;
+		return m_AvailableLanguages.includes(languageCode);
 	}
 
 	void CTextToSpeech::setDefaultLanguage(const LanguageCode languageCode)
 	{
-		
+		m_DefaultLanguage = languageCode;
 	}
 
 	LanguageCode CTextToSpeech::getDefaultLanguage() const
 	{
-		return LanguageCode::Unspecified;
+		return m_DefaultLanguage;
 	}
 
 	bool CTextToSpeech::speak(const StringView text, const LanguageCode languageCode)
 	{
-		return (false);
+		const LanguageCode lang = (languageCode == LanguageCode::Unspecified) ? m_DefaultLanguage : languageCode;
+
+		detail::siv3dStartTextToSpeechLanguages(text.toUTF8().c_str(), m_Speed, m_Volume, lang);
+		return (true);
 	}
 
 	bool CTextToSpeech::isSpeaking() const
 	{
-		return (false);
+		return detail::siv3dIsSpeakingTextToSpeechLanguages();
 	}
 
 	void CTextToSpeech::pause()
 	{
-
+		detail::siv3dPauseTextToSpeechLanguages();
 	}
 
 	void CTextToSpeech::resume()
 	{
-		
+		detail::siv3dResumeTextToSpeechLanguages();
 	}
 
 	void CTextToSpeech::setVolume(const double volume)
 	{
-		
+		m_Volume = volume;
 	}
 
 	double CTextToSpeech::getVolume() const
 	{
-		return 0.0;
+		return m_Volume;
 	}
 
 	void CTextToSpeech::setSpeed(const double speed)
 	{
-		
+		m_Speed = speed;
 	}
 
 	double CTextToSpeech::getSpeed() const
 	{
 		// [Siv3D ToDo]
-		return 1.0;
+		return m_Speed;
 	}
 }
