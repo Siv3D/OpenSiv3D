e1f21b83d70f34e7fa04e46457c4d24770728c9f
Ryo Suzuki <reputeless+github@gmail.com>
2023-02-06 22:25:19 +0900

    [共通] Main.cpp の更新 #940

--- Linux/App/Main.cpp
+++ Linux/App/Main.cpp
@@ -22,61 +22,99 @@ void Main()
 //
 //	Example graphical program
 //
-# include <Siv3D.hpp> // OpenSiv3D v0.6.6
+# include <Siv3D.hpp> // OpenSiv3D v0.6.7
+
 void Main()
 {
-	// 背景の色を設定 | Set background color
-	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+	// 背景の色を設定する | Set the background color
+	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
 
-	// 通常のフォントを作成 | Create a new font
-	const Font font{ 60 };
-
-	// 絵文字用フォントを作成 | Create a new emoji font
-	const Font emojiFont{ 60, Typeface::ColorEmoji };
-
-	// `font` が絵文字用フォントも使えるようにする | Set emojiFont as a fallback
-	font.addFallback(emojiFont);
-
-	// 画像ファイルからテクスチャを作成 | Create a texture from an image file
+	// 画像ファイルからテクスチャを作成する | Create a texture from an image file
 	const Texture texture{ U"example/windmill.png" };
 
-	// 絵文字からテクスチャを作成 | Create a texture from an emoji
-	const Texture emoji{ U"🐈"_emoji };
+	// 絵文字からテクスチャを作成する | Create a texture from an emoji
+	const Texture emoji{ U"🦖"_emoji };
 
-	// 絵文字を描画する座標 | Coordinates of the emoji
-	Vec2 emojiPos{ 300, 150 };
+	// 太文字のフォントを作成する | Create a bold font with MSDF method
+	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
 
-	// テキストを画面にデバッグ出力 | Print a text
-	Print << U"Push [A] key";
+	// テキストに含まれる絵文字のためのフォントを作成し、font に追加する | Create a font for emojis in text and add it to font as a fallback
+	const Font emojiFont{ 48, Typeface::ColorEmoji };
+	font.addFallback(emojiFont);
+
+	// ボタンを押した回数 | Number of button presses
+	int32 count = 0;
+
+	// チェックボックスの状態 | Checkbox state
+	bool checked = false;
+
+	// プレイヤーの移動スピード | Player's movement speed
+	double speed = 200.0;
+
+	// プレイヤーの X 座標 | Player's X position
+	double playerPosX = 400;
+
+	// プレイヤーが右を向いているか | Whether player is facing right
+	bool isPlayerFacingRight = true;
 
 	while (System::Update())
 	{
-		// テクスチャを描く | Draw a texture
-		texture.draw(200, 200);
+		// テクスチャを描く | Draw the texture
+		texture.draw(20, 20);
 
-		// テキストを画面の中心に描く | Put a text in the middle of the screen
-		font(U"Hello, Siv3D!🚀").drawAt(Scene::Center(), Palette::Black);
+		// テキストを描く | Draw text
+		font(U"Hello, Siv3D!🎮").draw(64, Vec2{ 20, 340 }, ColorF{ 0.2, 0.4, 0.8 });
 
-		// サイズをアニメーションさせて絵文字を描く | Draw a texture with animated size
-		emoji.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(emojiPos);
+		// 指定した範囲内にテキストを描く | Draw text within a specified area
+		font(U"Siv3D (シブスリーディー) は、ゲームやアプリを楽しく簡単な C++ コードで開発できるフレームワークです。")
+			.draw(18, Rect{ 20, 430, 480, 200 }, Palette::Black);
 
-		// マウスカーソルに追随する半透明な円を描く | Draw a red transparent circle that follows the mouse cursor
-		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1, 0, 0, 0.5 });
+		// 長方形を描く | Draw a rectangle
+		Rect{ 540, 20, 80, 80 }.draw();
 
-		// もし [A] キーが押されたら | When [A] key is down
-		if (KeyA.down())
+		// 角丸長方形を描く | Draw a rounded rectangle
+		RoundRect{ 680, 20, 80, 200, 20 }.draw(ColorF{ 0.0, 0.4, 0.6 });
+
+		// 円を描く | Draw a circle
+		Circle{ 580, 180, 40 }.draw(Palette::Seagreen);
+
+		// 矢印を描く | Draw an arrow
+		Line{ 540, 330, 760, 260 }.drawArrow(8, SizeF{ 20, 20 }, ColorF{ 0.4 });
+
+		// 半透明の円を描く | Draw a semi-transparent circle
+		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1.0, 0.0, 0.0, 0.5 });
+
+		// ボタン | Button
+		if (SimpleGUI::Button(U"count: {}"_fmt(count), Vec2{ 520, 370 }, 120, (checked == false)))
 		{
-			// 選択肢からランダムに選ばれたメッセージをデバッグ表示 | Print a randomly selected text
-			Print << Sample({ U"Hello!", U"こんにちは", U"你好", U"안녕하세요?" });
+			// カウントを増やす | Increase the count
+			++count;
 		}
 
-		// もし [Button] が押されたら | When [Button] is pushed
-		if (SimpleGUI::Button(U"Button", Vec2{ 640, 40 }))
+		// チェックボックス | Checkbox
+		SimpleGUI::CheckBox(checked, U"Lock \U000F033E", Vec2{ 660, 370 }, 120);
+
+		// スライダー | Slider
+		SimpleGUI::Slider(U"speed: {:.1f}"_fmt(speed), speed, 100, 400, Vec2{ 520, 420 }, 140, 120);
+
+		// 左キーが押されていたら | If left key is pressed
+		if (KeyLeft.pressed())
 		{
-			// 画面内のランダムな場所に座標を移動
-			// Move the coordinates to a random position in the screen
-			emojiPos = RandomVec2(Scene::Rect());
+			// プレイヤーが左に移動する | Player moves left
+			playerPosX = Max((playerPosX - speed * Scene::DeltaTime()), 60.0);
+			isPlayerFacingRight = false;
 		}
+
+		// 右キーが押されていたら | If right key is pressed
+		if (KeyRight.pressed())
+		{
+			// プレイヤーが右に移動する | Player moves right
+			playerPosX = Min((playerPosX + speed * Scene::DeltaTime()), 740.0);
+			isPlayerFacingRight = true;
+		}
+
+		// プレイヤーを描く | Draw the player
+		emoji.scaled(0.75).mirrored(isPlayerFacingRight).drawAt(playerPosX, 540);
 	}
 }
 //
--- Web/VisualStudioTemplate/Files/Main.cpp
+++ Web/VisualStudioTemplate/Files/Main.cpp
@@ -2,56 +2,94 @@
 
 void Main()
 {
-    // Set background color to sky blue
-    Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+	// 背景の色を設定する | Set the background color
+	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
 
-    // Create a new font
-    const Font font{ 60 };
-    
-    // Create a new emoji font
-    const Font emojiFont{ 60, Typeface::ColorEmoji };
-    
-    // Set emojiFont as a fallback
-    font.addFallback(emojiFont);
+	// 画像ファイルからテクスチャを作成する | Create a texture from an image file
+	const Texture texture{ U"example/windmill.png" };
 
-    // Create a texture from an image file
-    const Texture texture{ U"example/windmill.png" };
+	// 絵文字からテクスチャを作成する | Create a texture from an emoji
+	const Texture emoji{ U"🦖"_emoji };
 
-    // Create a texture from an emoji
-    const Texture emoji{ U"🐈"_emoji };
+	// 太文字のフォントを作成する | Create a bold font with MSDF method
+	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
 
-    // Coordinates of the emoji
-    Vec2 emojiPos{ 300, 150 };
+	// テキストに含まれる絵文字のためのフォントを作成し、font に追加する | Create a font for emojis in text and add it to font as a fallback
+	const Font emojiFont{ 48, Typeface::ColorEmoji };
+	font.addFallback(emojiFont);
 
-    // Print a text
-    Print << U"Push [A] key";
+	// ボタンを押した回数 | Number of button presses
+	int32 count = 0;
 
-    while (System::Update())
-    { 
-        // Draw a texture
-        texture.draw(200, 200);
+	// チェックボックスの状態 | Checkbox state
+	bool checked = false;
 
-        // Put a text in the middle of the screen
-        font(U"Hello, Siv3D!🚀").drawAt(Scene::Center(), Palette::Black);
+	// プレイヤーの移動スピード | Player's movement speed
+	double speed = 200.0;
 
-        // Draw a texture with animated size
-        emoji.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(emojiPos);
+	// プレイヤーの X 座標 | Player's X position
+	double playerPosX = 400;
 
-        // Draw a red transparent circle that follows the mouse cursor
-        Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1, 0, 0, 0.5 });
+	// プレイヤーが右を向いているか | Whether player is facing right
+	bool isPlayerFacingRight = true;
 
-        // When [A] key is down
-        if (KeyA.down())
-        {
-            // Print a randomly selected text
-            Print << Sample({ U"Hello!", U"こんにちは", U"你好", U"안녕하세요?" });
-        }
+	while (System::Update())
+	{
+		// テクスチャを描く | Draw the texture
+		texture.draw(20, 20);
 
-        // When [Button] is pushed
-        if (SimpleGUI::Button(U"Button", Vec2{ 640, 40 }))
-        {
-            // Move the coordinates to a random position in the screen
-            emojiPos = RandomVec2(Scene::Rect());
-        }
-    }
+		// テキストを描く | Draw text
+		font(U"Hello, Siv3D!🎮").draw(64, Vec2{ 20, 340 }, ColorF{ 0.2, 0.4, 0.8 });
+
+		// 指定した範囲内にテキストを描く | Draw text within a specified area
+		font(U"Siv3D (シブスリーディー) は、ゲームやアプリを楽しく簡単な C++ コードで開発できるフレームワークです。")
+			.draw(18, Rect{ 20, 430, 480, 200 }, Palette::Black);
+
+		// 長方形を描く | Draw a rectangle
+		Rect{ 540, 20, 80, 80 }.draw();
+
+		// 角丸長方形を描く | Draw a rounded rectangle
+		RoundRect{ 680, 20, 80, 200, 20 }.draw(ColorF{ 0.0, 0.4, 0.6 });
+
+		// 円を描く | Draw a circle
+		Circle{ 580, 180, 40 }.draw(Palette::Seagreen);
+
+		// 矢印を描く | Draw an arrow
+		Line{ 540, 330, 760, 260 }.drawArrow(8, SizeF{ 20, 20 }, ColorF{ 0.4 });
+
+		// 半透明の円を描く | Draw a semi-transparent circle
+		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1.0, 0.0, 0.0, 0.5 });
+
+		// ボタン | Button
+		if (SimpleGUI::Button(U"count: {}"_fmt(count), Vec2{ 520, 370 }, 120, (checked == false)))
+		{
+			// カウントを増やす | Increase the count
+			++count;
+		}
+
+		// チェックボックス | Checkbox
+		SimpleGUI::CheckBox(checked, U"Lock \U000F033E", Vec2{ 660, 370 }, 120);
+
+		// スライダー | Slider
+		SimpleGUI::Slider(U"speed: {:.1f}"_fmt(speed), speed, 100, 400, Vec2{ 520, 420 }, 140, 120);
+
+		// 左キーが押されていたら | If left key is pressed
+		if (KeyLeft.pressed())
+		{
+			// プレイヤーが左に移動する | Player moves left
+			playerPosX = Max((playerPosX - speed * Scene::DeltaTime()), 60.0);
+			isPlayerFacingRight = false;
+		}
+
+		// 右キーが押されていたら | If right key is pressed
+		if (KeyRight.pressed())
+		{
+			// プレイヤーが右に移動する | Player moves right
+			playerPosX = Min((playerPosX + speed * Scene::DeltaTime()), 740.0);
+			isPlayerFacingRight = true;
+		}
+
+		// プレイヤーを描く | Draw the player
+		emoji.scaled(0.75).mirrored(isPlayerFacingRight).drawAt(playerPosX, 540);
+	}
 }
