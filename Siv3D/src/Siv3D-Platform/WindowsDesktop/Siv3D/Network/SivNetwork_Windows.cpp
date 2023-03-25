3203aaf41a948a33b293455364846a9a8fda379b
Ryo Suzuki <reputeless+github@gmail.com>
2023-03-25 21:09:51 +0900

    [Windows] Network::IsConnected()

---
+++ Siv3D/src/Siv3D-Platform/WindowsDesktop/Siv3D/Network/SivNetwork_Windows.cpp
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2023 Ryo Suzuki
+//	Copyright (c) 2016-2023 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/Common.hpp>
+# include <Siv3D/Network.hpp>
+# include <Siv3D/Windows/Windows.hpp>
+# include <wininet.h>
+
+namespace s3d
+{
+	namespace Network
+	{
+		bool IsConnected()
+		{
+			DWORD flags = 0;
+			return ::InternetGetConnectedState(&flags, 0);
+		}
+	}
+}
---
+++ Siv3D/src/Siv3D-Platform/macOS/Siv3D/Network/SivNetwork_macOS.cpp
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2023 Ryo Suzuki
+//	Copyright (c) 2016-2023 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include <Siv3D/Common.hpp>
+# include <Siv3D/Network.hpp>
+
+namespace s3d
+{
+	namespace Network
+	{
+		bool IsConnected()
+		{
+			return(true);
+		}
+	}
+}
