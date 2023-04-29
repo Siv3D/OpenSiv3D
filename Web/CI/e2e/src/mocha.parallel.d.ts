eda780da278d11efe489a66d40c9538a3de5fdd5
nokotan <kamenokonokotan@gmail.com>
2023-04-29 12:57:30 +0900

    [Web] remove all files that user will not use/modify

--- Web/CI/e2e/src/mocha.parallel.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "mocha.parallel" {
-
-    export default function parallel(name: string, fn: (this: import("mocha").Context) => void): void;
-
-    export function limit(n: number): void;
-}
