05c163780d309d79a825ea397e6d4f7084abc154
nokotan <kamenokonokotan@gmail.com>
2023-07-19 14:40:37 +0900

    [Web][CI] split tests into separate files

---
+++ Test/Siv3DTest_Format.cpp
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2023 Ryo Suzuki
+//	Copyright (c) 2016-2023 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include "Siv3DTest.hpp"
+
+TEST_CASE("FormatLiteral")
+{
+	SECTION("Fmt")
+	{
+		REQUIRE(Fmt(U"")() == U"");
+		REQUIRE(Fmt(U"{}")(12345) == U"12345");
+
+		REQUIRE(Fmt(U""sv)() == U"");
+		REQUIRE(Fmt(U"{}"sv)(12345) == U"12345");
+
+		REQUIRE(Fmt(String(U""))() == U"");
+		REQUIRE(Fmt(String(U"{}"))(12345) == U"12345");
+	}
+
+	SECTION("_fmt")
+	{
+		REQUIRE(U""_fmt() == U"");
+		REQUIRE(U"{}"_fmt(12345) == U"12345");
+		REQUIRE(U"{}"_fmt(U"OpenSiv3D") == U"OpenSiv3D");
+		REQUIRE(U"{}"_fmt(U"あいうえお") == U"あいうえお");
+	}
+
+	SECTION("formatter<String>")
+	{
+		const String s = U"ABCDE";
+		REQUIRE(U"{}"_fmt(s) == U"ABCDE");
+		REQUIRE(U"{0}"_fmt(s) == U"ABCDE");
+		REQUIRE(U"{:<6}"_fmt(s) == U"ABCDE ");
+		REQUIRE(U"{:*>10}"_fmt(s) == U"*****ABCDE");
+	}
+}
---
+++ Test/Siv3DTest_String.cpp
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2023 Ryo Suzuki
+//	Copyright (c) 2016-2023 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# include "Siv3DTest.hpp"
+
+TEST_CASE("String")
+{
+	SECTION("Constructor")
+	{
+		REQUIRE(String(5, U'A') == U"AAAAA");
+	}
+
+	SECTION("operator bool")
+	{
+		REQUIRE(!String(U"") == true);
+		REQUIRE(!String(U"ABC") == false);
+		REQUIRE(static_cast<bool>(String(U"")) == false);
+		REQUIRE(static_cast<bool>(String(U"ABC")) == true);
+	}
+}
