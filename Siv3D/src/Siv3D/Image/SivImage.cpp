7f50ec7e019b55f37223af7a3e44af02b0ea792b
comefrombottom <120954679+comefrombottom@users.noreply.github.com>
2023-09-17 13:20:55 +0900

    Update Siv3D/src/Siv3D/Image/SivImage.cpp
    Co-authored-by: Raclamusi <61970673+Raclamusi@users.noreply.github.com>

--- Siv3D/src/Siv3D/Image/SivImage.cpp
+++ Siv3D/src/Siv3D/Image/SivImage.cpp
@@ -1232,9 +1232,20 @@ namespace s3d
 
 	Image Image::rotated90(int32 n)&&
 	{
-		// rotate90() が最適化されたら次の実装に変更する
-		// return std::move(rotate90(n));
-		return rotated90(n);
+		switch (n % 4) // 時計回りに何回 90° 回転するか
+		{
+		case 1:
+		case -3:
+			return std::move(*this).rotated90(); // 1 回または -3 回
+		case 2:
+		case -2:
+			return std::move(*this).rotated180(); // 2 回または -2 回
+		case 3:
+		case -1:
+			return std::move(*this).rotated270(); // 3 回または -1 回
+		default:
+			return std::move(*this); // 0 回
+		}
 	}
 
 	Image& Image::rotate180()
