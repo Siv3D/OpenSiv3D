fabff7e9af28281358cce32e10746b517e832cff
raclamusi <raclamusi@gmail.com>
2023-11-14 14:31:51 +0900

    Polygon のバウンディングボックスの計算速度、精度を改善

--- Siv3D/src/Siv3D/Polygon/PolygonDetail.cpp
+++ Siv3D/src/Siv3D/Polygon/PolygonDetail.cpp
@@ -18,14 +18,13 @@ namespace s3d
 {
 	namespace detail
 	{
-		template <class Type>
-		static RectF CalculateBoundingRect(const Vector2D<Type>* const pVertex, const size_t vertexSize)
+		static RectF CalculateBoundingRect(const Vec2* const pVertex, const size_t vertexSize)
 		{
 			assert(pVertex != nullptr);
 			assert(vertexSize != 0);
 
-			const Vector2D<Type>* it = pVertex;
-			const Vector2D<Type>* itEnd = (it + vertexSize);
+			const Vec2* it = pVertex;
+			const Vec2* itEnd = (it + vertexSize);
 
 			double left		= it->x;
 			double top		= it->y;
@@ -209,7 +208,7 @@ namespace s3d
 		m_indices = std::move(indices);
 
 		// [5 of 5]
-		m_boundingRect = detail::CalculateBoundingRect(pOuterVertex, vertexSize);
+		m_boundingRect = detail::CalculateBoundingRect(m_polygon.outer().data(), m_polygon.outer().size());
 	}
 
 	const Array<Vec2>& Polygon::PolygonDetail::outer() const noexcept
@@ -370,7 +369,7 @@ namespace s3d
 			m_holes.back().insert(m_holes.back().end(), hole.begin(), hole.end());
 		}
 
-		m_boundingRect = detail::CalculateBoundingRect(m_vertices.data(), m_vertices.size());
+		m_boundingRect = detail::CalculateBoundingRect(m_polygon.outer().data(), m_polygon.outer().size());
 	}
 
 	void Polygon::PolygonDetail::transform(const double s, const double c, const Vec2& pos)
@@ -417,7 +416,7 @@ namespace s3d
 			m_holes.back().insert(m_holes.back().end(), hole.begin(), hole.end());
 		}
 
-		m_boundingRect = detail::CalculateBoundingRect(m_vertices.data(), m_vertices.size());
+		m_boundingRect = detail::CalculateBoundingRect(m_polygon.outer().data(), m_polygon.outer().size());
 	}
 
 	void Polygon::PolygonDetail::scale(const double s)
