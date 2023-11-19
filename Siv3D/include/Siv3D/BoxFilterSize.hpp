e9def58cf7fa6b57564b1f643952af04c67605ac
Ryo Suzuki <reputeless+github@gmail.com>
2023-11-19 16:24:10 +0900

    BoxFilterSize

---
+++ Siv3D/include/Siv3D/BoxFilterSize.hpp
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------
+//
+//	This file is part of the Siv3D Engine.
+//
+//	Copyright (c) 2008-2023 Ryo Suzuki
+//	Copyright (c) 2016-2023 OpenSiv3D Project
+//
+//	Licensed under the MIT License.
+//
+//-----------------------------------------------
+
+# pragma once
+
+namespace s3d
+{
+	enum class BoxFilterSize
+	{
+		BoxFilter5x5 = 5,
+		
+		BoxFilter9x9 = 9,
+		
+		BoxFilter13x13 = 13,
+	};
+}
