6911db167d23c54436725ecd248239db8d683859
Raclamusi <61970673+Raclamusi@users.noreply.github.com>
2024-03-01 15:16:21 +0900

    LineString の .calculateBuffer(), .calculateRoundBuffer() を1頂点に対応 #1209 (#1210)

--- Siv3D/src/Siv3D/Polygon/PolygonDetail.cpp
+++ Siv3D/src/Siv3D/Polygon/PolygonDetail.cpp
@@ -1210,7 +1210,7 @@ namespace s3d
 	{
 		Polygon CalculateBuffer(const LineString& points, const double distance, CloseRing closeRing, int32 bufferQuality)
 		{
-			if (points.size() < 2)
+			if (points.isEmpty())
 			{
 				return{};
 			}
@@ -1290,7 +1290,7 @@ namespace s3d
 
 		Polygon CalculateRoundBuffer(const LineString& points, const double distance, CloseRing closeRing, int32 bufferQuality)
 		{
-			if (points.size() < 2)
+			if (points.isEmpty())
 			{
 				return{};
 			}
