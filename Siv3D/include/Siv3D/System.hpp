9bdeaf3983be5253d6127d63a6eee121632d3bda
aoriika05 <75664746+aoriika05@users.noreply.github.com>
2024-11-05 21:44:09 +0900

    Doxygenの誤字修正 (#1270)

--- Siv3D/include/Siv3D/System.hpp
+++ Siv3D/include/Siv3D/System.hpp
@@ -22,7 +22,7 @@ namespace s3d
 	namespace System
 	{
 		/// @brief 描画や入力情報など、フレームを更新します。
-		/// @remarkアプリケーション終了トリガーが発生するか、内部で回復不能なエラーが発生した場合に false を返します。この関数が false を返したらプログラムを終了させるべきです。
+		/// @remark アプリケーション終了トリガーが発生するか、内部で回復不能なエラーが発生した場合に false を返します。この関数が false を返したらプログラムを終了させるべきです。
 		/// @return プログラムの続行の可否
 		bool Update();
 
@@ -42,7 +42,7 @@ namespace s3d
 		uint32 GetTerminationTriggers() noexcept;
 
 		/// @brief 前回のフレームで発生したユーザアクションを返します。
-		/// @remarkフラグには `UserAction` の値の組み合わせが使われています。
+		/// @remark フラグには `UserAction` の値の組み合わせが使われています。
 		/// @return 前のフレームで発生したユーザアクションのフラグの組み合わせ
 		[[nodiscard]]
 		uint32 GetUserActions() noexcept;
