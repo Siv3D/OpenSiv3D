2c13022fdb63dab9e6778e6a482d21ec5e42c469
Raclamusi <raclamusi@gmail.com>
2024-11-17 16:38:18 +0900

    長方形内のテキスト描画でドットの間隔がフォントサイズによって変わる問題を修正

--- Siv3D/src/Siv3D/Font/GlyphCache/MSDFGlyphCache.cpp
+++ Siv3D/src/Siv3D/Font/GlyphCache/MSDFGlyphCache.cpp
@@ -105,12 +105,12 @@ namespace s3d
 		}
 		updateTexture();
 
-		const double dotXAdvance = m_glyphTable.find(dotGlyphCluster[0].glyphIndex)->second.info.xAdvance;
 		const Vec2 areaBottomRight = area.br();
 
 		const auto& prop = font.getProperty();
 		const double scale = (size / prop.fontPixelSize);
 		const double lineHeight = (prop.height() * scale * lineHeightScale);
+		const double dotXAdvance = (m_glyphTable.find(dotGlyphCluster[0].glyphIndex)->second.info.xAdvance * scale);
 
 		if ((area.w < (dotXAdvance * 3)) || (area.h < lineHeight))
 		{
@@ -219,12 +219,12 @@ namespace s3d
 		}
 		updateTexture();
 
-		const double dotXAdvance = m_glyphTable.find(dotGlyphCluster[0].glyphIndex)->second.info.xAdvance;
 		const Vec2 areaBottomRight = area.br();
 
 		const auto& prop = font.getProperty();
 		const double scale = (size / prop.fontPixelSize);
 		const double lineHeight = (prop.height() * scale * lineHeightScale);
+		const double dotXAdvance = (m_glyphTable.find(dotGlyphCluster[0].glyphIndex)->second.info.xAdvance * scale);
 
 		if ((area.w < (dotXAdvance * 3)) || (area.h < lineHeight))
 		{
